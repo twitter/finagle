e04e51645374f8d958d85de384142dd00f4b7574
Nick Kallen <nkallen@tallbro.local>
2010-10-18 12:30:51 -0700

    Project scaffolding

---
+++ project/build/Project.scala
@@ -0,0 +1,13 @@
+import sbt._
+
+class Project(info: ProjectInfo) extends DefaultProject(info) {
+  override def managedStyle = ManagedStyle.Maven
+
+  val nettyRepo = "repository.jboss.org" at "http://repository.jboss.org/nexus/content/groups/public/"
+
+  val netty = "org.jboss.netty" % "netty" % "3.2.2.Final"
+  val util = "com.twitter" %% "util" % "1.2-SNAPSHOT"
+
+  val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test" withSources()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
+}
