8fe71f0130a938ccfd8492d5c04911f656deb799
marius a. eriksen <marius@twitter.com>
2010-10-20 15:37:48 -0700

    inline thrift generated java stubs, & go back to DefaultProject.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,12 +1,11 @@
 import sbt._
-import com.twitter.sbt._
 
-class Project(info: ProjectInfo) extends StandardProject(info) {
-  override def disableCrossPaths = false
-
+class Project(info: ProjectInfo) extends DefaultProject(info) {
+  override def compileOrder = CompileOrder.JavaThenScala
   override def managedStyle = ManagedStyle.Maven
 
   val nettyRepo = "repository.jboss.org" at "http://repository.jboss.org/nexus/content/groups/public/"
+  val twitterRepo = "twitter.com" at "http://www.lag.net/nest/"
 
   val netty = "org.jboss.netty" % "netty" % "3.2.2.Final" withSources()
   val util = "com.twitter" %% "util" % "1.2-SNAPSHOT"
@@ -15,35 +14,4 @@ class Project(info: ProjectInfo) extends StandardProject(info) {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources()
   val slf4jNop = "org.slf4j" % "slf4j-nop" % "1.6.1"
   val thrift = "thrift" % "libthrift" % "0.2.0"
-
-  // TODO: consolidate all of this stuff & contribute back to
-  // standard-project.
-  val thriftCompilerPath = "thrift"
-  override def compileThriftAction(lang: String) = task {
-    import Process._
-    outputPath.asFile.mkdirs()
-    val tasks = thriftSources.getPaths.map { path =>
-      execTask { "%s --gen %s -o %s %s".format(
-        thriftCompilerPath, lang, outputPath.absolutePath, path) }
-    }
-    if (tasks.isEmpty) None else tasks.reduceLeft { _ && _ }.run
-  }
-
-  // thrift generation.
-  def testThriftSources = (testSourcePath / "thrift" ##) ** "*.thrift"
-  val testOutputPath = outputPath / "test-gen"
-  def compileTestThriftAction(lang: String) = task {
-    import Process._
-    testOutputPath.asFile.mkdirs()
-    val tasks = testThriftSources.getPaths.map { path =>
-      execTask { "%s --gen %s -o %s %s".format(
-        thriftCompilerPath, lang, testOutputPath.absolutePath, path) }
-    }
-    if (tasks.isEmpty) None else tasks.reduceLeft { _ && _ }.run
-  }
-  override def testSourceRoots = super.testSourceRoots +++ (testOutputPath / "gen-java" ##)
-
-  lazy val compileTestThriftJava = compileTestThriftAction("java") describedAs("Compile (test) thrift into java")
-  override def testCompileAction = super.testCompileAction dependsOn(compileTestThriftJava)
-
 }
