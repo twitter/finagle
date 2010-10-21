7c63969aafbd9cd52a559b333eedf7c7b353e6a5
Nick Kallen <nkallen@tallbro.local>
2010-10-20 19:03:19 -0700

    temp

--- src/test/scala/com/twitter/finagle/channel/PoolingBrokerSpec.scala
+++ src/test/scala/com/twitter/finagle/channel/PoolingBrokerSpec.scala
@@ -6,21 +6,17 @@ import org.jboss.netty.channel._
 
 class PoolingBrokerSpec extends Specification with Mockito {
   "PoolingBroker" should {
-    val channelConnector = mock[ChannelConnector]
+    val someMessage = mock[Object]
     val reservedChannel = mock[Channel]
-    val brokeredChannel = mock[BrokeredChannel]
     val pool = mock[ChannelPool]
     val reserveFuture = new DefaultChannelFuture(reservedChannel, false)
     pool.reserve() returns reserveFuture
-    val writeFuture = new DefaultChannelFuture(reservedChannel, false)
-    val e = mock[MessageEvent]
-    channelConnector(brokeredChannel, reservedChannel, e) returns writeFuture
-    val poolingBroker = new PoolingBroker(channelConnector, pool)
+    val brokeredChannel = new BrokeredChannelFactory().newChannel(Channels.pipeline())
+    brokeredChannel.connect(new PoolingBroker(pool))
 
     "dispatch reserves and releases connection from the pool" in {
-      poolingBroker.dispatch(brokeredChannel, e)
-      reserveFuture.setSuccess()
-      writeFuture.setSuccess()
+      Channels.write(brokeredChannel, someMessage)
+
       there was one(pool).reserve()
       there was one(pool).release(reservedChannel)
     }
