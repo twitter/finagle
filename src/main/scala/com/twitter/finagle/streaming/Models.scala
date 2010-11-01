de6a43757e30b1783c0e24263f654d389e9850cb
wilhelm bierbaum <wilhelm@twitter.com>
2010-11-01 10:08:46 -0700

    CachedMessage -> Models

--- src/main/scala/com/twitter/finagle/streaming/Models.scala
+++ src/main/scala/com/twitter/finagle/streaming/Models.scala
@@ -2,7 +2,30 @@ package com.twitter.finagle.streaming
 
 import CachedMessage._
 
-// Components
+case class CachedMessage(message: String, kind: Int)
+
+object CachedMessage {
+  // Kind as bitmask
+  val KIND_UNKNOWN =       0
+  val KIND_STATUS =        1
+  val KIND_STATUS_DELETE = 2
+  val KIND_LIMIT =         4
+  val KIND_SCRUB_GEO =     8
+  val KIND_SOCIAL =       16
+  val KIND_DM =           32
+
+  // Statuses and friends
+  val KIND_STATUS_LIKE =  (KIND_STATUS|KIND_STATUS_DELETE|KIND_LIMIT|KIND_SCRUB_GEO)
+
+  // Scope as bitmask
+  val SCOPE_PUBLIC =       1
+  val SCOPE_RESTRICTED =   2
+  val SCOPE_PRIVATE =      4
+
+  // Public and Restricted can be explicitly selected by userid
+  val SCOPE_SELECTABLE =   3
+}
+
 case class Geo(lat: Double, long: Double)
 case class Place(minLat: Double, minLong: Double, maxLat: Double, maxLong: Double)
 case class AnnotationAttribute(label: String, value: String)
