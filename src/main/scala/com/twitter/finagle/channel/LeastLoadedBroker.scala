4460d49c899c7778be4d169c26ee7709607c88c6
Nick Kallen <nkallen@tallbro.local>
2010-11-01 14:52:56 -0700

    - Test for LeastLoadedBroker - Test for RequestCountingBroker

--- src/main/scala/com/twitter/finagle/channel/LeastLoadedBroker.scala
+++ src/main/scala/com/twitter/finagle/channel/LeastLoadedBroker.scala
@@ -1,7 +1,5 @@
 package com.twitter.finagle.channel
 
-import java.util.concurrent.atomic.AtomicInteger
-
 import org.jboss.netty.channel.MessageEvent
 
 trait LoadedBroker[A <: LoadedBroker[A]] extends Broker with Ordered[A]
@@ -9,21 +7,20 @@ trait LoadedBroker[A <: LoadedBroker[A]] extends Broker with Ordered[A]
 class RequestCountingBroker(underlying: Broker)
   extends Broker with LoadedBroker[RequestCountingBroker]
 {
-  private[channel] val dispatchCount = new AtomicInteger(0)
+  private[channel] var dispatchCount = 0
 
   def dispatch(e: MessageEvent) = {
-    dispatchCount.incrementAndGet()
+    dispatchCount += 1
     underlying.dispatch(e)
   }
 
   def compare(that: RequestCountingBroker) =
-    dispatchCount.get compare that.dispatchCount.get
+    dispatchCount compare that.dispatchCount
 }
 
 class LeastLoadedBroker[A <: LoadedBroker[A]](endpoints: Seq[A]) extends Broker {
   def dispatch(e: MessageEvent) =
     leastLoadedEndpoint.dispatch(e)
 
-  private def leastLoadedEndpoint =
-    endpoints.min
+  private def leastLoadedEndpoint = endpoints.min
 }
--- src/test/scala/com/twitter/finagle/channel/LeastLoadedBrokerSpec.scala
+++ src/test/scala/com/twitter/finagle/channel/LeastLoadedBrokerSpec.scala
@@ -5,36 +5,43 @@ import org.specs.mock.Mockito
 import org.jboss.netty.channel._
 
 object LeastLoadedBrokerSpec extends Specification with Mockito {
+  class FakeLoadedBroker extends LoadedBroker[FakeLoadedBroker] {
+    def compare(that: FakeLoadedBroker) = 0
+    def dispatch(e: MessageEvent) = null
+  }
+
   "RequestCountingBroker" should {
     "increment on dispatch" in {
-      val underlying = mock[Broker]
-      val request = mock[MessageEvent]
-      val replyFuture = mock[ReplyFuture]
-      underlying.dispatch(request) returns replyFuture
-      
-      val rcBroker = new RequestCountingBroker(underlying)
+      val rcBroker1 = new RequestCountingBroker(mock[Broker])
+      val rcBroker2 = new RequestCountingBroker(mock[Broker])
 
-      rcBroker.dispatchCount.get must be_==(0)
-      rcBroker.dispatch(request) must be_==(replyFuture)
-      rcBroker.dispatchCount.get must be_==(1)
+      (0 until 3) foreach { _ => rcBroker1.dispatch(mock[MessageEvent]) }
+      (0 until 1) foreach { _ => rcBroker2.dispatch(mock[MessageEvent]) }
+
+      (rcBroker1 compare rcBroker2) mustEqual 1
+
+      (0 until 3) foreach { _ => rcBroker2.dispatch(mock[MessageEvent]) }
+
+      (rcBroker1 compare rcBroker2) mustEqual -1
     }
   }
 
+  "LeastLoadedBroker" should {
+    val request = mock[MessageEvent]
 
-  // "LeastLoadedBroker" should {
-  //   val broker1 = mock[LoadedBroker]
-  //   broker1.load returns 1
-  //   val broker2 = mock[LoadedBroker]
-  //   broker2.load returns 2
-  //   val broker3 = mock[LoadedBroker]
-  //   broker3.load returns 3
-  //   val leastLoadedBroker = new LeastLoadedBroker(Seq(broker1, broker2, broker3))
-  //   val channel = mock[BrokeredChannel]
-  //   val messageEvent = mock[MessageEvent]
-  //  
-  //   "dispatches to the least loaded" in {
-  //     leastLoadedBroker.dispatch(messageEvent)
-  //     there was one(broker1).dispatch(messageEvent)
-  //   }
-  // }
+    "dispatch to the least loaded" in {
+      val loadedBroker1 = spy(new FakeLoadedBroker)
+      val loadedBroker2 = spy(new FakeLoadedBroker)
+
+      val leastLoadedBroker = new LeastLoadedBroker(Seq(loadedBroker1, loadedBroker2))
+
+      (loadedBroker1 compare loadedBroker2) returns -1
+      leastLoadedBroker.dispatch(request)
+      there was one(loadedBroker1).dispatch(request)
+
+      (loadedBroker1 compare loadedBroker2) returns 1
+      leastLoadedBroker.dispatch(request)
+      there was one(loadedBroker2).dispatch(request)
+    }
+  }
 }
