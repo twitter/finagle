93dfbe2c33225e036dfb6783701c64e1cd93f53b
marius a. eriksen <marius@twitter.com>
2010-11-11 15:22:43 -0800

    connection limiting channel pool.

--- src/test/scala/com/twitter/finagle/channel/ChannelPool.scala
+++ src/test/scala/com/twitter/finagle/channel/ChannelPool.scala
@@ -10,18 +10,19 @@ import org.specs.mock.Mockito
 
 object ChannelPoolSpec extends Specification with Mockito {
   "ChannelPool" should {
-      val bs = mock[BrokerClientBootstrap]
-      val cp = new ChannelPool(bs)
-      val f1 = mock[ChannelFuture]
-      val c1 = mock[Channel]
-      val f2 = mock[ChannelFuture]
-      val c2 = mock[Channel]
-      f1.getChannel returns c1
-      f2.getChannel returns c2
-      bs.connect() returns f1
+    val bs = mock[BrokerClientBootstrap]
+    val cp = new ChannelPool(bs)
+    val c1 = mock[Channel]
+    val f1 = Channels.succeededFuture(c1)
+    val c2 = mock[Channel]
+    val f2 = Channels.succeededFuture(c2)
+
+    bs.connect() returns f1
 
     "with no Channels, creates a new one and returns it" in {
-      cp.reserve().getChannel mustEqual c1
+      val f = cp.reserve()
+      f.isSuccess must beTrue
+      f.getChannel mustEqual c1
       there was one(bs).connect()
     }
 
@@ -44,6 +45,54 @@ object ChannelPoolSpec extends Specification with Mockito {
       c3.isOpen returns true
       cp.release(c3)
       cp.reserve().getChannel mustEqual c3
+    }
+  }
+
+  "ConnectionLimitingChannelPool" should {
+    val bs = mock[BrokerClientBootstrap]
+    val cp = new ConnectionLimitingChannelPool(bs, 10)
+    val c1 = mock[Channel]
+    c1.isOpen returns true
+    val f1 = Channels.succeededFuture(c1)
+    val cause = new Exception
+    val f2 = Channels.failedFuture(c1, cause)
+
+    bs.connect() returns f1
+
+    "not make more than the specified number of connections" in {
+      0 until 10 foreach { i =>
+        val f = cp.reserve()
+        f.isSuccess must beTrue
+        f.isDone must beTrue
+      }
+
+      val f = cp.reserve()
+      f.isDone must beFalse
+      cp.release(c1)
+      f.isDone must beTrue
+      f.isSuccess must beTrue
+      f.getChannel must be_==(c1)
+    }
+
+    "discount unhealthy channels" in {
+      bs.connect() returns f2
+
+      0 until 10 foreach { i =>
+        val f = cp.reserve()
+        f.isSuccess must beFalse
+        f.isDone must beTrue
+        f.getCause must be_==(cause)
+      }
+
+      // (Note: no releases were made.)
+      
+      bs.connect() returns f1
+
+      0 until 10 foreach { i =>
+        val f = cp.reserve()
+        f.isSuccess must beTrue
+        f.isDone must beTrue
+      }
     }
   }
 }
