22706d14f8799ab2cd1363803f675863b37217ee
marius a. eriksen <marius@twitter.com>
2010-11-29 13:58:01 -0800

    simple service spec for composition.

---
+++ src/test/scala/com/twitter/finagle/service/ServiceSpec.scala
@@ -0,0 +1,34 @@
+package com.twitter.finagle.service
+
+import org.specs.Specification
+
+import com.twitter.util.{Future, Return, Throw}
+
+object ServiceSpec extends Specification {
+  "filters" should {
+    val intToString =
+      new Filter[java.lang.Integer, java.lang.Integer, String, String] {
+        def apply(request: java.lang.Integer, service: Service[String, String]) =
+          service(request.toString) map (_.toInt)
+      }
+
+    val stringToInt = 
+      new Filter[String, String, java.lang.Integer, java.lang.Integer] {
+        def apply(request: String, service: Service[java.lang.Integer, java.lang.Integer]) =
+          service(request.toInt) map (_.toString)
+      }
+
+    "compose" in {
+      val filter: Filter[
+        java.lang.Integer, java.lang.Integer,
+        java.lang.Integer, java.lang.Integer] = intToString andThen stringToInt
+
+      val result = filter(123, new Service[java.lang.Integer, java.lang.Integer] {
+        def apply(request: java.lang.Integer) = Future(2 * request.intValue)
+      })
+
+      result.isReturn must beTrue
+      result() must be_==(123 * 2)
+    }
+  }
+}
