14ecb8e962c5302c7186638dc587a770dfe0c418
marius a. eriksen <marius@twitter.com>
2011-01-14 15:40:53 -0800

    thrift2->thrift

--- finagle-thrift/src/main/scala/com/twitter/finagle/thrift/ThriftFrameCodec.scala
+++ finagle-thrift/src/main/scala/com/twitter/finagle/thrift/ThriftFrameCodec.scala
@@ -1,9 +1,11 @@
 package com.twitter.finagle.thrift
 
 import org.jboss.netty.channel.{
-  SimpleChannelHandler, ChannelEvent, ChannelHandlerContext}
+  SimpleChannelHandler, Channel, ChannelEvent, ChannelHandlerContext}
+import org.jboss.netty.buffer.{ChannelBuffer, ChannelBuffers}
 import org.jboss.netty.handler.codec.frame.{
   LengthFieldBasedFrameDecoder, LengthFieldPrepender}
+import org.jboss.netty.handler.codec.oneone.{OneToOneEncoder, OneToOneDecoder}
 
 class ThriftFrameCodec extends SimpleChannelHandler {
   protected[thrift] val decoder = new LengthFieldBasedFrameDecoder(0x7FFFFFFF, 0, 4, 0, 4)
@@ -14,4 +16,22 @@ class ThriftFrameCodec extends SimpleChannelHandler {
 
   override def handleDownstream(ctx: ChannelHandlerContext, e: ChannelEvent) =
     encoder.handleDownstream(ctx, e)
+}
+
+class ChannelBufferEncoder extends OneToOneEncoder {
+  def encode(ctx: ChannelHandlerContext, ch: Channel, message: Object) = {
+    message match {
+      case array: Array[Byte] => ChannelBuffers.wrappedBuffer(array)
+      case _ => throw new IllegalArgumentException("no byte array")
+    }
+  }
+}
+
+class ChannelBufferDecoder extends OneToOneDecoder {
+  def decode(ctx: ChannelHandlerContext, ch: Channel, message: Object) = {
+    message match {
+      case buffer: ChannelBuffer => buffer.array()  // is this kosher?
+      case _ => throw new IllegalArgumentException("no byte buffer")
+    }
+  }
 }
