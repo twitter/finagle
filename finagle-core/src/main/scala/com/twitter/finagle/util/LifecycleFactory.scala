9a02a83844edc9d361d10a23aba9342354d86961
marius a. eriksen <marius@twitter.com>
2011-01-21 17:09:13 -0800

    a caching lifecycle factory (wrapper) [with a timeout]. used to implement connection "idle time" cleanup.

--- finagle-core/src/main/scala/com/twitter/finagle/util/LifecycleFactory.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/util/LifecycleFactory.scala
@@ -1,6 +1,8 @@
 package com.twitter.finagle.util
 
-import com.twitter.util.Future
+import collection.mutable.Queue
+
+import com.twitter.util.{Future, Time, Duration}
 
 /**
  * Manage object lifecycle [eg. in a pool]. Specifically: creation,
@@ -24,4 +26,43 @@ trait LifecycleFactory[A] {
   def isHealthy(item: A): Boolean
 }
 
+/**
+ * A LifecycleFactory wrapper that temporarily caches items from the
+ * underlying one, up to the given timeout amount of time. Note that
+ * cache cleanup is clocked at the same duration, and so the
+ * timekeeping is sloppy: an item may indeed be persisted for any time
+ * in the range [timeout, 2 x timeout].
+ */
+class CachingLifecycleFactory[A](
+    underlying: LifecycleFactory[A],
+    timeout: Duration,
+    timer: com.twitter.util.Timer = Timer.default)
+  extends LifecycleFactory[A]
+{
+  private[this] val deathRow = Queue[(Time, A)]()
 
+  // Every `timeout', clean up deathRow.
+  timer.schedule(timeout) {
+    val now = Time.now
+    deathRow.dequeueAll { case (timestamp, _) => timestamp.until(now) > timeout }
+  }
+
+  def make(): Future[A] = synchronized {
+    while (!deathRow.isEmpty) {
+      val (_, item) = deathRow.dequeue()
+      if (isHealthy(item))
+        return Future.value(item)
+    }
+
+    underlying.make()
+  }
+
+  def dispose(item: A) = synchronized {
+    if (isHealthy(item))
+      deathRow += ((Time.now, item))
+    else
+      underlying.dispose(item)
+  }
+
+  def isHealthy(item: A) = underlying.isHealthy(item)
+}
