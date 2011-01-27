6f14791745b29c75aef2cfa0db4295716664f99c
marius a. eriksen <marius@twitter.com>
2011-01-27 15:20:57 -0800

    test prepareChannel

--- finagle-core/src/main/scala/com/twitter/finagle/Protocol.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/Protocol.scala
@@ -16,18 +16,10 @@ trait Codec[Req, Rep] {
 
 /**
  * A Protocol describes a complete protocol. Currently this is
- * specified by a Codec and a PrepareConnection.
+ * specified by a Codec and a PrepareChannel.
  */
 trait Protocol[Req, Rep] {
-  /**
-   * A PrepareConnection function is invoked to prepare the connection
-   * for requests. For example, this may be used to implement
-   * connection-oriented authentication. The function returns a
-   * Service[Req, Rep], and so it is free to wrap the underlying
-   * ChannelService[Req, Rep] for protocol specific concerns.
-   */
-  type PrepareChannel = (ChannelService[Req, Rep]) => Future[_ <: ChannelService[Req, Rep]]
-
   def codec: Codec[Req, Rep]
-  def prepareChannel: PrepareChannel = Future.value(_)
+  def prepareChannel(underlying: ChannelService[Req, Rep]) =
+    Future.value(underlying)
 }
