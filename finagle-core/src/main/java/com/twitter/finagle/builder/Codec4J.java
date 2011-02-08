39b99c758a706d2773c3e68757975cd6fecd1c68
marius a. eriksen <marius@twitter.com>
2011-02-07 18:16:45 -0800

    recover template parameters in HTTP Codec4J

--- finagle-core/src/main/java/com/twitter/finagle/builder/Codec4J.java
+++ finagle-core/src/main/java/com/twitter/finagle/builder/Codec4J.java
@@ -1,8 +1,11 @@
 package com.twitter.finagle.builder;
 
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+
 import com.twitter.finagle.Codec;
 
 public class Codec4J {
-  public static Codec Http = new Http(0);
-  public static Codec HttpWithCompression = new Http(6);
+  public static Codec<HttpRequest, HttpResponse> Http = new Http(0);
+  public static Codec<HttpRequest, HttpResponse> HttpWithCompression = new Http(6);
 }
