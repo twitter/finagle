9c3af0161f6eea8278a98b2061bfc0529c03da00
marius a. eriksen <marius@twitter.com>
2011-02-14 14:57:38 -0800

    generic tracing support: TraceContext, Transcript.

---
+++ finagle-core/src/main/scala/com/twitter/finagle/tracing/TraceContext.scala
@@ -0,0 +1,38 @@
+package com.twitter.finagle.tracing
+
+/** 
+ * Support for tracing in finagle. The main abstraction herein is the
+ * "TraceContext", which is a local that contains various metadata
+ * required for distributed tracing.
+ */ 
+
+import scala.util.Random
+
+import com.twitter.util.Local
+
+case class TraceContext(
+  var transactionID: Long,     // 64-bit transction identifier
+  var transcript: Transcript   // an associated transcript
+)
+
+// Span stuff.  Define a thrift struct for carrying this.  Blah.
+// transcript: machine identifier (IP address?)  subspans, etc?  these
+// are usually reconstructed later...  it's what we get for logging.
+
+object TraceContext {
+  private[this] val rng = new Random
+  private[this] val current = new Local[TraceContext]
+
+  def update(ctx: TraceContext) {
+    current() = ctx
+  }
+
+  def apply(): TraceContext = {
+    if (!current().isDefined)
+      current() = newContext()
+
+    current().get
+  }
+
+  def newContext() = TraceContext(rng.nextLong(), NullTranscript)
+}
---
+++ finagle-core/src/main/scala/com/twitter/finagle/tracing/Transcript.scala
@@ -0,0 +1,58 @@
+package com.twitter.finagle.tracing
+
+/** 
+ * Transcripts are records of events and are contained in a trace
+ * context.
+ */ 
+
+import collection.mutable.ArrayBuffer
+import com.twitter.util.Time
+
+case class Record(
+  host: Int,         // 32-bit IP address
+  timestamp: Time,   // (nanosecond granularity)
+  message: String    // an arbitrary string message
+)
+
+trait Transcript extends Iterable[Record] {
+  // Log levels?
+
+  def record(message: => String)
+  def isRecording = true
+}
+
+/**
+ * The default transcript uses the current transcript as of per trace
+ * context.
+ */
+object Transcript extends Transcript {
+  def record(message: => String) { TraceContext().transcript.record(message) }
+  def iterator = TraceContext().transcript.iterator
+  override def isRecording = TraceContext().transcript.isRecording
+}
+
+/**
+ * Sinks all messages
+ */
+object NullTranscript extends Transcript {
+  def record(message: => String) {}
+  def iterator = Iterator.empty
+  override def isRecording = false
+} 
+
+/**
+ * Buffers messages to an ArrayBuffer.
+ */
+class BufferingTranscript extends Transcript {
+  private[this] val buffer = new ArrayBuffer[Record]
+
+  def record(message: => String) = synchronized {
+    buffer += Record(Host(), Time.now, message)
+  }
+
+  def iterator = buffer.iterator
+
+  def clear() = synchronized {
+    buffer.clear()
+  }
+}
