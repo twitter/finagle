df79b5641cc6d00d3b7757dce0699debd33d6b08
marius a. eriksen <marius@twitter.com>
2011-03-31 16:53:18 -0700

    [split] better open sores.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,9 +1,22 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  override def repositories = Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
+  import scala.collection.jcl
+  val environment = jcl.Map(System.getenv())
+  def isSBTOpenTwitter =
+    environment.get("SBT_OPEN_TWITTER") map { _ == "true" } getOrElse false
+  def isSBTTwitter =
+    environment.get("SBT_TWITTER") map { _ == "true" } getOrElse false
+
+  override def repositories = if (isSBTOpenTwitter) {
+    Set("twitter.artifactory" at "http://artifactory.local.twitter.com/open-source/")
+  } else if (isSBTTwitter) {
+    Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
+  } else {
+    super.repositories ++ Seq("twitter.com" at "http://artifactory.local.twitter.com/repo")
+  }
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
-  val twitterMaven   = "twitter.com" at "http://maven.twttr.com/"
+
   val defaultProject = "com.twitter" % "standard-project" % "0.11.16-NEST"
   val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.2.0"
 }
