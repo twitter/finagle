aa2cc2ed9fffadb9b628217130e902d85e0be595
marius a. eriksen <marius@twitter.com>
2011-03-31 14:29:41 -0700

    [split] update to latest standard-project-NEST

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -4,6 +4,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   override def repositories = Set("twitter.artifactory" at "http://artifactory.local.twitter.com/repo/")
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
   val twitterMaven   = "twitter.com" at "http://maven.twttr.com/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.11.15-SNAPSHOT"
+  val defaultProject = "com.twitter" % "standard-project" % "0.11.15-NEST"
   val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.2.0"
 }
