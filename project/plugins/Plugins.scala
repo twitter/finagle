336af48ccc58abeb5fb6c6fdee726ac15e8328c3
William Morgan <william@twitter.com>
2011-04-14 16:43:57 -0700

    [split] bump sbt-thrift dep in finagle

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -16,5 +16,5 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
   val defaultProject = "com.twitter" % "standard-project" % "0.11.37-NEST"
-  val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.2.0"
+  val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.3.10"
 }
