310abe4759f51784f21d0ea99a47dc9d60690144
marius a. eriksen <marius@twitter.com>
2011-04-15 10:59:29 -0700

    [split] update standard-project.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -15,6 +15,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   }
   override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
 
-  val defaultProject = "com.twitter" % "standard-project" % "0.11.37-NEST"
+  val defaultProject = "com.twitter" % "standard-project" % "0.11.38-NEST"
   val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.3.10"
 }
