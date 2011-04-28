5984e1b32d89cdc7959e2b156fe8d49a1b1e09da
marius a. eriksen <marius@twitter.com>
2011-04-28 15:27:49 -0700

    [split] Add opensource-friendly Plugins.scala to util & finagle; publish & update versions.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,6 +5,7 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val environment = jcl.Map(System.getenv())
   def isSBTOpenTwitter = environment.get("SBT_OPEN_TWITTER").isDefined
   def isSBTTwitter = environment.get("SBT_TWITTER").isDefined
+  def isInternal = isSBTOpenTwitter || isSBTTwitter
 
   override def repositories = if (isSBTOpenTwitter) {
     Set("twitter.artifactory" at "http://artifactory.local.twitter.com/open-source/")
@@ -13,7 +14,11 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   } else {
     super.repositories ++ Seq("twitter.com" at "http://maven.twttr.com/")
   }
-  override def ivyRepositories = Seq(Resolver.defaultLocal(None)) ++ repositories
+  override def ivyRepositories =
+    if (isInternal)
+      Seq(Resolver.defaultLocal(None)) ++ repositories
+    else
+      super.ivyRepositories
 
   val defaultProject = "com.twitter" % "standard-project" % "0.12.1"
   val sbtThrift      = "com.twitter" % "sbt-thrift" % "1.4.2"
