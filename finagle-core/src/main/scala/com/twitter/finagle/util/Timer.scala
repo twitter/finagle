c2e27367ea0ffc5d6764447ec21c6a0d84d7ecc7
marius a. eriksen <marius@twitter.com>
2011-05-05 13:42:10 -0700

    [split] * util/../Timer.scala: add `ThreadStoppingTimer`, allowing for stopping a timer in a threadpool. this allows for the user case where we must guarantee that a timer is not stopped in its own thread
    * finagle/../Timer.scala: use `ThreadStoppingTimer` with a fixed
    threadpool of size 1 to stop reference counted timers. also clean up
    the timer code quite a big.

--- finagle-core/src/main/scala/com/twitter/finagle/util/Timer.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/util/Timer.scala
@@ -2,64 +2,96 @@ package com.twitter.finagle.util
 
 import collection.mutable.HashSet
 
-import java.util.concurrent.TimeUnit
-import com.twitter.util.{Time, Duration, TimerTask, ReferenceCountedTimer}
 import org.jboss.netty.util.{HashedWheelTimer, Timeout}
 
+import java.util.concurrent.{TimeUnit, Executors}
+import java.util.concurrent.atomic.AtomicReference
+import com.twitter.util.{
+  Time, Duration, TimerTask,
+  ReferenceCountedTimer, ThreadStoppingTimer}
+import com.twitter.concurrent.NamedPoolThreadFactory
+
 private[finagle] object Timer {
+  private[this] val timerStoppingExecutor = Executors.newFixedThreadPool(
+    1, new NamedPoolThreadFactory("FINAGLE-TIMER-STOPPER", true/*daemon*/))
+
   // This timer should only be used inside the context of finagle,
   // since it requires explicit reference count management. (Via the
-  // builder routines.)
-  implicit val default =
-    new ReferenceCountedTimer(() =>
-      new Timer(new HashedWheelTimer(10, TimeUnit.MILLISECONDS)))
+  // builder routines.) We jump through a ridiculous number of hoops
+  // here to make this work reliably:
+  //
+  //   Reference counting for resource management within finagle (so
+  //   we can actually quit processes.)
+  //
+  //   `ThreadStoppingTimer` to ensure that timer threads themselves
+  //   can shut down reliably (they cannot be shut down from the timer
+  //   threads themselves.)
+  implicit val default = {
+    def factory() = {
+      val underlying = new Timer(new HashedWheelTimer(10, TimeUnit.MILLISECONDS))
+      new ThreadStoppingTimer(underlying, timerStoppingExecutor)
+    }
 
-  val defaultNettyTimer = new org.jboss.netty.util.Timer {
-    private[this] val theTimer = this
-    private[this] val timeouts = new HashSet[Timeout]
+    new ReferenceCountedTimer(factory)
+  }
 
-    def newTimeout(task: org.jboss.netty.util.TimerTask, delay: Long, unit: TimeUnit) = {
-      val timeout = new Timeout {
-        private[this] var _isCancelled = false
-        private[this] var _isExpired = false
+  // Spin off a thread to close.
+  val defaultNettyTimer = new TimerToNettyTimer(default)
+}
 
-        theTimer.synchronized { timeouts += this }
+/**
+ * Implements the Netty timer interface on top of `default`, which is
+ * reference counted. This allows us to provide a reference counted
+ * (via cancellation) timer to Netty classes that need it. This may be
+ * slightly confusing since `default` in turn is implemented on top of
+ * a Netty timer.
+ */
+private[finagle] class TimerToNettyTimer(underlying: ReferenceCountedTimer)
+  extends org.jboss.netty.util.Timer
+{
+  private[this] object State extends Enumeration {
+    type State = Value
+    val Pending, Expired, Cancelled = Value
+  }
 
-        def cancel() = synchronized { _isCancelled = true; run() }
-        def isCancelled = synchronized { _isCancelled }
-
-        def getTask() = task
-        def getTimer = theTimer
-
-        def isExpired = _isExpired
-
-        def run(): Unit = synchronized {
-          if (_isExpired)
-            return
-
-          theTimer.synchronized { timeouts -= this }
-          _isExpired = true
-          if (!isCancelled)
+  def newTimeout(task: org.jboss.netty.util.TimerTask, delay: Long, unit: TimeUnit) = {
+    val timeout = new Timeout {
+      import State._
+      private[this] val state = new AtomicReference[State](Pending)
+      private[this] def transition(targetState: State) {
+        if (state.compareAndSet(Pending, targetState)) {
+          if (targetState == Expired)
             task.run(this)
-          default.stop()
+          underlying.stop()
         }
       }
 
-      default.acquire()
-      default.schedule(Time.now + Duration.fromTimeUnit(delay, unit)) {
-        timeout.run()
-      }
-
-      timeout
+      def cancel()    = transition(Cancelled)
+      def isCancelled = state.get == Cancelled
+      def getTask()   = task
+      def getTimer    = TimerToNettyTimer.this
+      def isExpired   = state.get == Expired || isCancelled
+      def run()       = transition(Expired)
     }
 
-    def stop() = {
-      throw new Exception(
-        "stop() has not been implemented for the wrapped netty timer.")
+    underlying.acquire()
+    underlying.schedule(Time.now + Duration.fromTimeUnit(delay, unit)) {
+      timeout.run()
     }
+
+    timeout
+  }
+
+  def stop() = {
+    throw new Exception(
+      "stop() has not been implemented for the wrapped netty timer.")
   }
 }
 
+/**
+ * Implements a [[com.twitter.util.Timer]] in terms of a
+ * [[org.jboss.netty.util.Timer]].
+ */
 class Timer(underlying: org.jboss.netty.util.Timer) extends com.twitter.util.Timer
 {
   def schedule(when: Time)(f: => Unit): TimerTask = {
@@ -85,4 +117,3 @@ class Timer(underlying: org.jboss.netty.util.Timer) extends com.twitter.util.Tim
     def cancel() { task.cancel() }
   }
 }
-
