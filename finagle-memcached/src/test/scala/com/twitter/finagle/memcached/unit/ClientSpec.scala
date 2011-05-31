67f35f1564a4d233c72c9c0f1f92eb066fd76e88
Glen D Sanford <glen@twitter.com>
2011-05-31 16:37:07 -0700

    [split] add release method to memcache Client trait, to release the underlying service(s). implement in ConnectedClient, KetamaClient, and RubyClient.
    also, call this new release method from Passbird's finagle memcache wrapper on shutdown.
    
    Squashed commit of the following:
    
    commit cd895bf99225e3b8a031036cd5c0ee9692295f74
    Merge: 5f4c527 022e8f0
    Author: Glen D Sanford <glen@twitter.com>
    Date:   Tue May 31 16:14:20 2011 -0700
    
        Merge branch 'master' into finagle_memcached_release
    
    commit 5f4c527e3c97f0ab4a411a941bd54efe7348e261
    Author: Glen D Sanford <glen@twitter.com>
    Date:   Thu May 26 17:57:13 2011 -0700
    
        add release method to memcache client, call release from passbird memcache wrapper

--- finagle-memcached/src/test/scala/com/twitter/finagle/memcached/unit/ClientSpec.scala
+++ finagle-memcached/src/test/scala/com/twitter/finagle/memcached/unit/ClientSpec.scala
@@ -7,41 +7,40 @@ import org.specs.Specification
 import scala.collection.mutable
 import _root_.java.io.{BufferedReader, InputStreamReader}
 
-
 object ClientSpec extends Specification with Mockito {
   "KetamaClient" should {
+    // Test from Smile's KetamaNodeLocatorSpec.scala
+
+    // Load known good results (key, hash(?), continuum ceiling(?), IP)
+    val stream = getClass.getClassLoader.getResourceAsStream("ketama_results")
+    val reader = new BufferedReader(new InputStreamReader(stream))
+    val expected = new mutable.ListBuffer[Array[String]]
+    var line: String = null
+    do {
+      line = reader.readLine
+      if (line != null) {
+        val segments = line.split(" ")
+        segments.length mustEqual 4
+        expected += segments
+      }
+    } while (line != null)
+    expected.size mustEqual 99
+
+    // Build Ketama client
+    val client = mock[Client]
+    val clients = Map(
+      ("10.0.1.1", 11211, 600)  -> mock[Client],
+      ("10.0.1.2", 11211, 300)  -> mock[Client],
+      ("10.0.1.3", 11211, 200)  -> mock[Client],
+      ("10.0.1.4", 11211, 350)  -> mock[Client],
+      ("10.0.1.5", 11211, 1000) -> mock[Client],
+      ("10.0.1.6", 11211, 800)  -> mock[Client],
+      ("10.0.1.7", 11211, 950)  -> mock[Client],
+      ("10.0.1.8", 11211, 100)  -> mock[Client]
+    )
+    val ketamaClient = new KetamaClient(clients, KeyHasher.KETAMA)
+
     "pick the correct node" in {
-      // Test from Smile's KetamaNodeLocatorSpec.scala
-
-      // Load known good results (key, hash(?), continuum ceiling(?), IP)
-      val stream = getClass.getClassLoader.getResourceAsStream("ketama_results")
-      val reader = new BufferedReader(new InputStreamReader(stream))
-      val expected = new mutable.ListBuffer[Array[String]]
-      var line: String = null
-      do {
-        line = reader.readLine
-        if (line != null) {
-          val segments = line.split(" ")
-          segments.length mustEqual 4
-          expected += segments
-        }
-      } while (line != null)
-      expected.size mustEqual 99
-
-      // Build Ketama client
-      val client = mock[Client]
-      val clients = Map(
-        ("10.0.1.1", 11211, 600)  -> mock[Client],
-        ("10.0.1.2", 11211, 300)  -> mock[Client],
-        ("10.0.1.3", 11211, 200)  -> mock[Client],
-        ("10.0.1.4", 11211, 350)  -> mock[Client],
-        ("10.0.1.5", 11211, 1000) -> mock[Client],
-        ("10.0.1.6", 11211, 800)  -> mock[Client],
-        ("10.0.1.7", 11211, 950)  -> mock[Client],
-        ("10.0.1.8", 11211, 100)  -> mock[Client]
-      )
-      val ketamaClient = new KetamaClient(clients, KeyHasher.KETAMA)
-
       // Test that ketamaClient.clientOf(key) == expected IP
       val mockClientToIp = clients.map { case (k,v) => v -> k._1 }
       for (testcase <- expected) {
@@ -50,20 +49,32 @@ object ClientSpec extends Specification with Mockito {
         resultIp must be_==(testcase(3))
       }
     }
+    "release" in {
+      ketamaClient.release()
+      clients.values foreach { client =>
+        there was one(client).release()
+      }
+    }
   }
 
   "RubyMemCacheClient" should {
-    "pick the correct node" in {
-      val client1 = mock[Client]
-      val client2 = mock[Client]
-      val client3 = mock[Client]
+    val client1 = mock[Client]
+    val client2 = mock[Client]
+    val client3 = mock[Client]
+    val rubyMemCacheClient = new RubyMemCacheClient(Seq(client1, client2, client3))
 
-      val rubyMemCacheClient = new RubyMemCacheClient(Seq(client1, client2, client3))
+    "pick the correct node" in {
       rubyMemCacheClient.clientOf("apple")    must be_==(client1)
       rubyMemCacheClient.clientOf("banana")   must be_==(client2)
       rubyMemCacheClient.clientOf("cow")      must be_==(client1)
       rubyMemCacheClient.clientOf("dog")      must be_==(client1)
       rubyMemCacheClient.clientOf("elephant") must be_==(client3)
+    }
+    "release" in {
+      rubyMemCacheClient.release()
+      there was one(client1).release()
+      there was one(client2).release()
+      there was one(client3).release()
     }
   }
 }
