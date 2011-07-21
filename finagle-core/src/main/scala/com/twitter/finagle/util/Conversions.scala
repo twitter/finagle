f76b9a428f82144cc014673f4e42ec0f18b811ad
marius a. eriksen <marius@twitter.com>
2011-07-21 14:42:01 -0700

    [split] ChannelServiceFactory: encode exceptions from bootstrap.connect()

--- finagle-core/src/main/scala/com/twitter/finagle/util/Conversions.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/util/Conversions.scala
@@ -6,5 +6,4 @@ import com.twitter.util.Future
 
 object Conversions {
   implicit def channelFutureToRichChannelFuture(f: ChannelFuture) = new RichChannelFuture(f)
-  implicit def futureToRichFuture[A](f: Future[A]) = new RichFuture(f)
 }
