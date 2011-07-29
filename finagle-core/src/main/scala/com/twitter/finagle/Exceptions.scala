4e7e1f37d7bedb840ccc55f41381f9dc42f5e027
marius a. eriksen <marius@twitter.com>
2011-07-28 17:02:28 -0700

    [split] finagle: convert WriteException to a case class
    this makes it is pattern-matcheable and the underlying exception is
    available.
    
    github issue 25: https://github.com/twitter/finagle/issues/25

--- finagle-core/src/main/scala/com/twitter/finagle/Exceptions.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/Exceptions.scala
@@ -24,7 +24,7 @@ class WriteTimedOutException                extends ChannelException
 class UnknownChannelException(e: Throwable) extends ChannelException {
   override def toString = "%s: %s".format(super.toString, e.toString)
 }
-class WriteException(e: Throwable)          extends ChannelException {
+case class WriteException(e: Throwable)     extends ChannelException {
   override def toString = "%s: %s".format(super.toString, e.toString)
 }
 
