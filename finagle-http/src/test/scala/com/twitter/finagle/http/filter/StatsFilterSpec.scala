ab4c36dc3a8f864069b57d67716cb37c62fa9638
David Helder <david@twitter.com>
2012-01-30 13:10:25 -0800

    [split] Add per-endpoint stats to Macaw and Woodstar Set X-Controller-Class / X-Action-Name headers for TFE
    * macaw-core/Controller.scala:
        Add controllerName and endpointName.
        apply: On success, copy controllerName and endpointName to TFE headers
        NullController: add
    * macaw-core/StatsFilter.scala:
        Add stats based on controllerName/endpointName.  (This might be moot
          once we're behind TFE)
    * woodstar/TimelinesController.scala:
        endpointName: implement
    
    * finagle-core/StatsReceiver.scala:
        InMemoryStatsReceiver: move here from finagle-http unit tests
    
    https://reviewboard.twitter.biz/r/52641/

--- finagle-http/src/test/scala/com/twitter/finagle/http/filter/StatsFilterSpec.scala
+++ finagle-http/src/test/scala/com/twitter/finagle/http/filter/StatsFilterSpec.scala
@@ -3,52 +3,12 @@ package com.twitter.finagle.http.filter
 import com.twitter.finagle.Service
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.http.service.NullService
-import com.twitter.finagle.stats.{Counter, Gauge, Stat, StatsReceiver}
+import com.twitter.finagle.stats.InMemoryStatsReceiver
 import com.twitter.util.{Future, Time}
 import org.specs.Specification
-import scala.collection.mutable
 
 
 object StatsFilterSpec extends Specification {
-
-  class InMemoryStatsReceiver extends StatsReceiver {
-    val repr = StatsFilterSpec.this
-    val counters = mutable.Map[String, Int]()
-    val stats    = mutable.Map[String, List[Float]]()
-    val gauges   = mutable.Map[String, () => Float]()
-
-    def counter(name: String*): Counter = {
-      val fullname = name.mkString(".")
-      new Counter {
-        def incr(delta: Int) {
-          val oldValue = counters.get(fullname).getOrElse(0)
-          counters(fullname) = oldValue + delta
-        }
-      }
-    }
-
-    def stat(name: String*): Stat = {
-      val fullname = name.mkString(".")
-      new Stat {
-        def add(value: Float) {
-          val oldValue = stats.get(fullname).getOrElse(Nil)
-          stats(fullname) = oldValue :+ value
-        }
-      }
-    }
-
-    def addGauge(name: String*)(f: => Float): Gauge = {
-      val fullname = name.mkString(",")
-      val gauge = new Gauge {
-        def remove() {
-          gauges -= fullname
-        }
-      }
-      gauges += fullname -> (() => f)
-      gauge
-    }
-  }
-
   "StatsFilter" should {
     "increment stats" in {
       val receiver = new InMemoryStatsReceiver
@@ -66,11 +26,11 @@ object StatsFilterSpec extends Specification {
         filter(Request())()
       }
 
-      receiver.counters("status.404") must_== 1
-      receiver.counters("status.4XX") must_== 1
-      receiver.stats("time.404")      must_== List(0.0)
-      receiver.stats("time.4XX")      must_== List(0.0)
-      receiver.stats("response_size") must_== List(5.0)
+      receiver.counters(Seq("status", "404")) must_== 1
+      receiver.counters(Seq("status", "4XX")) must_== 1
+      receiver.stats(Seq("time", "404"))      must_== Seq(0.0)
+      receiver.stats(Seq("time", "4XX"))      must_== Seq(0.0)
+      receiver.stats(Seq("response_size"))    must_== Seq(5.0)
     }
   }
 }
