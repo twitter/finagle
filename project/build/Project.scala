bb8faece4546321dca5a3938105dc55417f647a9
Steve Gury <stevegury@twitter.com>
2012-02-02 13:14:01 -0800

    [split] Automatically import Netty Sources

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -155,7 +155,7 @@ class Project(info: ProjectInfo) extends StandardParentProject(info)
     with Defaults
   {
     override def compileOrder = CompileOrder.ScalaThenJava
-    val netty = "org.jboss.netty" %  "netty" % "3.2.7.Final"
+    val netty = "org.jboss.netty" %  "netty" % "3.2.7.Final" withSources()
 
     projectDependencies(
       "util" ~ "util-core",
