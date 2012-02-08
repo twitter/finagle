c632f0faa501102884d0bd016e11a5e06976bfe6
Wilhelm Bierbaum <wilhelm@twitter.com>
2012-02-08 10:56:54 -0800

    [split] add openssl patch; flush out script

--- finagle-native/make_tomcat_native.sh
+++ finagle-native/make_tomcat_native.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 VER="1.1.22"
 REL="tomcat-native-$VER"
 URL="http://www.apache.org/dist/tomcat/tomcat-connectors/native/$VER/source/$REL-src.tar.gz"
 TARBALL="$REL-src.tar.gz"
+SRC="$REL-src"
 
 here=`dirname $0`
 
@@ -20,6 +21,19 @@ test -f $TARBALL || \
     die "could not download tarball '$TARBALL' from '$URL'"
 
 tar zxf $TARBALL
-cd $REL
-patch < $here/tomcat-native-$VER.finagle.patch || die "patch did not apply"
+cd $SRC
+git apply < ../tomcat-native-$VER.finagle.patch || die "patch did not apply"
 cd -
+rm $TARBALL
+
+echo "$REL in $SRC has been patched to support Finagle."
+echo ""
+echo "You should follow the instructions at http://tomcat.apache.org/native-doc/ to build."
+echo ""
+echo "You will need these:"
+echo "  APR:     http://apr.apache.org/"
+echo "  OpenSSL: http://openssl.org/"
+echo ""
+echo "You need OpenSSL 1.0.1 or greater; or 1.0.0g with an SPDY NPN patch applied."
+echo "For 1.0.0g, an NPN patch is included in this directory. 'git apply' it in the 1.0.0g source tree""
+
