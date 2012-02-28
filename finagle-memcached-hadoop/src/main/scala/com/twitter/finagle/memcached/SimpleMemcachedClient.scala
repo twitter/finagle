6fbfa15818634900cc17bd9a6180e6a0984c98fa
Kyle Maxwell <kyle@twitter.com>
2012-02-28 12:45:15 -0800

    [split] fix hash scheme

--- finagle-memcached-hadoop/src/main/scala/com/twitter/finagle/memcached/SimpleMemcachedClient.scala
+++ finagle-memcached-hadoop/src/main/scala/com/twitter/finagle/memcached/SimpleMemcachedClient.scala
@@ -8,20 +8,17 @@ import com.twitter.finagle.memcached.protocol.text.Memcached
 import scala.collection.JavaConversions._
 import _root_.java.util.{List => JList}
 
-class SimpleMemcachedClientFactory(hosts: Seq[String], port: Int, hostConnectionLimit: Int = 1) extends SerializableKeyValueClientFactory {
-  def this(hosts: JList[String], port: Int, hostConnectionLimit: Int) = this(hosts.toSeq, port, hostConnectionLimit)
-  def newInstance() = new SimpleMemcachedClient(hosts, port, hostConnectionLimit)
+class SimpleMemcachedClientFactory(hosts: Seq[String], port: Int) extends SerializableKeyValueClientFactory {
+  def this(hosts: JList[String], port: Int) = this(hosts.toSeq, port)
+  def newInstance() = new SimpleMemcachedClient(hosts, port)
 }
 
-class SimpleMemcachedClient(hosts: Seq[String], port: Int, hostConnectionLimit: Int = 1) extends KeyValueClient {
+class SimpleMemcachedClient(hosts: Seq[String], port: Int) extends KeyValueClient {
   val forever = Int.MaxValue.seconds.fromNow
   val mkhosts = hosts.map{ host => host + ":" + String.valueOf(port) }.mkString(",")
-  val client = Client(
-    ClientBuilder()
-      .hosts(mkhosts)
-      .hostConnectionLimit(hostConnectionLimit)
-      .codec(new Memcached)
-      .build()).withBytes
+  val client = KetamaClientBuilder()
+          .nodes(mkhosts)
+          .build().withBytes
 
   def put(key: String, value: Array[Byte]) = {
     client.set(key, 0, forever, value)
