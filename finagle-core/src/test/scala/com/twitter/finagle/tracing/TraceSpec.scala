a1baba4dd04928278f0f97ca1f542309f3176655
Franklin Hu <franklin@twitter.com>
2012-04-23 10:23:12 -0700

    [split] finagle: annotate client connection when tracing
    - Remove "stack" semantics around the Trace class. Instead of a stack,
      the Trace class keeps a single TraceId and terminal flag to represent
      the current trace id. Stack semantics are managed inherently using
      Trace.unwind with the Future Locals.
    - Keep a list of `Tracer`s and record annotations to all `Tracer`s
      in the list since we no longer have stack semantics.
    - Deprecate Trace.pushId methods in favor of Trace.setId
    - Add ability to set the current TraceId as "terminal." This causes
      future calls to Trace.setId to do nothing, and forces all subsequent
      annotations to use the terminal id. This is useful for tightly wrapping
      calls to clients that do not have tracing support, but we would like to
      add annotations to (see cassie changes).

--- finagle-core/src/test/scala/com/twitter/finagle/tracing/TraceSpec.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/tracing/TraceSpec.scala
@@ -21,43 +21,39 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
       }
     }
 
-    "keep ids in a stack" in {
-      Trace.pushId(id0)
-      Trace.id must be_==(id0)
-      Trace.pushId(id1)
-      Trace.id must be_==(id1)
-      Trace.pushId(id2)
-      Trace.id must be_==(id2)
-
-      Trace.popId()
-      Trace.id must be_==(id1)
-      Trace.popId()
-      Trace.id must be_==(id0)
-      Trace.popId()
-      Trace.id must beLike {  // back to default
-        case TraceId(None, None, _, None) => true
-      }
-    }
-
-    "Trace.pushId" in {
-      "push a fresh id when none exist" in {
+    "Trace.set" in {
+      "set a fresh id when none exist" in {
         Trace.idOption must beNone
         val defaultId = Trace.id
-        Trace.pushId()
+        Trace.setId(Trace.nextId)
         Trace.id must be_!=(defaultId)
         Trace.id must beLike {
           case TraceId(None, None, _, None) => true
         }
       }
 
-      "push a derived id when one exists" in {
-        Trace.pushId()
+      "set a derived id when one exists" in {
+        Trace.setId(Trace.nextId)
         val topId = Trace.id
-        Trace.pushId()
+        Trace.setId(Trace.nextId)
         Trace.id must beLike {
           case TraceId(Some(traceId), Some(parentId), _, None)
           if (traceId == topId.traceId && parentId == topId.spanId) => true
         }
+      }
+
+      "not set additional terminal id" in {
+        Trace.setId(Trace.nextId, true)
+        val topId = Trace.id
+        Trace.setId(Trace.nextId, true)
+        Trace.id must be_==(topId)
+      }
+
+      "not set id when terminal id exists" in {
+        Trace.setId(Trace.nextId, true)
+        val topId = Trace.id
+        Trace.setId(Trace.nextId)
+        Trace.id must be_==(topId)
       }
     }
 
@@ -65,7 +61,7 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
       var didRun = false
       val priorId = Trace.id
       Trace.unwind {
-        Trace.pushId(id0)
+        Trace.setId(id0)
         Trace.id must be_==(id0)
         didRun = true
       }
@@ -77,22 +73,22 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
       val tracer1 = mock[Tracer]
       val tracer2 = mock[Tracer]
 
-      "report topmost id to tracers below this id" in Time.withCurrentTimeFrozen { tc =>
-        Trace.pushId(id0)
+      "report topmost id to all tracers" in Time.withCurrentTimeFrozen { tc =>
+        Trace.setId(id0)
         Trace.pushTracer(tracer1)
         val ann = Annotation.Message("hello")
         Trace.record(ann)
-        there was no(tracer1).record(any)
-        Trace.pushId(id1)
+        there was one(tracer1).record(any)
+        Trace.setId(id1)
         Trace.record(ann)
         there was one(tracer1).record(Record(id1, Time.now, ann))
         tc.advance(1.second)
-        Trace.pushId(id2)
+        Trace.setId(id2)
         Trace.record(ann)
         there was one(tracer1).record(Record(id2, Time.now, ann))
         tc.advance(1.second)
         Trace.pushTracer(tracer2)
-        Trace.pushId(id0)
+        Trace.setId(id0)
         Trace.record(ann)
         there was one(tracer1).record(Record(id0, Time.now, ann))
         there was one(tracer2).record(Record(id0, Time.now, ann))
@@ -100,7 +96,7 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
 
       "record IDs not in the stack to all tracers" in {
         Trace.pushTracer(tracer1)
-        Trace.pushId(id0)
+        Trace.setId(id0)
         Trace.pushTracer(tracer2)
         val rec1 = Record(id1, Time.now, Annotation.Message("wtf"))
         Trace.record(rec1)
@@ -109,28 +105,16 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
         val rec0 = Record(id0, Time.now, Annotation.Message("wtf0"))
         Trace.record(rec0)
         there was one(tracer1).record(rec0)
-        there was no(tracer2).record(rec0)
+        there was one(tracer2).record(rec0)
       }
 
       "record binary annotations" in Time.withCurrentTimeFrozen { tc  =>
         Trace.pushTracer(tracer1)
-        Trace.pushId(id0)
+        Trace.setId(id0)
         val rec1 = Record(id0, Time.now,
           Annotation.BinaryAnnotation("key", "test"))
         Trace.recordBinary("key", "test")
         there was one(tracer1).record(rec1)
-      }
-
-      "report to each unique tracer exactly once" in {
-        Trace.pushTracer(tracer1)
-        Trace.pushTracer(tracer2)
-        Trace.pushTracer(tracer1)
-        Trace.pushId(id0)
-        there was no(tracer1).record(any)
-        there was no(tracer2).record(any)
-        Trace.record("oh hey")
-        there was one(tracer1).record(any)
-        there was one(tracer2).record(any)
       }
 
       "not report when tracing turned off" in {
@@ -138,7 +122,7 @@ class TraceSpec extends SpecificationWithJUnit with Mockito {
           Trace.disable
           Trace.pushTracer(tracer1)
           Trace.pushTracer(tracer2)
-          Trace.pushId(id0)
+          Trace.setId(id0)
           there was no(tracer1).record(any)
           there was no(tracer2).record(any)
           Trace.record("oh hey")
