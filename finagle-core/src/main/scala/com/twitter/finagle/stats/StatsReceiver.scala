81e6160c3ca0161182af21436180a68e6cd3cc6d
Kevin Oliver <koliver@twitter.com>
2012-04-30 10:08:29 -0700

    [split] Add Duration.inUnit and expose that in StatsReciever.time and StatsReciever.timeFuture
    RB_ID=62891

--- finagle-core/src/main/scala/com/twitter/finagle/stats/StatsReceiver.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/stats/StatsReceiver.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable.{HashMap, SynchronizedMap, WeakHashMap}
 import scala.ref.WeakReference
 
 import com.twitter.util.{Future, Time, JavaSingleton}
+import java.util.concurrent.TimeUnit
 
 /**
  * A writeable Counter. Only sums are kept of Counters.  An example
@@ -38,23 +39,37 @@ trait StatsReceiver {
   val repr: AnyRef
 
   /**
+   * Time a given function using the given TimeUnit
+   */
+  def time[T](unit: TimeUnit, name: String*)(f: => T): T = {
+    val start = Time.now
+    val result = f
+    stat(name: _*).add((Time.now - start).inUnit(unit))
+    result
+  }
+
+  /**
    * Time a given function in milliseconds
    */
   def time[T](name: String*)(f: => T): T = {
+    time(TimeUnit.MILLISECONDS, name: _*)(f)
+  }
+
+  /**
+   * Time a given future using the given TimeUnit
+   */
+  def timeFuture[T](unit: TimeUnit, name: String*)(f: => Future[T]): Future[T] = {
     val start = Time.now
-    val result = f
-    stat(name: _*).add((Time.now - start).inMilliseconds)
-    result
+    f ensure {
+      stat(name: _*).add((Time.now - start).inUnit(unit))
+    }
   }
 
   /**
    * Time a given future in milliseconds
    */
   def timeFuture[T](name: String*)(f: => Future[T]): Future[T] = {
-    val start = Time.now
-    f ensure {
-      stat(name: _*).add((Time.now - start).inMilliseconds)
-    }
+    timeFuture(TimeUnit.MILLISECONDS, name: _*)(f)
   }
 
   /**
