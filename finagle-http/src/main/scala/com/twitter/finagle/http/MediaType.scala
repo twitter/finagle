bc27fb63fa2452f3c5521012bcfdd6b7aebc5057
Julio Capote <julio@twitter.com>
2012-06-04 11:09:21 -0700

    [split] Ports monorail's request.client_id to macaw-core This ports the request.client_id method (and subsequently needed methods) from the monorail to macaw-core. I've tried to keep the names as similar as possible for ease of referencing.
    Methods ported:
    
    AuthenticatedSystem#client_id
    MobileAdditions#is_mobile?
    MobileAdditions#m2_request?
    MobileAdditions#mobile_user_agent?
    MobileAdditions#phoenix_incompatible?
    
    RB_ID=66910

--- finagle-http/src/main/scala/com/twitter/finagle/http/MediaType.scala
+++ finagle-http/src/main/scala/com/twitter/finagle/http/MediaType.scala
@@ -4,6 +4,7 @@ object MediaType {
   // Common media types
   val Atom        = "application/atom+xml"
   val Html        = "text/html"
+  val Iframe      = "application/iframe"
   val Javascript  = "application/javascript"
   val Json        = "application/json"
   val OctetStream = "application/octet-stream"
