07f431b073745906ce87b63752b8fbcd6ee7852d
Mairbek Khadikov <mkhadikov@gmail.com>
2012-12-15 21:29:45 +0200

    Formatting

--- src/test/scala/com/twitter/finagle/postgres/protocol/UtilsSpec.scala
+++ src/test/scala/com/twitter/finagle/postgres/protocol/UtilsSpec.scala
@@ -9,14 +9,14 @@ import org.specs2.matcher.DataTables
 
 @RunWith(classOf[JUnitRunner])
 class UtilsSpec extends Specification with DataTables {
-  
+
   "Buffers readCString" should {
-    
+
     def newBuffer(): (ChannelBuffer, String, String) = {
       val str = "Some string"
       val cStr = str + '\0'
       val buffer = ChannelBuffers.copiedBuffer(cStr, Charsets.Utf8)
-      (buffer, str, cStr) 
+      (buffer, str, cStr)
     }
 
     "fully read a string" in {
@@ -33,7 +33,7 @@ class UtilsSpec extends Specification with DataTables {
 
       buffer.readerIndex() === cStr.length
     }
-    
+
     "respect initial reader index" in {
       val (buffer, str, cStr) = newBuffer
       buffer.readChar()
@@ -45,38 +45,38 @@ class UtilsSpec extends Specification with DataTables {
 
     "throw appropriate exception if string passed is not C style" in {
       val bufferWithWrongString = ChannelBuffers.copiedBuffer("not a C style string", Charsets.Utf8)
-      
+
       Buffers.readCString(bufferWithWrongString) must throwAn[IndexOutOfBoundsException]
     }
-  }  
-  
+  }
+
   "Md5Encryptor encrypt" should {
-    
+
     def ba(str: String) = str.getBytes()
-    
+
     "encrypt everything right" ! samples
-    
-    def samples = 
-      "user"        | "password"         | "salt"                      | "result"                              |
-      ba("john")    ! ba("john25")       ! Array[Byte] (1, 2, 3, 4)    ! "md5305d62541687fa0c5871edfdb1140133" |
-      ba("john")    ! ba("john25")       ! Array[Byte] (4, 3, 1, 2)    ! "md5156cf720128cad07c39e018eca91ff8d" |
-      ba("john")    ! ba("john22")       ! Array[Byte] (1, 2, 3, 4)    ! "md57042902d6531e1840b3019a880f66edc" |
-      ba("lomack")  ! ba("lowmuck245$3") ! Array[Byte] (15, 19, 33, 1) ! "md56aa29016af76de6b793f3e7e009a26c2" |> {
-      (user, password, salt, result) => new String(Md5Encryptor.encrypt(user, password, salt)) must_== result  
-    }
-      
+
+    def samples =
+      "user" | "password" | "salt" | "result" |
+        ba("john") ! ba("john25") ! Array[Byte](1, 2, 3, 4) ! "md5305d62541687fa0c5871edfdb1140133" |
+        ba("john") ! ba("john25") ! Array[Byte](4, 3, 1, 2) ! "md5156cf720128cad07c39e018eca91ff8d" |
+        ba("john") ! ba("john22") ! Array[Byte](1, 2, 3, 4) ! "md57042902d6531e1840b3019a880f66edc" |
+        ba("lomack") ! ba("lowmuck245$3") ! Array[Byte](15, 19, 33, 1) ! "md56aa29016af76de6b793f3e7e009a26c2" |> {
+        (user, password, salt, result) => new String(Md5Encryptor.encrypt(user, password, salt)) must_== result
+      }
+
     "throw exception if any of the parameters is missing or empty" in {
       val user = ba("john")
       val password = ba("john25")
-      val salt = Array[Byte] (1, 2, 3, 4)
+      val salt = Array[Byte](1, 2, 3, 4)
       val empty = Array[Byte]()
- 
+
       Md5Encryptor.encrypt(empty, password, salt) must throwAn[IllegalArgumentException]
-	  Md5Encryptor.encrypt(null, password, salt) must throwAn[IllegalArgumentException]
+      Md5Encryptor.encrypt(null, password, salt) must throwAn[IllegalArgumentException]
       Md5Encryptor.encrypt(user, empty, salt) must throwAn[IllegalArgumentException]
-	  Md5Encryptor.encrypt(user, null, salt) must throwAn[IllegalArgumentException]
+      Md5Encryptor.encrypt(user, null, salt) must throwAn[IllegalArgumentException]
       Md5Encryptor.encrypt(user, password, empty) must throwAn[IllegalArgumentException]
       Md5Encryptor.encrypt(user, password, null) must throwAn[IllegalArgumentException]
-    }  
+    }
   }
 }
