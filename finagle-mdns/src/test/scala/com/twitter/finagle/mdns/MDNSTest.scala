0ca58bf10e29db9481a72603de1a2576a18bb70c
Jeff Smick <jeff@twitter.com>
2013-02-22 18:58:30 +0000

    [split]  add tests to ensure resolvers resolve
    RB_ID=126659

--- finagle-mdns/src/test/scala/com/twitter/finagle/mdns/MDNSTest.scala
+++ finagle-mdns/src/test/scala/com/twitter/finagle/mdns/MDNSTest.scala
@@ -1,5 +1,6 @@
 package com.twitter.finagle.mdns
 
+import com.twitter.finagle.{Announcer, Resolver}
 import com.twitter.util.{Await, RandomSocket}
 import java.net.InetSocketAddress
 import org.junit.runner.RunWith
@@ -10,7 +11,7 @@ import org.scalatest.time.SpanSugar._
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
 @RunWith(classOf[JUnitRunner])
-class MdnsSpec extends FunSuite with BeforeAndAfter {
+class MdnsTest extends FunSuite with BeforeAndAfter {
   test("bind locally") {
     val addr = RandomSocket()
     val resolver = new MDNSResolver
@@ -25,5 +26,16 @@ class MdnsSpec extends FunSuite with BeforeAndAfter {
     } finally {
       Await.ready(announcement.close())
     }
+  }
+
+  test("resolve via the main resolver") {
+    assert(Resolver.resolve("mdns!_foo._tcp.local.").isReturn)
+    assert(Resolver.resolve("local!foo").isReturn)
+  }
+
+  test("announce via the main announcer") {
+    val sock = RandomSocket()
+    assert(Await.ready(Announcer.announce(sock, "local!foo")).isReturn)
+    assert(Await.ready(Announcer.announce(sock, "mdns!" + sock.getPort + "._foo._tcp.local.")).isReturn)
   }
 }
---
+++ finagle-serversets/src/test/scala/com/twitter/finagle/zookeeper/ZkResolverTest.scala
@@ -0,0 +1,87 @@
+package com.twitter.finagle.zookeeper
+
+
+import org.scalatest.concurrent.Eventually._
+import org.scalatest.concurrent.Timeouts._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import com.twitter.common.zookeeper.ServerSetImpl
+import com.twitter.finagle.Resolver
+import com.twitter.thrift.Status._
+import com.twitter.util.Await
+import scala.collection.JavaConverters._
+import java.net.InetSocketAddress
+
+@RunWith(classOf[JUnitRunner])
+class ZkResolverTest extends FunSuite with BeforeAndAfter {
+  val inst = new ZkInstance
+  import inst._
+  before(start())
+  after(stop())
+
+  test("represent the underlying ServerSet") {
+    val serverSet = new ServerSetImpl(zookeeperClient, "/foo/bar/baz")
+    val clust = new ZkGroup(serverSet, "/foo/bar/baz")
+    assert(clust().isEmpty)
+
+    val status8080 = serverSet.join(
+      new InetSocketAddress(8080),
+      Map[String, InetSocketAddress]().asJava, ALIVE)
+
+    eventually { assert(clust().size == 1) }
+    val ep = clust().head.getServiceEndpoint
+    assert(ep.getHost == "0.0.0.0")
+    assert(ep.getPort == 8080)
+
+    assert(clust() === clust())
+    val snap = clust()
+
+    val status8081 = serverSet.join(
+      new InetSocketAddress(8081),
+      Map[String, InetSocketAddress]().asJava, ALIVE)
+
+    eventually { assert(clust().size == 2) }
+    assert {
+      val Seq(fst) = (clust() &~ snap).toSeq
+      fst.getServiceEndpoint.getPort == 8081
+    }
+  }
+
+  test("resolve ALIVE endpoints") {
+    val res = new ZkResolver
+    val clust = res.resolve("localhost:%d!/foo/bar/baz".format(zookeeperAddress.getPort))()
+    assert(clust().isEmpty)
+    val inetClust = clust collect { case ia: InetSocketAddress => ia }
+    assert(inetClust() === inetClust())
+    val serverSet = new ServerSetImpl(zookeeperClient, "/foo/bar/baz")
+    val addr = new InetSocketAddress("127.0.0.1", 8080)
+    val blahAddr = new InetSocketAddress("10.0.0.1", 80)
+    val status8080 = serverSet.join(
+      addr,
+      Map[String, InetSocketAddress]("blah" -> blahAddr).asJava, ALIVE)
+    eventually { assert(inetClust().size == 1) }
+    assert {
+      val Seq(addr1) = inetClust().toSeq
+      addr == addr1
+    }
+    status8080.leave()
+    eventually { assert(inetClust().isEmpty) }
+    serverSet.join(
+      addr,
+      Map[String, InetSocketAddress]("blah" -> blahAddr).asJava, ALIVE)
+    eventually { assert(inetClust().size == 1) }
+
+    val blahClust = res.resolve("localhost:%d!/foo/bar/baz!blah".format(zookeeperAddress.getPort))()
+    eventually { assert(blahClust().size == 1) }
+    assert(blahClust() === blahClust())
+    assert {
+      val Seq(addr1) = blahClust().toSeq
+      addr1 == blahAddr
+    }
+  }
+
+  test("resolves from the main resolver") {
+    assert(Resolver.resolve("zk!localhost:%d!/foo/bar/baz!blah".format(zookeeperAddress.getPort)).isReturn)
+  }
+}
