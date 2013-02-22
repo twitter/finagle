2cc8a4e5eb8784adeed7cc74fd4fb0cdd91834f5
marius a. eriksen <marius@twitter.com>
2013-02-22 17:59:26 +0000

    [split]  finagle: a simple landing page
    RB_ID=126461

---
+++ pushsite.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+dir=/tmp/finagle.$$
+trap "rm -fr $dir" 0 1 2
+
+echo 'making site...' 1>&2
+./sbt finagle-doc/make-site >/dev/null 2>&1
+
+echo 'making unidoc...' 1>&2
+./sbt unidoc >/dev/null 2>&1
+
+echo 'cloning...' 1>&2
+git clone -b gh-pages --single-branch git@github.com:twitter/finagle.git $dir >/dev/null 2>&1
+
+savedir=$(pwd)
+cd $dir
+git rm -fr .
+cp $savedir/site/index.html .
+cp -r $savedir/target/scala-2.9.2/unidoc/ docs
+cp -r $savedir/doc/target/site guide
+git add -f .
+git commit -am"site push by $(whoami)"
+git push origin gh-pages:gh-pages
