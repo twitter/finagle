a47ed15c82e72c15534af60564857c76c3374e56
Alex Yarmula <ak@twitter.com>
2013-04-18 21:47:41 +0000

    [split]  Add Deploytracker to birdcage master This is Deploytracker, a project that lives up to its name. There are docs under go/dt-docs and live version on go/dt.
    RB_ID=140991

--- finagle-mysql/src/main/scala/com/twitter/finagle/mysql/util/BufferUtil.scala
+++ finagle-mysql/src/main/scala/com/twitter/finagle/mysql/util/BufferUtil.scala
@@ -4,14 +4,17 @@ object BufferUtil {
   /**
    * Helper method to do a hex dump of a sequence of bytes.
    */
-  def hex(data: Seq[Byte], output: String = ""): String = {
+  def hex(data: Seq[Byte], output: StringBuilder = new
+StringBuilder()): String = {
     val (begin,end) = data.splitAt(16)
     val hexline = begin.map { "%02X".format(_) } mkString(" ")
-    val charline = begin.map { b => if (0x20 <= b && b <= 0x7E) b.toChar else " " } mkString("")
+    val charline = begin.map { b => if (0x20 <= b && b <= 0x7E)
+b.toChar else " " } mkString("")
     val line = "%-47s".format(hexline) + "     " + charline
+    val res = output ++= line ++= "\n"
     if (end.isEmpty)
-      output + line + "\n"
+      res.toString
     else
-      hex(end, output + line + "\n")
+      hex(end, res)
   }
 }
