d03f9f9f6c33c559ee88a2f7194efaca9f887ffa
Bhaskar Maddala <bhaskar@tumblr.com>
2013-04-29 12:22:45 -0400

    The ServerBuilder API in finagle provides options for specifying settings (a) channelMaxLifeTime (b) channelMaxIdleDuration and (c) OpenConnectionsThresholds.
    Code that implemented these settings was removed as part of the refactorings
    
    * https://github.com/twitter/finagle/commit/04f4d25ddbef0ba01569fe1e117295fe54234c2c
    * https://github.com/twitter/finagle/commit/d92fedf49df
    
    This change reintroduces these settings.

--- finagle-core/src/main/scala/com/twitter/finagle/netty3/server.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/netty3/server.scala
@@ -148,10 +148,6 @@ object Netty3Listener {
  * @param bootstrapOptions Additional options for Netty's
  * `ServerBootstrap`
  *
- * @param channelMaxIdleTime The maximum idle time of any channel.
- *
- * @param channelMaxLifeTime The maximum life time of any channel.
- *
  * @param channelReadTimeout Channels are given this much time to
  * read a request.
  *
@@ -170,8 +166,6 @@ case class Netty3Listener[In, Out](
     "reuseAddress" -> java.lang.Boolean.TRUE,
     "child.tcpNoDelay" -> java.lang.Boolean.TRUE
   ),
-  channelMaxIdleTime: Duration = Duration.Top,
-  channelMaxLifeTime: Duration = Duration.Top,
   channelReadTimeout: Duration = Duration.Top,
   channelWriteCompletionTimeout: Duration = Duration.Top,
   tlsConfig: Option[Netty3ListenerTLSConfig] = None,
