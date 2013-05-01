7381a5182a8a7bdbbbbff9415ddb34dbea9aae6d
Bhaskar Maddala <bhaskar@tumblr.com>
2013-05-01 18:45:11 -0400

    Dont forget to remove the connection from the queue

--- finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoClient.scala
+++ finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoClient.scala
@@ -4,24 +4,25 @@ import com.twitter.finagle.builder.ClientBuilder
 import java.net.InetSocketAddress
 import com.twitter.finagle.Service
 
-object EchoClient {
-  def main(args: Array[String]) {
+class EchoClient(id: Int) {
     // Construct a client, and connect it to localhost:8080
-    val client: Service[String, String] = ClientBuilder()
-      .codec(StringCodec)
-      .hosts(new InetSocketAddress(8080))
-      .hostConnectionLimit(1)
-      .build()
+  val client: Service[String, String] = ClientBuilder()
+    .codec(StringCodec)
+    .hosts(new InetSocketAddress(8090))
+    .hostConnectionLimit(1)
+    .keepAlive(true)
+    .build()
 
+  def apply(msg: String) {
     // Issue a newline-delimited request, respond to the result
     // asynchronously:
-    client("hi mom\n") onSuccess { result =>
-      println("Received result asynchronously: " + result)
+    client(msg) onSuccess { result =>
+      println("Received result asynchronously: " + result + " on " + id)
     } onFailure { error =>
       error.printStackTrace()
     } ensure {
       // All done! Close TCP connection(s):
-      client.close()
+      //client.close()
     }
   }
 }
--- finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoServer.scala
+++ finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoServer.scala
@@ -1,26 +1,40 @@
 package com.twitter.finagle.example.echo
 
+import com.twitter.conversions.time._
 import com.twitter.finagle.Service
 import com.twitter.util.Future
 import java.net.InetSocketAddress
 import com.twitter.finagle.builder.{Server, ServerBuilder}
+import com.twitter.finagle.channel.OpenConnectionsThresholds
+import com.twitter.util.{Future, CountDownLatch}
 
 object EchoServer {
-  def main(args: Array[String]) {
+  //var latch = new CountDownLatch(1)
+  def start() = {
     /**
      * A very simple service that simply echos its request back
      * as a response. Note that it returns a Future, since everything
      * in Finagle is asynchronous.
      */
     val service = new Service[String, String] {
-      def apply(request: String) = Future.value(request)
+      def apply(request: String) = {
+    //    latch.await()
+        Future.value(request)
+      }
     }
 
+    val threshold = OpenConnectionsThresholds(
+      lowWaterMark = 1,
+      highWaterMark = 2,
+      idleTimeout = 5 seconds
+    )
+
     // Bind the service to port 8080
-    val server: Server = ServerBuilder()
+    ServerBuilder()
       .codec(StringCodec)
-      .bindTo(new InetSocketAddress(8080))
+      .bindTo(new InetSocketAddress(8090))
       .name("echoserver")
+      .openConnectionsThresholds(threshold)
       .build(service)
   }
 }
