a16213b568255cfabdff2aa86c90cbadd9bd9beb
Bhaskar Maddala <bhaskar@tumblr.com>
2013-05-03 15:37:30 -0400

    Undo testing changes for echo server/client

--- finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoClient.scala
+++ finagle-example/src/main/scala/com/twitter/finagle/example/echo/EchoClient.scala
@@ -4,25 +4,24 @@ import com.twitter.finagle.builder.ClientBuilder
 import java.net.InetSocketAddress
 import com.twitter.finagle.Service
 
-class EchoClient(id: Int) {
+object EchoClient {
+  def main(args: Array[String]) {
     // Construct a client, and connect it to localhost:8080
-  val client: Service[String, String] = ClientBuilder()
-    .codec(StringCodec)
-    .hosts(new InetSocketAddress(8090))
-    .hostConnectionLimit(1)
-    .keepAlive(true)
-    .build()
+    val client: Service[String, String] = ClientBuilder()
+      .codec(StringCodec)
+      .hosts(new InetSocketAddress(8080))
+      .hostConnectionLimit(1)
+      .build()
 
-  def apply(msg: String) {
     // Issue a newline-delimited request, respond to the result
     // asynchronously:
-    client(msg) onSuccess { result =>
-      println("Received result asynchronously: " + result + " on " + id)
+    client("hi mom\n") onSuccess { result =>
+      println("Received result asynchronously: " + result)
     } onFailure { error =>
       error.printStackTrace()
     } ensure {
       // All done! Close TCP connection(s):
-      //client.close()
+      client.close()
     }
   }
 }
