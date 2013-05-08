d1804df8bf2303a7b3de9121ee36eb2cb164b797
Bhaskar Maddala <bhaskar@tumblr.com>
2013-05-08 14:24:43 -0400

    Add a counter to track the number of closed connections

--- finagle-core/src/main/scala/com/twitter/finagle/channel/IdleConnectionFilter.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/channel/IdleConnectionFilter.scala
@@ -4,7 +4,6 @@ import com.twitter.collection.BucketGenerationalQueue
 import com.twitter.finagle.service.FailedService
 import com.twitter.finagle.stats.{NullStatsReceiver, StatsReceiver}
 import com.twitter.finagle.{ServiceFactory, ServiceFactoryProxy}
-import com.twitter.conversions.time._
 
 import com.twitter.util.{Future, Duration}
 import java.util.concurrent.atomic.AtomicInteger
@@ -42,6 +41,7 @@ class IdleConnectionFilter[Req, Rep](
     queue.collectAll(threshold.idleTimeout).size
   }
   private[this] val refused = statsReceiver.counter("refused")
+  private[this] val closed = statsReceiver.counter("closed")
 
   def openConnections = connectionCounter.get()
 
@@ -80,6 +80,7 @@ class IdleConnectionFilter[Req, Rep](
     queue.collect(threshold.idleTimeout) match {
       case Some(conn) => {
         conn.close()
+        closed.incr()
         true
       }
       case None =>
