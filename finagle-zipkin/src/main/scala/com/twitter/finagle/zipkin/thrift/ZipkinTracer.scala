3831f5ab3abda612a108a3ca08ccef07adb2110e
Jeff Smick <jeff@twitter.com>
2013-05-15 22:08:21 +0000

    [split]  finagle-zipkin: make ZipkinTracer loadable
    RB_ID=144296

--- finagle-zipkin/src/main/scala/com/twitter/finagle/zipkin/thrift/ZipkinTracer.scala
+++ finagle-zipkin/src/main/scala/com/twitter/finagle/zipkin/thrift/ZipkinTracer.scala
@@ -1,8 +1,10 @@
 package com.twitter.finagle.zipkin.thrift
 
-import com.twitter.finagle.stats.{NullStatsReceiver, StatsReceiver}
+import com.twitter.finagle.stats.{DefaultStatsReceiver, NullStatsReceiver, StatsReceiver}
 import com.twitter.finagle.tracing.{TraceId, Record, Tracer}
+import com.twitter.finagle.zipkin.{Host, InitialSampleRate}
 import collection.mutable.{SynchronizedMap, HashMap}
+import java.net.InetSocketAddress
 
 object ZipkinTracer {
 
@@ -19,8 +21,8 @@ object ZipkinTracer {
    * @param sampleRate How much data to collect. Default sample rate 0.1%. Max is 1, min 0.
    */
   @deprecated("Use mk() instead", "6.1.0")
-  def apply(scribeHost: String = "localhost",
-            scribePort: Int = 1463,
+  def apply(scribeHost: String = Host().getHostName,
+            scribePort: Int = Host().getPort,
             statsReceiver: StatsReceiver = NullStatsReceiver,
             sampleRate: Float = Sampler.DefaultSampleRate
   ): Tracer.Factory = () => mk(scribeHost, scribePort, statsReceiver, sampleRate)
@@ -31,8 +33,8 @@ object ZipkinTracer {
    * @param statsReceiver Where to log information about tracing success/failures
    * @param sampleRate How much data to collect. Default sample rate 0.1%. Max is 1, min 0.
    */
-  def mk(host: String = "localhost",
-         port: Int = 1463,
+  def mk(host: String = Host().getHostName,
+         port: Int = Host().getPort,
          statsReceiver: StatsReceiver = NullStatsReceiver,
          sampleRate: Float = Sampler.DefaultSampleRate
   ): Tracer = synchronized {
@@ -50,15 +52,15 @@ object ZipkinTracer {
    * @param sr stats receiver to send successes/failures to
    */
   @deprecated("Use mk() instead", "6.1.0")
-  def apply(sr: StatsReceiver): Tracer.Factory = () => 
-    mk("localhost", 1463, sr, Sampler.DefaultSampleRate)
+  def apply(sr: StatsReceiver): Tracer.Factory = () =>
+    mk(Host().getHostName, Host().getPort, sr, Sampler.DefaultSampleRate)
 
   /**
    * Util method since named parameters can't be called from Java
    * @param sr stats receiver to send successes/failures to
    */
   def mk(statsReceiver: StatsReceiver): Tracer =
-    mk("localhost", 1463, statsReceiver, Sampler.DefaultSampleRate)
+    mk(Host().getHostName, Host().getPort, statsReceiver, Sampler.DefaultSampleRate)
 }
 
 /**
@@ -69,6 +71,10 @@ object ZipkinTracer {
 class SamplingTracer(underlyingTracer: Tracer, initialSampleRate: Float) extends Tracer {
   private[this] val sampler = new Sampler
   setSampleRate(initialSampleRate)
+
+  def this() = this(
+    new RawZipkinTracer(Host().getHostName, Host().getPort, DefaultStatsReceiver.scope("zipkin")),
+    InitialSampleRate())
 
   def sampleTrace(traceId: TraceId) = sampler.sampleTrace(traceId)
 
