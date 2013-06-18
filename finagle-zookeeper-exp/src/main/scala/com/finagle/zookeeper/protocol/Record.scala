4965b3c874c991a9bb242c212dc5784873abb5a3
Radu Stoenescu <radu_stoenescu@yahoo.com>
2013-06-19 01:42:48 +0300

    Add Record trait for wire exchangeable messages.

---
+++ finagle-zookeeper-exp/src/main/scala/com/finagle/zookeeper/protocol/Record.scala
@@ -0,0 +1,15 @@
+package com.finagle.zookeeper.protocol
+
+/**
+ * Every unit of communication between a client and a server should
+ * expose a method of serialization to a byte array
+ */
+trait Record {
+
+  /**
+   * Output the record to a byte array for being sent down the wire.
+   * @return
+   */
+  def serialize: Array[Byte]
+
+}
