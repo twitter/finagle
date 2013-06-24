f34ef0bc6db0a48b2ce99bc49c1c5c6606463986
Steve Gury <stevegury@twitter.com>
2013-06-24 23:51:45 +0000

    [split] Revert "Stream interface (i.e. chunked) for HTTP message bodies"
    RB_ID=158723

--- finagle-http/src/main/scala/com/twitter/finagle/http/codec/ResponseEncoder.scala
+++ finagle-http/src/main/scala/com/twitter/finagle/http/codec/ResponseEncoder.scala
@@ -1,9 +1,7 @@
 package com.twitter.finagle.http.codec
 
 import com.twitter.finagle.http.Response
-import java.net.SocketAddress
-import org.jboss.netty.channel.{Channels, ChannelFuture, ChannelFutureListener, ChannelHandlerContext, MessageEvent,
-  SimpleChannelDownstreamHandler}
+import org.jboss.netty.channel.{ChannelHandlerContext, MessageEvent, SimpleChannelDownstreamHandler}
 import org.jboss.netty.handler.codec.http.HttpHeaders
 
 
@@ -12,51 +10,13 @@ import org.jboss.netty.handler.codec.http.HttpHeaders
  */
 class ResponseEncoder extends SimpleChannelDownstreamHandler {
 
-  private[this] def continue(ctx: ChannelHandlerContext,
-                             future: ChannelFuture,
-                             response: Response,
-                             remoteAddr: SocketAddress) {
-    response.readChunk() onSuccess { chunk =>
-      val writeFuture = Channels.future(ctx.getChannel())
-      Channels.write(ctx, writeFuture, chunk, remoteAddr)
-      writeFuture.addListener(new ChannelFutureListener() {
-        def operationComplete(f: ChannelFuture) {
-          if (f.isCancelled()) future.cancel()
-          else if (!f.isSuccess()) future.setFailure(f.getCause())
-          else if (chunk.isLast()) future.setSuccess()
-          else continue(ctx, future, response, remoteAddr)
-        }
-      })
-    }
-  }
-
-  private[this] def writeResponse(ctx: ChannelHandlerContext,
-                                  future: ChannelFuture,
-                                  response: Response,
-                                  remoteAddr: SocketAddress) {
-    val writeFuture = Channels.future(ctx.getChannel())
-    Channels.write(ctx, writeFuture, response, remoteAddr)
-    writeFuture.addListener(new ChannelFutureListener() {
-      def operationComplete(f: ChannelFuture) {
-        if (f.isCancelled()) future.cancel()
-        else if (!f.isSuccess()) future.setFailure(f.getCause())
-        else continue(ctx, future, response, remoteAddr)
-      }
-    })
-  }
-
   override def writeRequested(ctx: ChannelHandlerContext, e: MessageEvent) {
     e.getMessage match {
       case response: Response =>
-        if (response.isChunked) {
-          writeResponse(ctx, e.getFuture(), response, e.getRemoteAddress())
-        } else {
-          // Ensure Content-Length is set if not chunked
-          if (!response.containsHeader(HttpHeaders.Names.CONTENT_LENGTH)) {
-            response.contentLength = response.getContent().readableBytes
-          }
-          super.writeRequested(ctx, e)
-        }
+        // Ensure Content-Length is set
+        if (!response.containsHeader(HttpHeaders.Names.CONTENT_LENGTH))
+          response.contentLength = response.getContent().readableBytes
+        super.writeRequested(ctx, e)
       case _ =>
         super.writeRequested(ctx, e)
     }
