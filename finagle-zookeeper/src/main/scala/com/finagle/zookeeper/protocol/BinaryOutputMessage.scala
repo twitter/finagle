4e1d82c3621185740b16c6661dc5259aad1d523b
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-06-26 21:16:35 +0300

    Add trait for message serialization.

---
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/BinaryOutputMessage.scala
@@ -0,0 +1,22 @@
+package com.finagle.zookeeper.protocol
+
+/**
+ * Outgoing binary message related set of methods.
+ *
+ * Caution: Any method call can modify the position inside
+ * the buffer after consuming a data item.
+ */
+class BinaryOutputMessage {
+
+  def writeByte: Byte
+  def writeBoolean: Boolean
+  def writeInteger: Integer
+  def writeLong: Long
+  def writeFloat: Float
+  def writeDouble: Double
+  def writeString: String
+  def writeBuffer: Array[Byte]
+
+  //TODO: Add rest of composite types
+
+}
