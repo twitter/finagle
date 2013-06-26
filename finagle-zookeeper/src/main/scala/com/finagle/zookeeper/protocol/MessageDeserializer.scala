dbc9488cb98bc0f2fee5fa69ab655c18128bb8a3
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-06-26 22:14:42 +0300

    Implement binary message (de)serialization functionality.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/MessageDeserializer.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/MessageDeserializer.scala
@@ -1,6 +1,8 @@
 package com.finagle.zookeeper.protocol
 
 import java.lang.Integer
+import java.io.{DataInputStream, InputStream}
+import java.nio.charset.StandardCharsets
 
 /**
  * Incoming binary message related set of methods.
@@ -16,9 +18,53 @@ trait MessageDeserializer {
   def readLong: Long
   def readFloat: Float
   def readDouble: Double
-  def readString: String
-  def readBuffer: Array[Byte]
+  def readString: Option[String]
+  def readBuffer: Option[Array[Byte]]
 
   //TODO: Add rest of composite types
+}
 
+class BinaryMessageDeserializer(inputStream: InputStream) extends MessageDeserializer {
+
+  private val streamWrapper = new DataInputStream(inputStream)
+
+  def readByte: Byte = streamWrapper.readByte
+
+  def readBoolean: Boolean = streamWrapper.readBoolean
+
+  def readInteger: Integer = streamWrapper.readInt
+
+  def readLong: Long = streamWrapper.readLong
+
+  def readFloat: Float = streamWrapper.readFloat
+
+  def readDouble: Double = streamWrapper.readDouble
+
+  def readString: Option[String] = {
+    val length = streamWrapper.readInt
+
+    length match {
+      case -1 => None
+      case _ => {
+        val byteBuffer = new Array[Byte](length)
+
+        streamWrapper.readFully(byteBuffer)
+        new Some(new String(byteBuffer, StandardCharsets.UTF_8))
+      }
+    }
+  }
+
+  def readBuffer: Option[Array[Byte]] = {
+    val length = streamWrapper.readInt
+
+    length match {
+      case -1 => None
+      case _ => {
+        val byteBuffer = new Array[Byte](length)
+
+        streamWrapper.readFully(byteBuffer)
+        new Some(byteBuffer)
+      }
+    }
+  }
 }
--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/MessageSerializer.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/MessageSerializer.scala
@@ -1,22 +1,68 @@
 package com.finagle.zookeeper.protocol
 
+import java.lang.Integer
+import java.io.{DataOutputStream, OutputStream}
+import java.nio.charset.StandardCharsets
+
 /**
  * Outgoing binary message related set of methods.
  *
  * Caution: Any method call can modify the position inside
  * the buffer after consuming a data item.
  */
-class MessageSerializer {
+trait MessageSerializer {
 
-  def writeByte: Byte
-  def writeBoolean: Boolean
-  def writeInteger: Integer
-  def writeLong: Long
-  def writeFloat: Float
-  def writeDouble: Double
-  def writeString: String
-  def writeBuffer: Array[Byte]
+  // TODO: Decide if this chain calling friendly interface is OK
+  def writeByte(byte: Byte): MessageSerializer
+  def writeBoolean(boolean: Boolean): MessageSerializer
+  def writeInteger(integer: Integer): MessageSerializer
+  def writeLong(long: Long): MessageSerializer
+  def writeFloat(float: Float): MessageSerializer
+  def writeDouble(double: Double): MessageSerializer
+  def writeString(string: String): MessageSerializer
+  def writeBuffer(buffer: Array[Byte]): MessageSerializer
 
   //TODO: Add rest of composite types
+}
 
+class BinaryMessageSerializer(outputStream: OutputStream) extends MessageSerializer {
+
+  private val streamWrapper = new DataOutputStream(outputStream)
+
+  def writeByte(byte: Byte): MessageSerializer = {
+    streamWrapper.writeByte(byte); this
+  }
+
+  def writeBoolean(boolean: Boolean): MessageSerializer = {
+    streamWrapper.writeBoolean(boolean); this
+  }
+
+  def writeInteger(integer: Integer): MessageSerializer = {
+    streamWrapper.writeInt(integer); this
+  }
+
+  def writeLong(long: Long): MessageSerializer = {
+    streamWrapper.writeLong(long); this
+  }
+
+  def writeFloat(float: Float): MessageSerializer = {
+    streamWrapper.writeFloat(float); this
+  }
+
+  def writeDouble(double: Double): MessageSerializer = {
+    streamWrapper.writeDouble(double); this
+  }
+
+  // TODO: Should consider ZooKeeper stringToBuffer optimization
+  def writeString(string: String): MessageSerializer = {
+    streamWrapper.writeInt(string.length)
+    streamWrapper.write(string.getBytes(StandardCharsets.UTF_8))
+    this
+  }
+
+  def writeBuffer(buffer: Array[Byte]): MessageSerializer = {
+    streamWrapper.writeInt(buffer.length)
+    streamWrapper.write(buffer)
+    this
+  }
 }
