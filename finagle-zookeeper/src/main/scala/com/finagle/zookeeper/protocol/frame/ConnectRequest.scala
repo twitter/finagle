37e862e8ab629b963a8b846775cfdcc779ed8843
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-28 20:08:29 +0300

    Implement connect request and request header frames.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/frame/ConnectRequest.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/frame/ConnectRequest.scala
@@ -1,5 +1,34 @@
 package com.finagle.zookeeper.protocol.frame
 
-class ConnectRequest {
+import com.finagle.zookeeper.protocol.{MessageDeserializer, RecordDeserializer, MessageSerializer, SerializableRecord}
+import java.io.OutputStream
 
+case class ConnectRequest(protocolVersion: Int,
+                          lastZXIDSeen: Long,
+                          timeOut: Int,
+                          sessionID: Long,
+                          password: Array[Byte]
+                           ) extends SerializableRecord{
+
+
+  override def serialize(out: MessageSerializer) {
+    out.writeInteger(protocolVersion)
+    out.writeLong(lastZXIDSeen)
+    out.writeInteger(timeOut)
+    out.writeLong(sessionID)
+    out.writeBuffer(password)
+  }
+
+}
+
+object ConnectRequest extends RecordDeserializer {
+  def deserialize(input: MessageDeserializer): SerializableRecord = {
+    new ConnectRequest(
+      input.readInteger,
+      input.readLong,
+      input.readInteger,
+      input.readLong,
+      input.readBuffer
+    )
+  }
 }
