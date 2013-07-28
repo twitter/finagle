be4add3463bb978c3c44c904252dd8ec421c700a
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-29 01:11:16 +0300

    Add first element in the pipeline, the frame splitter.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/Zookeeper.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/Zookeeper.scala
@@ -1,12 +1,13 @@
 package com.finagle.zookeeper
 
-import org.jboss.netty.channel.ChannelPipelineFactory
+import org.jboss.netty.channel.{Channels, ChannelPipelineFactory}
 import com.twitter.finagle.Client
 import com.twitter.finagle.{ServiceFactory, Group, CodecFactory}
 import com.twitter.finagle.netty3.Netty3Transporter
 import com.twitter.finagle.client.{Bridge, DefaultClient}
 import com.twitter.finagle.dispatch.SerialClientDispatcher
 import java.net.SocketAddress
+import org.jboss.netty.handler.codec.frame.LengthFieldBasedFrameDecoder
 
 object Zookeeper extends Client[ZookeeperRequest, ZookeeperResponse] {
 
@@ -31,7 +32,21 @@ object ZookeeperTransporter extends Netty3Transporter[ZookeeperRequest, Zookeepe
 object ZookeeperClientPipelineFactory extends ChannelPipelineFactory{
 
   def getPipeline() = {
-    null
+    val pipeline = Channels.pipeline()
+
+    /**
+     * First element in the pipeline does the packet splitting from the wire.
+     */
+    pipeline.addLast("PackageSplitter", new LengthFieldBasedFrameDecoder(
+      //TODO: this should be smaller I guess
+      Int.MaxValue,
+      0,
+      4,
+      0,
+      // The length field is skipped.
+      4)
+    )
+    pipeline
   }
 
 }
