c9cf440dc666b5b1d3cd7e83904c47c3af224aa7
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-28 20:16:59 +0300

    Implement connect response frame.

---
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/protocol/frame/ConnectResponse.scala
@@ -0,0 +1,30 @@
+package com.finagle.zookeeper.protocol.frame
+
+import com.finagle.zookeeper.protocol.{MessageDeserializer, RecordDeserializer, MessageSerializer, SerializableRecord}
+
+case class ConnectResponse(protocolVersion: Int,
+                           timeOut: Int,
+                           sessionID: Long,
+                           password: Array[Byte]
+                            ) extends SerializableRecord{
+
+
+  override def serialize(out: MessageSerializer) {
+    out.writeInteger(protocolVersion)
+    out.writeInteger(timeOut)
+    out.writeLong(sessionID)
+    out.writeBuffer(password)
+  }
+
+}
+
+object ConnectResponse extends RecordDeserializer {
+  def deserialize(input: MessageDeserializer): SerializableRecord = {
+    new ConnectResponse(
+      input.readInteger,
+      input.readInteger,
+      input.readLong,
+      input.readBuffer
+    )
+  }
+}
