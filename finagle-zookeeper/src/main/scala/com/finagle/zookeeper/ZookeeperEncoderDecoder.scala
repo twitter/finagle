d7a63f06f64224a19fbf708d4bca8b29f540a03e
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-29 00:08:15 +0300

    Add state transiton logic.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/ZookeeperEncoderDecoder.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/ZookeeperEncoderDecoder.scala
@@ -1,12 +1,12 @@
 package com.finagle.zookeeper
 
-import org.jboss.netty.channel.SimpleChannelHandler
+import org.jboss.netty.channel.{ChannelStateEvent, ChannelHandlerContext, SimpleChannelHandler}
 import com.twitter.util.StateMachine
 
 /**
- * This is the core artifact of the module, implementinc the transition to the wire protocol.
+ * This is the core artifact of the module, implementing the transition to the wire protocol.
  *
- * It encorporates state machine behaviour because of the logic of the Zookeeper protocol.
+ * It uses a state machine behaviour because of the logic of the Zookeeper protocol.
  */
 class ZookeeperEncoderDecoder extends SimpleChannelHandler with StateMachine {
 
@@ -21,8 +21,27 @@ class ZookeeperEncoderDecoder extends SimpleChannelHandler with StateMachine {
   case object AuthFailed extends State
   case object NotConnected extends State
 
-  def reset() {
+  //TODO: Not sure if ok, maybe transition is better.
+  state = reset
 
+  /**
+   * Begin connection establishment phase.
+   */
+  def begin() {
+    state = Connecting
   }
 
+  def reset() {
+    state = NotConnected
+  }
+
+  override def channelConnected(ctx: ChannelHandlerContext, e: ChannelStateEvent) {
+    super.channelConnected(ctx, e)
+    begin()
+  }
+
+  override def channelDisconnected(ctx: ChannelHandlerContext, e: ChannelStateEvent) {
+    super.channelDisconnected(ctx, e)
+    reset()
+  }
 }
