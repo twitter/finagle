4c7acd6998b30457f4297ece240166e6454ae1cc
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-28 23:54:19 +0300

    Add possible states.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/ZookeeperEncoderDecoder.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/ZookeeperEncoderDecoder.scala
@@ -3,6 +3,19 @@ package com.finagle.zookeeper
 import org.jboss.netty.channel.SimpleChannelHandler
 import com.twitter.util.StateMachine
 
+
+sealed trait State
+/**
+ * Possible states of the client
+ */
+case object Connecting extends State
+case object Associating extends State
+case object Connected extends State
+case object ReadOnlyConnected extends State
+case object Closed extends State
+case object AuthFailed extends State
+case object NotConnected extends State
+
 /**
  * This is the core artifact of the module, implementinc the transition to the wire protocol.
  *
