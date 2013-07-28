6bd7ff1258848e3e6b833044edd41bd98df682fd
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-28 21:35:11 +0300

    Add Zookeeper client blueprint, no pipeline factory implementation provided.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/Zookeeper.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/Zookeeper.scala
@@ -1,5 +1,37 @@
 package com.finagle.zookeeper
 
-class Zookeeper {
+import org.jboss.netty.channel.ChannelPipelineFactory
+import com.twitter.finagle.Client
+import com.twitter.finagle.{ServiceFactory, Group, CodecFactory}
+import com.twitter.finagle.netty3.Netty3Transporter
+import com.twitter.finagle.client.{Bridge, DefaultClient}
+import com.twitter.finagle.dispatch.SerialClientDispatcher
+import java.net.SocketAddress
+
+object Zookeeper extends Client[ZookeeperRequest, ZookeeperResponse] {
+
+  def newClient(group: Group[SocketAddress]): ServiceFactory[ZookeeperRequest, ZookeeperResponse] =
+    ZookeeperClient.newClient(group)
+}
+
+object ZookeeperClient extends DefaultClient[ZookeeperRequest, ZookeeperResponse](
+  name = "zookeeper",
+  endpointer = Bridge[ZookeeperRequest, ZookeeperResponse, ZookeeperRequest, ZookeeperResponse](
+    ZookeeperTransporter,
+  //TODO: May be safe to use piplelining since requests are sequenced at server side.
+    new SerialClientDispatcher(_)
+  )
+)
+
+object ZookeeperTransporter extends Netty3Transporter[ZookeeperRequest, ZookeeperResponse](
+  "zookeeper",
+  ZookeeperClientPipelineFactory
+)
+
+object ZookeeperClientPipelineFactory extends ChannelPipelineFactory{
+
+  def getPipeline() = {
+    null
+  }
 
 }
