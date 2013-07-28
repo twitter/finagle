29c4837c4044e968b54e770a68df80823cbea70e
Radu Stoenescu <Radu_Stoenescu@yahoo.com>
2013-07-28 23:49:58 +0300

    Add comment and todo for the type aliases employed.

--- finagle-zookeeper/src/main/scala/com/finagle/zookeeper/package.scala
+++ finagle-zookeeper/src/main/scala/com/finagle/zookeeper/package.scala
@@ -4,6 +4,10 @@ import zookeeper.protocol.SerializableRecord
 
 package object zookeeper {
 
+  /**
+   * Conenience renamings.
+   * TODO: These shluld go in favor of a better approach.
+   */
   type ZookeeperRequest = SerializableRecord
   type ZookeeperResponse = SerializableRecord
 
