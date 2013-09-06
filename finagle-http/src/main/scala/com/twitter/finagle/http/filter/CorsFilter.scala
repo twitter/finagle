f2b36de72669e1326b3a601baeac05c829a75590
David McLaughlin <dmclaughlin@twitter.com>
2013-09-06 17:45:53 +0000

    [split] Add Access-Control-Expose-Headers to Finagle CORSFilter
    RB_ID=180993

--- finagle-http/src/main/scala/com/twitter/finagle/http/filter/CorsFilter.scala
+++ finagle-http/src/main/scala/com/twitter/finagle/http/filter/CorsFilter.scala
@@ -4,8 +4,10 @@ package com.twitter.finagle.http.filter
 object CorsFilter {
   def apply(origin:  String = "*",
             methods: String = "GET",
-            headers: String = "x-requested-with") = new AddResponseHeadersFilter(Map(
+            headers: String = "x-requested-with",
+            exposes: String = "") = new AddResponseHeadersFilter(Map(
     "Access-Control-Allow-Origin"  -> origin,
     "Access-Control-Allow-Methods" -> methods,
-    "Access-Control-Allow-Headers" -> headers))
+    "Access-Control-Allow-Headers" -> headers,
+    "Access-Control-Expose-Headers" -> exposes))
 }
