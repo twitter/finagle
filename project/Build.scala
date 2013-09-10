2d4dad19eb9e85842d2ca95755cc7e7b7bc5730c
Brian Degenhardt <bdegenhardt@twitter.com>
2013-09-10 19:02:13 +0000

    [split] bump util to 6.5.1-SNAPSHOT
    RB_ID=183369
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Finagle extends Build {
   val libVersion = "6.5.2"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.4.0"
+  val utilVersion = "6.5.0"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.6.6.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % "9.1.2"
