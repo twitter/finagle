454eef9a49c5bd8303dfcd65bb01b1d37e046ce0
Moses Nakamura <mnakamura@twitter.com>
2013-09-19 21:23:30 +0000

    [split] finagle-core: lets the number of cores be specified at the command line
    RB_ID=183001

--- finagle-core/src/main/scala/com/twitter/finagle/netty3/WorkerPool.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/netty3/WorkerPool.scala
@@ -2,11 +2,10 @@ package com.twitter.finagle.netty3
 
 import com.twitter.app.GlobalFlag
 import com.twitter.concurrent.NamedPoolThreadFactory
+import com.twitter.jvm.numProcs
 import java.util.concurrent.Executors
 import org.jboss.netty.channel.socket.nio.NioWorkerPool
 
-object numWorkers extends GlobalFlag(
-  Runtime.getRuntime().availableProcessors()*2,
-  "Size of netty3 worker pool")
+object numWorkers extends GlobalFlag((numProcs() * 2).ceil.toInt, "Size of netty3 worker pool")
 
 object WorkerPool extends NioWorkerPool(Executor, numWorkers())
---
+++ finagle-core/src/test/scala/com/twitter/finagle/netty3/NumWorkersTest.scala
@@ -0,0 +1,26 @@
+package com.twitter.finagle.netty3
+
+import org.junit.runner.RunWith
+
+import org.scalatest.FunSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class NumWorkersTest extends FunSpec {
+  describe("numWorkers") {
+    it("should have 2 * the number of available processors according to the runtime by default") {
+      assert(System.getProperty("com.twitter.jvm.numProcs") === null)
+      assert(numWorkers() === Runtime.getRuntime().availableProcessors() * 2)
+    }
+
+    it("should be settable as a flag") {
+      val old = numWorkers()
+      numWorkers.parse("20")
+
+      assert(numWorkers() === 20)
+
+      numWorkers.parse()
+      assert(numWorkers() === old)
+    }
+  }
+}
