87c01d19cdd321980e0b1a171ebe54e896fdfb76
Alex Gleyzer <alex@gleyzer.com>
2013-10-18 15:22:14 -0400

    Turned DnsCluster into a trait

--- finagle-core/src/main/scala/com/twitter/finagle/builder/DnsCluster.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/builder/DnsCluster.scala
@@ -10,41 +10,24 @@ import java.security.Security
 import scala.collection._
 import scala.collection.JavaConverters._
 
-/** A dynamic cluster that keeps all DNS-resolved addresses for a
- * host.
+/**
+ * A dynamic cluster that keeps all DNS-resolved addresses for a host.
  *
- * Runs a timer to keep itself up to date with DNS changes. Can be
+ * Usese timer to keep itself up to date with DNS changes. Can be
  * helpful to access resources with frequently-changing addresses
  * (Akamai/Amazon S3/Google) from behind a firewall which does a
  * similar DNS lookup.
  */
-class DnsCluster(host: String, port: Int, ttl: Duration,
-                 timer: Timer = DefaultTimer.twitter)
-  extends Cluster[SocketAddress] {
+protected[builder] trait DnsCluster extends Cluster[SocketAddress] {
   import Cluster._
 
-  private[this] val log = Logger(this.getClass)
+  def ttl: Duration
+  def timer: Timer
+  def resolveHost: Future[Set[SocketAddress]]
 
+  private[this] val log = Logger(this.getClass)
   private[this] var underlyingSet = Set.empty[SocketAddress]
   private[this] var changes = new SpoolSource[Cluster.Change[SocketAddress]]
-
-  // exposed for testing
-  protected[builder] def blockingDnsCall: Set[SocketAddress] = {
-    InetAddress.getAllByName(host).map { address =>
-      new InetSocketAddress(address, port): SocketAddress
-    }.toSet
-  }
-
-  // exposed for testing
-  protected[builder] def resolveHost: Future[Set[SocketAddress]] =
-    FuturePool.unboundedPool(blockingDnsCall) handle {
-      case ex: UnknownHostException =>
-        log.error("DNS could not resolve host %s", host)
-        Set.empty[SocketAddress]
-      case ex =>
-        log.error(ex, "failed to update host %s", host)
-        underlyingSet
-    }
 
   private[this] def updateAddresses(newSet: Set[SocketAddress]): Unit = synchronized {
     if (newSet != underlyingSet) {
@@ -60,29 +43,19 @@ class DnsCluster(host: String, port: Int, ttl: Duration,
       }
 
       underlyingSet = newSet
-
-      log.debug("%s is now %s", host, underlyingSet)
     }
   }
 
-  // exectutes the future after a delay.. is there a standard function for this
-  def scheduleFuture[T](d: Duration)(f: => Future[T]): Future[T] =
-    timer.doLater(d)(()) flatMap { _ => f }
+  protected[builder] def loop(): Unit = {
+    resolveHost handle { case ex =>
+      log.error(ex, "failed to resolve host")
+      Set.empty[SocketAddress]
 
-  private[this] def loop(): Future[Unit] =
-    resolveHost flatMap { newSet =>
+    } onSuccess { newSet =>
       updateAddresses(newSet)
-      scheduleFuture(ttl) { loop() }
+      timer.doLater(ttl) { loop() }
     }
-
-  log.debug("starting DNS cluster for %s:%d, ttl %s", host, port, ttl)
-
-  // scheduleFuture(0) here helps dealing with test subclasses that
-  // override blockingDnsCall or resolveHost, calling loop() directly
-  // from constructor would cause an NPE in tests
-  val loopyFuture = scheduleFuture(0.millis) { loop() }
-
-  def stop(): Unit = loopyFuture.raise(new FutureCancelledException)
+  }
 
   def snap: (Seq[SocketAddress], Future[Spool[Change[SocketAddress]]]) =
     synchronized {
@@ -91,12 +64,29 @@ class DnsCluster(host: String, port: Int, ttl: Duration,
 }
 
 
+protected[builder] class BlockingDnsCluster(host: String, port: Int,
+  val ttl: Duration, val timer: Timer = DefaultTimer.twitter) extends DnsCluster {
+
+  private def blockingDnsCall: Set[SocketAddress] = {
+    InetAddress.getAllByName(host).map { address =>
+      new InetSocketAddress(address, port): SocketAddress
+    }.toSet
+  }
+
+  def resolveHost: Future[Set[SocketAddress]] =
+    FuturePool.unboundedPool(blockingDnsCall)
+
+  loop()
+}
+
+
 object DnsCluster {
 
-  def apply(host: String, port: Int): DnsCluster = {
+  def apply(host: String, port: Int, ttl: Duration): DnsCluster =
+    new BlockingDnsCluster(host, port, ttl)
 
-    // TTL using Java secutity property
-    // http://stackoverflow.com/questions/1256556/any-way-to-make-java-honor-the-dns-caching-timeout-ttl
+  def apply(host: String, port: Int): DnsCluster = {
+    // TTL using Java standard secutity property
     val ttl = {
       val minTtl = 5.seconds
       val defaultTtl = 10.seconds
@@ -106,6 +96,6 @@ object DnsCluster {
       property map (_.toInt.seconds max minTtl min maxTtl) getOrElse defaultTtl
     }
 
-    new DnsCluster(host, port, ttl)
+    apply(host, port, ttl)
   }
 }
--- finagle-core/src/test/scala/com/twitter/finagle/builder/DnsClusterSpec.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/builder/DnsClusterSpec.scala
@@ -38,14 +38,15 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
   import DnsClusterSpec._
 
   describe("DnsCluster") {
-    it("should be able to block till server set is ready") {
+     it("should be able to block till server set is ready") {
       Time.withCurrentTimeFrozen { tc =>
         val server = makeServer("reverse", s => s.reverse)
 
-        val timer = new MockTimer()
-
-        val cluster = new DnsCluster("block.until.ready.test", 80, 10.seconds, timer) {
+        val cluster = new DnsCluster {
+          override val ttl = 10.seconds
+          override val timer = new MockTimer()
           override val resolveHost = Promise[Set[SocketAddress]]()
+          loop()
         }
 
         val client = makeClient(cluster)
@@ -57,7 +58,7 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
         cluster.resolveHost.setValue((Set(server.localAddress)))
 
         tc.advance(10.seconds)
-        timer.tick()
+        cluster.timer.tick()
 
         cluster.ready.isDefined must be(true)
 
@@ -67,31 +68,31 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
 
     it("should be able to recognize a DNS change") {
       Time.withCurrentTimeFrozen { tc =>
-        val timer = new MockTimer()
-
         val server1 = makeServer("server1", _ + " server1")
         val server2 = makeServer("server2", _ + " server2")
 
-        val cluster = new DnsCluster("dns.change.test",
-          80, 10.seconds, timer = timer) {
+        val cluster = new DnsCluster {
+          override val ttl = 10.seconds
+          override val timer = new MockTimer()
 
           @volatile
           var currentServer: Server = server1
 
-          override def resolveHost =
-            Future.value(Set(currentServer.localAddress))
+          override def resolveHost = Future.value(Set(currentServer.localAddress))
+
+          loop()
         }
 
         val client = makeClient(cluster)
 
         cluster.currentServer = server1
 
-        timer.tick()
+        cluster.timer.tick()
         Await.result(client("hello\n"), 1.second) must be("hello server1")
 
         cluster.currentServer = server2
         tc.advance(10.seconds)
-        timer.tick()
+        cluster.timer.tick()
 
         Await.result(client("hello\n"), 1.second) must be("hello server2")
       }
@@ -99,20 +100,21 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
 
     it("should handle multiple random DNS chages") {
       Time.withCurrentTimeFrozen { tc =>
-
-        val timer = new MockTimer()
-
         val addresses = for (n <- 1 to 10) yield {
           var bytes = Array[Byte](10, 0, 0, n.toByte)
           new InetSocketAddress(InetAddress.getByAddress(bytes), 80)
         }
 
-        val cluster = new DnsCluster("multi.dns.changes.test",
-          80, 10.seconds, timer) {
+        val cluster = new DnsCluster {
+          override val ttl = 10.seconds
+          override val timer = new MockTimer()
 
           @volatile
-          var current: Set[SocketAddress] = _
+          var current: Set[SocketAddress] = Set.empty
+
           override def resolveHost = Future.value(current)
+
+          loop()
         }
 
         val rnd = new scala.util.Random
@@ -129,7 +131,8 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
           cluster.current = Set(rnd.shuffle(addresses).take(2): _*)
 
           tc.advance(10.seconds)
-          timer.tick()
+          cluster.timer.tick()
+
           current must equal(cluster.current)
 
           current = cluster.current
@@ -141,11 +144,15 @@ class DnsClusterSpec extends FunSpec with MustMatchers {
       Time.withCurrentTimeFrozen { tc =>
         val timer = new MockTimer()
 
-        val cluster = new DnsCluster("unknown.host.exception.test",
-          80, 10.seconds, timer) {
+        val cluster = new DnsCluster {
+          override val ttl = 10.seconds
+          override val timer = new MockTimer()
 
-          override def blockingDnsCall: Set[SocketAddress] =
-            throw new UnknownHostException("blah")
+          @volatile
+            var current: Set[SocketAddress] = _
+            override def resolveHost = Future.exception(new UnknownHostException)
+
+          loop()
         }
 
         cluster.ready.isDefined must be(false)
