f43f32adaa4633d72a4caa6396ee895d95c70d20
Ruben Oanta <roanta@twitter.com>
2013-10-20 00:11:52 +0000

    [split] bump finagle to 6.7.5-SNAPSHOT
    RB_ID=212508
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.7.3"
+  val libVersion = "6.7.4"
   val zkVersion = "3.3.4"
   val utilVersion = "6.7.0"
   val ostrichVersion = "9.1.3"
