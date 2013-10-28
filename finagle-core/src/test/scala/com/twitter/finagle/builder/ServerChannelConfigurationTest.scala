e2e83742528f0f4360df8bf58cf0b362d7624f02
Harley Cooper <hcooper@twitter.com>
2013-10-28 18:24:09 +0000

    [split] Tagging ServerChannelConfiguration test as flaky
    RB_ID=216432

--- finagle-core/src/test/scala/com/twitter/finagle/builder/ServerChannelConfigurationTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/builder/ServerChannelConfigurationTest.scala
@@ -57,7 +57,7 @@ class ServerChannelConfigurationTest extends FunSuite {
     def apply(request: String) = Future.value(request)
   }
 
-  test("close connection after max life time duration") {
+  if (!sys.props.contains("SKIP_FLAKY")) test("close connection after max life time duration") {
     // create a server builder which will close connections in 2 seconds
     val address = new InetSocketAddress(0)
     val server = ServerBuilder()
