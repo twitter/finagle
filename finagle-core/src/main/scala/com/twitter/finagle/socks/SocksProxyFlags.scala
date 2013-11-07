16ec8bae7dce008bc61d09efe2337bd5d8146d3d
Jake Donham <jdonham@twitter.com>
2013-11-07 18:37:58 +0000

    [split] fix -socksProxy{Host,Port} usage
    RB_ID=223733

--- finagle-core/src/main/scala/com/twitter/finagle/socks/SocksProxyFlags.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/socks/SocksProxyFlags.scala
@@ -4,8 +4,8 @@ package socks
 import com.twitter.app.GlobalFlag
 import java.net.{SocketAddress, InetSocketAddress}
 
-private[finagle] object socksProxyHost extends GlobalFlag[String]("SOCKS proxy host") { override val name = "socksProxyHost" }
-private[finagle] object socksProxyPort extends GlobalFlag[Int]("SOCKS proxy port") { override val name = "socksProxyPort" }
+private[finagle] object socksProxyHost extends GlobalFlag("", "SOCKS proxy host") { override val name = "socksProxyHost" }
+private[finagle] object socksProxyPort extends GlobalFlag(0, "SOCKS proxy port") { override val name = "socksProxyPort" }
 
 private[finagle] object SocksProxyFlags {
   def socksProxy: Option[SocketAddress] =
