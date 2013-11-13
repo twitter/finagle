5c2ffcd8f29d53f2a1fa58ff3874ff4f97172a0a
Steve Gury <stevegury@twitter.com>
2013-11-13 04:39:34 +0000

    [split] Release ostrich version 9.2.0
    RB_ID=230327
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Finagle extends Build {
   val libVersion = "6.7.4"
   val zkVersion = "3.3.4"
   val utilVersion = "6.8.0"
-  val ostrichVersion = "9.1.3"
+  val ostrichVersion = "9.2.0"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.7.0.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion
