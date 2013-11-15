9acd9fefa1b1976da1135ce1690706810485940e
Steve Gury <stevegury@twitter.com>
2013-11-15 18:14:25 +0000

    [split] Release util version 6.8.1
    RB_ID=232807
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Finagle extends Build {
   val libVersion = "6.8.0"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.8.0"
+  val utilVersion = "6.8.1"
   val ostrichVersion = "9.2.0"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.8.0.Final"
