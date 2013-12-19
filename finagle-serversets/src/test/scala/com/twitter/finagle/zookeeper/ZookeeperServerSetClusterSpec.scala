6b69edc83bcac41b3a362a2575d7865b3fdb40f3
Stephan Zuercher <stephan@twitter.com>
2013-12-19 17:15:55 +0000

    [split] finagle-serversets: use mocks to fix flakey ZookeeperServerSetClusterSpec and convert to scalatest
    RB_ID=255669

--- finagle-serversets/src/test/scala/com/twitter/finagle/zookeeper/ZookeeperServerSetClusterSpec.scala
+++ finagle-serversets/src/test/scala/com/twitter/finagle/zookeeper/ZookeeperServerSetClusterSpec.scala
@@ -1,138 +1,137 @@
 package com.twitter.finagle.zookeeper
 
-
-import com.twitter.common.zookeeper.ServerSetImpl
+import com.google.common.collect.ImmutableSet
+import com.twitter.common.net.pool.DynamicHostSet.HostChangeMonitor
+import com.twitter.common.zookeeper.ServerSet
 import com.twitter.conversions.time._
-import com.twitter.finagle.{NoBrokersAvailableException, Codec, CodecFactory, Service}
-import com.twitter.finagle.builder.{ClientBuilder, Server, ServerBuilder}
-import com.twitter.util.{Await, Future}
-import java.net.InetSocketAddress
-import org.jboss.netty.channel._
-import org.jboss.netty.handler.codec.frame.{
-  Delimiters, DelimiterBasedFrameDecoder}
-import org.jboss.netty.handler.codec.string.{StringEncoder, StringDecoder}
-import org.jboss.netty.util.CharsetUtil
-import org.specs.SpecificationWithJUnit
+import com.twitter.finagle.builder.Cluster
+import com.twitter.thrift.{Endpoint, ServiceInstance, Status}
+import com.twitter.util.Await
+import java.net.{InetSocketAddress, SocketAddress}
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.anyObject
+import org.mockito.Mockito.{doNothing, when, verify}
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+import scala.collection.JavaConverters._
 
-object StringCodec extends CodecFactory[String, String] {
-  def server = Function.const {
-    val pipeline = Channels.pipeline()
-    pipeline.addLast("line",
-      new DelimiterBasedFrameDecoder(100, Delimiters.lineDelimiter: _*))
-    pipeline.addLast("stringDecoder", new StringDecoder(CharsetUtil.UTF_8))
-    pipeline.addLast("stringEncoder", new StringEncoder(CharsetUtil.UTF_8))
-    Codec.ofPipeline(pipeline)
+@RunWith(classOf[JUnitRunner])
+class ZookeeperServerSetClusterSpec extends FunSuite with MockitoSugar {
+  type EndpointMap = Map[String, InetSocketAddress]
+  val EmptyEndpointMap = Map.empty[String, InetSocketAddress]
+
+  def forClient(
+    endpointName: Option[String]
+  )(
+    f: (ZookeeperServerSetCluster, (InetSocketAddress, EndpointMap) => Unit) => Unit
+  ) {
+    val serverSet = mock[ServerSet]
+    val monitorCaptor = ArgumentCaptor.forClass(classOf[HostChangeMonitor[ServiceInstance]])
+    doNothing.when(serverSet).monitor(monitorCaptor.capture)
+
+    val cluster = new ZookeeperServerSetCluster(serverSet, endpointName)
+    cluster.thread.join()
+
+    verify(serverSet).monitor(anyObject())
+    val clusterMonitor = monitorCaptor.getValue()
+
+    def registerHost(socketAddr: InetSocketAddress, extraEndpoints: EndpointMap) {
+      val additionalEndpoints = extraEndpoints map { case (name, addr) =>
+        name -> new Endpoint(addr.getHostName, addr.getPort)
+      }
+
+      val serviceInstance = new ServiceInstance(
+        new Endpoint(socketAddr.getHostName, socketAddr.getPort),
+        additionalEndpoints.asJava,
+        Status.ALIVE
+      )
+      val serviceInstances = ImmutableSet.builder[ServiceInstance].add(serviceInstance).build()
+
+      clusterMonitor.onChange(serviceInstances)
+    }
+
+    f(cluster, registerHost)
   }
 
-  def client = Function.const {
-    val pipeline = Channels.pipeline()
-    pipeline.addLast("stringEncode", new StringEncoder(CharsetUtil.UTF_8))
-    pipeline.addLast("stringDecode", new StringDecoder(CharsetUtil.UTF_8))
-    pipeline
-    Codec.ofPipeline(pipeline)
+  test("ZookeeperServerSetCluster registers the server with ZooKeeper") {
+    val serverSet = mock[ServerSet]
+    when(
+      serverSet.join(anyObject, anyObject, anyObject[Status])
+    ).thenReturn(mock[ServerSet.EndpointStatus])
+
+    val cluster = new ZookeeperServerSetCluster(serverSet)
+    cluster.thread.join()
+
+    val localAddress = new InetSocketAddress(8080)
+    cluster.join(localAddress)
+
+    verify(serverSet).join(localAddress, EmptyEndpointMap.asJava, Status.ALIVE)
   }
-}
 
-class ZookeeperServerSetClusterSpec extends SpecificationWithJUnit {
-  "ZookeeperServerSetCluster" should {
-    val inst = new ZkInstance
-    import inst._
-    doBefore(start())
-    doAfter(stop())
+  test("ZookeeperServerSetCluster registers the server with multiple endpoints") {
+    val serverSet = mock[ServerSet]
+    when(
+      serverSet.join(anyObject, anyObject, anyObject[Status])
+    ).thenReturn(mock[ServerSet.EndpointStatus])
 
-    def withServer(f: Server => Unit) {
-      val sillyService = new Service[String, String] {
-        def apply(request: String) = Future(request.reverse)
-      }
+    val cluster = new ZookeeperServerSetCluster(serverSet)
+    cluster.thread.join()
 
-      val server = ServerBuilder()
-        .codec(StringCodec)
-        .bindTo(new InetSocketAddress(0))
-        .name("ZKTestServer")
-        .build(sillyService)
+    val localAddress = new InetSocketAddress(8080)
+    val altLocalAddress = new InetSocketAddress(9090)
 
-      f(server)
+    cluster.join(localAddress, Map("alt" -> altLocalAddress))
+
+    verify(serverSet).join(localAddress, Map("alt" -> altLocalAddress).asJava, Status.ALIVE)
+  }
+
+  test("ZookeeperServerSetCluster receives a registration from ZooKeeper") {
+    forClient(None) { (cluster, registerHost) =>
+      val (current, futureChanges) = cluster.snap
+      assert(current.isEmpty)
+
+      val remoteAddress = new InetSocketAddress("host", 8080)
+      registerHost(remoteAddress, EmptyEndpointMap)
+
+      val changes = Await.result(futureChanges, 1.minute)
+      assert(changes.head == Cluster.Add(remoteAddress: SocketAddress))
     }
+  }
 
-    def withClient(cluster: ZookeeperServerSetCluster)(f: Service[String, String] => Unit) {
-      val client = ClientBuilder()
-        .cluster(cluster)
-        .codec(StringCodec)
-        .hostConnectionLimit(1)
-        .build()
+  test("ZookeeperServerSetCluster is able to block till server set is ready") {
+    forClient(None) { (cluster, registerHost) =>
+      assert(!cluster.ready.isDefined)
 
-      f(client)
+      val remoteAddress = new InetSocketAddress("host", 8080)
+      registerHost(remoteAddress, EmptyEndpointMap)
+
+      assert(cluster.ready.isDefined)
+      val (hosts, _) = cluster.snap
+      assert(hosts == Seq(remoteAddress: SocketAddress))
     }
+  }
 
-    "register the server with ZooKeeper" in {
-      val serverSet = new ServerSetImpl(zookeeperClient, "/twitter/services/silly")
-      val cluster = new ZookeeperServerSetCluster(serverSet)
+  test("ZookeeperServerSetCluster is able to use an additional endpoint") {
+    forClient(Some("other-endpoint")) { (cluster, registerHost) =>
+      val (current, futureChanges) = cluster.snap
+      assert(current.isEmpty)
 
-      withServer { server =>
-        cluster.join(server.localAddress)
+      val remoteAddress = new InetSocketAddress("host", 8080)
+      val otherRemoteAddress = new InetSocketAddress("host", 9090)
+      registerHost(remoteAddress, Map("other-endpoint" -> otherRemoteAddress))
 
-        withClient(cluster) { client =>
-          cluster.thread.join()
-          Await.result(client("hello\n"), 1.seconds) mustEqual "olleh"
-        }
-      }
+      val changes = Await.result(futureChanges, 1.minute)
+      assert(changes.head == Cluster.Add(otherRemoteAddress: SocketAddress))
     }
+  }
 
-    if (!sys.props.contains("SKIP_FLAKY")) "Be able to block till server set is ready" in {
-      val serverSet = new ServerSetImpl(zookeeperClient, "/twitter/services/silly")
-      val cluster = new ZookeeperServerSetCluster(serverSet)
-
-      withServer { server =>
-        withClient(cluster) { client =>
-          var response: Future[String] = Future.value("init")
-          val clientThread = new Thread {
-            override def run {
-              response = try {
-                client("hello\n")
-              } catch {
-                case _ : NoBrokersAvailableException =>
-                  true mustBe(false)
-                  Future.value("fail")
-              }
-            }
-          }
-
-          clientThread.start()
-          clientThread.join()
-          cluster.ready.isDefined must beFalse
-          cluster.join(server.localAddress)
-          cluster.thread.join()
-          Await.result(response) mustEqual "olleh"
-          cluster.ready.isDefined must beTrue
-        }
-      }
-    }
-
-    if (!Option(System.getProperty("SKIP_FLAKY")).isDefined) {
-      "Be able to use an additional endpoint" in {
-        val serverSet = new ServerSetImpl(zookeeperClient, "/twitter/services/silly")
-        val cluster = new ZookeeperServerSetCluster(serverSet, "other-endpoint")
-
-        withServer { server =>
-          cluster.join(new InetSocketAddress(8000),
-                       Map("other-endpoint" -> server.localAddress.asInstanceOf[InetSocketAddress]))
-
-          withClient(cluster) { client =>
-            cluster.thread.join()
-            Await.result(client("hello\n"), 1.seconds) mustEqual "olleh"
-          }
-        }
-      }
-    }
-
-    "Ignore a server which does not specify the additional endpoint" in {
-      val serverSet = new ServerSetImpl(zookeeperClient, "/twitter/services/silly")
-      val cluster = new ZookeeperServerSetCluster(serverSet, "this-endpoint")
-
-      withServer { server =>
-        cluster.join(server.localAddress)
-        cluster.ready.isDefined must beFalse
-      }
+  test("ZookeeperServerSetCluster ignores a server which does not specify the additional endpoint") {
+    forClient(Some("this-endpoint")) { (cluster, registerHost) =>
+      val remoteAddress = new InetSocketAddress("host", 8080)
+      registerHost(remoteAddress, EmptyEndpointMap)
+      assert(!cluster.ready.isDefined)
     }
   }
 }
