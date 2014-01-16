908ea9bd43c03be5ddf1fca92e9b8317eb371fc7
Moses Nakamura <mnakamura@twitter.com>
2014-01-16 17:36:55 +0000

    [split] finagle-core: masks cancellation on PipeliningDispatcher If an unsatisfied Future is interrupted on a Service returned by a ReusingPool, every unsatisfied Future returned by that Service will also be interrupted (and typically it will close the socket).
    Anecdotally, we have found that this is a vulnerability in finagle servers, that if a client established a connection and then quickly hangs up, that they can force connection churn, and possibly make other requests less successful.
    
    RB_ID=264131

--- finagle-core/src/main/scala/com/twitter/finagle/dispatch/PipeliningDispatcher.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/dispatch/PipeliningDispatcher.scala
@@ -5,6 +5,18 @@ import com.twitter.concurrent.{AsyncQueue, Permit}
 import com.twitter.util.{Future, Promise, Throw, Return}
 import com.twitter.finagle.WriteException
 
+/**
+ * A generic pipelining dispatcher, which assumes that servers will
+ * respect normal pipelining semantics, and that replies will be sent
+ * in the same order as requests were sent.  Exploits
+ * [[com.twitter.finagle.dispatch.GenSerialClientDispatcher
+ * GenSerialClientDispatcher]] to serialize requests.
+ *
+ * Because many requests might be sharing the same transport,
+ * [[com.twitter.util.Future Futures]] returned by PipeliningDispatcher#apply
+ * are masked, and will ignore interrupts.  This ensures that interrupting a
+ * Future in one request won't change the result of another request.
+ */
 class PipeliningDispatcher[Req, Rep](trans: Transport[Req, Rep])
     extends GenSerialClientDispatcher[Req, Rep, Req, Rep](trans) {
   private[this] val q = new AsyncQueue[Promise[Rep]]
@@ -24,4 +36,6 @@ class PipeliningDispatcher[Req, Rep](trans: Transport[Req, Rep])
 
   protected def dispatch(req: Req, p: Promise[Rep]): Future[Unit] =
     trans.write(req) onSuccess { _ => q.offer(p) }
+
+  override def apply(req: Req): Future[Rep] = super.apply(req).masked
 }
--- finagle-mux/src/test/scala/com/twitter/finagle/mux/EndToEndTest.scala
+++ finagle-mux/src/test/scala/com/twitter/finagle/mux/EndToEndTest.scala
@@ -1,15 +1,17 @@
 package com.twitter.finagle.mux
 
 import com.twitter.finagle.{Dtab, Mux, Service}
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Await, Future, Promise}
 import java.io.{PrintWriter, StringWriter}
+import java.nio.charset.Charset
 import org.jboss.netty.buffer.{ChannelBuffer, ChannelBuffers}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
+import org.scalatest.concurrent.Eventually
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class EndToEndTest extends FunSuite {
+class EndToEndTest extends FunSuite with Eventually {
 
   test("Dtab propagation") {
     val server = Mux.serve(":*", new Service[ChannelBuffer, ChannelBuffer] {
@@ -20,9 +22,9 @@ class EndToEndTest extends FunSuite {
         Future.value(ChannelBuffers.wrappedBuffer(stringer.toString.getBytes))
       }
     })
-    
+
     val client = Mux.newService(server)
-    
+
     Dtab.unwind {
       Dtab.delegate("/foo", "/bar")
       Dtab.delegate("/web", "inet!twitter.com:80")
@@ -33,5 +35,27 @@ class EndToEndTest extends FunSuite {
       assert(str === "Dtab(2)\n\t/foo -> /bar\n\t/web -> inet!twitter.com:80\n")
     }
 
+  }
+
+  test("Discard request properly sent") {
+    @volatile var handled = false
+    val p = Promise[ChannelBuffer]()
+    p.setInterruptHandler { case t: Throwable =>
+      handled = true
+    }
+
+    val server = Mux.serve(":*", new Service[ChannelBuffer, ChannelBuffer] {
+      def apply(req: ChannelBuffer) = {
+        p
+      }
+    })
+
+    val client = Mux.newService(server)
+
+    val f = client(ChannelBuffers.EMPTY_BUFFER)
+    assert(!f.isDefined)
+    assert(!p.isDefined)
+    f.raise(new Exception())
+    eventually { assert(handled) }
   }
 }
