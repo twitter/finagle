5a291beabb9142f80f8555faf57d72fd119737d1
Steve Gury <stevegury@twitter.com>
2014-02-19 03:56:53 +0000

    [split] release finable version 6.12.1
    RB_ID=290583
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.12.0"
+  val libVersion = "6.12.1"
   val zkVersion = "3.3.4"
   val utilVersion = "6.12.1"
   val ostrichVersion = "9.3.1"
