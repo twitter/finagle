16becd99f9b08de30610846be6ff263c50f0e7f3
Moses Nakamura <mnakamura@twitter.com>
2014-03-20 17:55:22 -0400

    tests pass

--- finagle-core/src/main/scala/com/twitter/finagle/service/DelayedFactory.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/service/DelayedFactory.scala
@@ -1,7 +1,10 @@
 package com.twitter.finagle.service
 
-import com.twitter.finagle.{ClientConnection, Service, ServiceFactory}
+import com.twitter.finagle.{ClientConnection, Service, ServiceFactory,
+  ServiceClosedException, CancelledConnectionException}
 import com.twitter.util.{Await, Closable, Future, Promise, Time, Throw, Return}
+import scala.collection.JavaConverters._
+import java.util.ArrayDeque
 
 /**
  * A factory that won't satisfy the service future until an underlying service factory
@@ -19,22 +22,41 @@ class DelayedFactory[Req, Rep](
   underlyingF: Future[ServiceFactory[Req, Rep]]
 ) extends ServiceFactory[Req, Rep] {
   private[this] def wrapped: Future[ServiceFactory[Req, Rep]] =
-    safelyInterruptible(wrapped)
+    safelyInterruptible(underlyingF)
+
+  private[this] val q = new ArrayDeque[Promise[ServiceFactory[Req, Rep]]]()
 
   private[this] def safelyInterruptible(
     f: Future[ServiceFactory[Req, Rep]]): Future[ServiceFactory[Req, Rep]] = {
     val p = Promise.attached(f)
     p setInterruptHandler { case t: Throwable =>
-        if (p.detach())
-          p.setException(t)
+        if (p.detach()) {
+          q.remove(p)
+          p.setException(new CancelledConnectionException(t))
+        }
     }
+    q.addLast(p)
     p
   }
 
   def apply(conn: ClientConnection): Future[Service[Req, Rep]] = wrapped.flatMap(_(conn))
 
-  override def close(deadline: Time) = wrapped.flatMap(_.close(deadline))
-  override def isAvailable = underlyingF.isDefined && Await.result(underlyingF).isAvailable
+  override def close(deadline: Time): Future[Unit] = {
+    if (underlyingF.isDefined) wrapped.flatMap(_.close(deadline))
+    else {
+      underlyingF.onSuccess(_.close(deadline))
+      val exc = new ServiceClosedException
+      underlyingF.raise(exc)
+      for (p <- q.asScala)
+        p.raise(exc)
+      Future.Done
+    }
+  }
+
+  override def isAvailable: Boolean =
+    underlyingF.isDefined && Await.result(underlyingF).isAvailable
+
+  private[finagle] def numWaiters(): Int = q.size()
 }
 
 object DelayedFactory {
