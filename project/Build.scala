8df28b77aa2d8b63cc99f86ea73ce119c151b244
Moses Nakamura <mnakamura@twitter.com>
2014-04-10 00:36:44 +0000

    [split] bump util to 6.14.1-SNAPSHOT
    RB_ID=331395
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Finagle extends Build {
   val libVersion = "6.13.1"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.13.2"
+  val utilVersion = "6.14.0"
   val ostrichVersion = "9.4.2"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.8.1.Final"
