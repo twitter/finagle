fb82464163dfb7103e1320791321e141e1668e2e
Jeff Pinner <jpinner@twitter.com>
2014-04-21 20:46:01 +0000

    [split] [finagle-core] fix truncation in ChannelBufferBuf#slice()
    Problem
    
    The contract for slice() request that out of bounds indices
    be truncated, but instead, ChannelBufferBuf#slice() throws
    an exception if i > 0 and j-i > length + buf.capacity().
    
    Solution
    
    Check for out of bounds indices and truncate the length in
    the call to buf.slice().
    
    Result
    
    slice() can be called with out of bounds indices.
    
    RB_ID=338297

--- finagle-core/src/test/scala/com/twitter/finagle/netty3/BufTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/netty3/BufTest.scala
@@ -9,12 +9,19 @@ import org.jboss.netty.buffer.{ChannelBuffer, ChannelBuffers}
 
 @RunWith(classOf[JUnitRunner])
 class BufTest extends FunSuite {
-  test("ChannelBufferBuf") {
+  test("ChannelBufferBuf.slice: slices according to the underlying ChannelBuffer") {
     val cb = ChannelBuffers.buffer(128)
     cb.writeBytes(Array[Byte](1,2,3,4,5,6,7,8,9))
     val buf = ChannelBufferBuf(cb)
     assert(buf.length === 9)
     assert(buf.slice(2, 4) === Buf.ByteArray(3,4))
     assert(buf === Buf.ByteArray(1,2,3,4,5,6,7,8,9))
+  }
+
+  test("ChannelBufferBuf.slice: truncates on out-of-bounds indices") {
+    val cb = ChannelBuffers.buffer(128)
+    cb.writeBytes(Array[Byte](1,2,3,4,5,6,7,8,9))
+    val buf = ChannelBufferBuf(cb)
+    buf.slice(1, Int.MaxValue)
   }
 }
