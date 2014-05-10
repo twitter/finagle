24f105fbe44fe52d0944dc3efa1582a912e66bec
p-antoine <pierre.antoine.ganaye@gmail.com>
2014-05-10 14:17:19 +0200

    Add  support to finagle-ostrich4

--- finagle-ostrich4/src/test/scala/com/twitter/finagle/stats/FinagleStatsTest.scala
+++ finagle-ostrich4/src/test/scala/com/twitter/finagle/stats/FinagleStatsTest.scala
@@ -1,20 +1,21 @@
 package com.twitter.finagle.stats
 
-import com.twitter.finagle.builder.{ClientBuilder, ServerBuilder}
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.mock.MockitoSugar
 import com.twitter.finagle.{Codec, CodecFactory, Service}
-import com.twitter.io.Charsets
-import com.twitter.ostrich.stats.Stats
-import com.twitter.util.Future
-import java.net.InetSocketAddress
+import com.twitter.util.{Await, Future}
 import org.jboss.netty.channel.{Channels, ChannelPipelineFactory}
-import org.jboss.netty.handler.codec.frame.{
-  Delimiters, DelimiterBasedFrameDecoder}
+import org.jboss.netty.handler.codec.frame.{Delimiters, DelimiterBasedFrameDecoder}
 import org.jboss.netty.handler.codec.string.{StringEncoder, StringDecoder}
-import org.specs.SpecificationWithJUnit
-import org.specs.mock.Mockito
+import com.twitter.io.Charsets
+import com.twitter.finagle.builder.{ClientBuilder, ServerBuilder}
+import java.net.InetSocketAddress
+import com.twitter.ostrich.stats.Stats
 
-class FinagleStatsSpec extends SpecificationWithJUnit with Mockito {
-
+@RunWith(classOf[JUnitRunner])
+class FinagleStatsTest extends FunSuite with MockitoSugar {
   val dummyService = new Service[String, String] {
     def apply(request: String) = Future.value("You said: " + request)
   }
@@ -66,27 +67,23 @@ class FinagleStatsSpec extends SpecificationWithJUnit with Mockito {
     .hostConnectionLimit(10)
     .build()
 
-  "Finagle stats system" should {
+  test("system should correctly count connections") {
+    /*TODO: is this ok? We are not registering connections gauge until connection
+    is needed.
+    Stats.getGauge("server/connections") must beSome(0.0)
+    Stats.getGauge("client/connections") must beSome(0.0)*/
 
-    "correctely count connections" in {
-      // TODO: is this ok? We are not registering connections gauge until connection
-      // is needed.
-//      Stats.getGauge("server/connections") must beSome(0.0)
-//      Stats.getGauge("client/connections") must beSome(0.0)
+    Await.result(service("Hello\n"))
+    assert(Stats.getGauge("server/connections") == Some(1.0))
+    assert(Stats.getGauge("client/connections") == Some(1.0))
+  }
 
-      service("Hello\n").get()
-      Stats.getGauge("server/connections") must beSome(1.0)
-      Stats.getGauge("client/connections") must beSome(1.0)
-    }
+  test("system should show symmetric stats on client and server") {
+    def equalsGauge(name: String) =
+      assert(Stats.getCounter("server/" + name)() == Stats.getCounter("client/" + name)())
 
-    "show symetric stats on client and server" in {
-      def equalsGauge(name: String) =
-        Stats.getCounter("server/" + name)() mustEqual Stats.getCounter("client/" + name)()
-
-      equalsGauge("requests")
-      equalsGauge("connects")
-      equalsGauge("success")
-    }
-
+    equalsGauge("requests")
+    equalsGauge("connects")
+    equalsGauge("success")
   }
 }
