519b2add869e4c6dcd41fc336b080b2827282e70
Ruben Oanta <roanta@twitter.com>
2014-05-14 16:16:02 +0000

    [split] finagle-mux: move exp.MuxClient and exp.MuxServer
    finagle-mux: redefine exp.MuxClient and exp.MuxServer
    
    Problem:
    
    ThriftMux relies on these being defined in a exp package, especially
    for the client definition. They inadvertently moved during a
    refactoring.
    
    Solution:
    
    Define Mux{Client,Server} in package exp and have the top-level
    Mux API use them.
    
    RB_ID=357015

--- finagle-mux/src/main/scala/com/twitter/finagle/mux/Mux.scala
+++ finagle-mux/src/main/scala/com/twitter/finagle/mux/Mux.scala
@@ -9,51 +9,61 @@ import com.twitter.finagle.stats.{ClientStatsReceiver, StatsReceiver}
 import com.twitter.finagle.transport.Transport
 import com.twitter.util.{Closable, CloseAwaitably, Future, Promise, Return, Time}
 import java.net.SocketAddress
-import org.jboss.netty.buffer.ChannelBuffer
+import org.jboss.netty.buffer.{ChannelBuffer => CB}
 
-object MuxTransporter extends Netty3Transporter[ChannelBuffer, ChannelBuffer](
+object MuxTransporter extends Netty3Transporter[CB, CB](
   "mux", mux.PipelineFactory)
 
-object MuxClient extends DefaultClient[ChannelBuffer, ChannelBuffer](
+object MuxClient extends DefaultClient[CB, CB](
   name = "mux",
   endpointer = (sa, sr) =>
-    (mux.lease.LeasedBridge[ChannelBuffer, ChannelBuffer, ChannelBuffer, ChannelBuffer](
+    (mux.lease.LeasedBridge[CB, CB, CB, CB](
       MuxTransporter, new mux.ClientDispatcher(_, sr))(sa, sr)),
   pool = (sr: StatsReceiver) => new ReusingPool(_, sr.scope("reusingpool")))
 
-object MuxListener extends Netty3Listener[ChannelBuffer, ChannelBuffer]("mux", mux.PipelineFactory)
-object MuxServer extends DefaultServer[ChannelBuffer, ChannelBuffer, ChannelBuffer, ChannelBuffer](
+object MuxListener extends Netty3Listener[CB, CB]("mux", mux.PipelineFactory)
+object MuxServer extends DefaultServer[CB, CB, CB, CB](
   "mux", MuxListener, new mux.ServerDispatcher(_, _, true)
 )
+
+package exp {
+  private[finagle] object MuxTransporter {
+    def apply(prms: Stack.Params): Transporter[CB, CB] =
+      Netty3Transporter(mux.PipelineFactory, prms)
+  }
+
+  private[finagle] object MuxClient extends StackClient[CB, CB, CB, CB](
+    // LeasedFactory.module needs to be directly before the stack endpoint.
+    StackClient.newStack
+      .replace(StackClient.Role.Pool, ReusingPool.module[CB, CB])
+      .replace(StackClient.Role.PrepConn, mux.lease.LeasedFactory.module[CB, CB]),
+    Stack.Params.empty) {
+    protected val newTransporter = MuxTransporter(_)
+    protected val newDispatcher: Stack.Params => Dispatcher = { prms =>
+      val param.Stats(sr) = prms[param.Stats]
+      trans => new mux.ClientDispatcher(trans.cast[CB, CB], sr)
+    }
+  }
+
+  private[finagle] object MuxListener {
+    def apply(prms: Stack.Params): Listener[CB, CB] =
+      Netty3Listener(mux.PipelineFactory, prms)
+  }
+
+  private[finagle] object MuxServer extends StackServer[CB, CB, CB, CB] {
+    protected val newListener = MuxListener(_)
+    protected val newDispatcher: Stack.Params => Dispatcher =
+      Function.const(new mux.ServerDispatcher(_, _, true))
+  }
+}
 
 /**
  * A client and server for the mux protocol described in [[com.twitter.finagle.mux]].
  */
-object Mux extends Client[ChannelBuffer, ChannelBuffer] with Server[ChannelBuffer, ChannelBuffer] {
-  def newClient(dest: Name, label: String): ServiceFactory[ChannelBuffer, ChannelBuffer] =
-    MuxClient.newClient(dest, label)
+object Mux extends Client[CB, CB] with Server[CB, CB] {
+  def newClient(dest: Name, label: String): ServiceFactory[CB, CB] =
+    exp.MuxClient.newClient(dest, label)
 
-  def serve(addr: SocketAddress, service: ServiceFactory[ChannelBuffer, ChannelBuffer]): ListeningServer =
-    MuxServer.serve(addr, service)
-
-  object MuxClient extends StackClient[ChannelBuffer, ChannelBuffer, ChannelBuffer, ChannelBuffer](
-    StackClient.newStack
-      .replace(StackClient.Role.Pool, ReusingPool.module[ChannelBuffer, ChannelBuffer])
-      // if LeasedFactory.module is not directly after the endpointer, the Stack will break
-      .replace(
-        StackClient.Role.PrepConn, mux.lease.LeasedFactory.module[ChannelBuffer, ChannelBuffer]
-      ),
-    Stack.Params.empty
-  ) {
-    protected val newTransporter: Stack.Params => Transporter[ChannelBuffer, ChannelBuffer] = { prms =>
-      Netty3Transporter(mux.PipelineFactory, prms)
-    }
-
-    protected val newDispatcher: Stack.Params =>
-      Transport[ChannelBuffer, ChannelBuffer] =>
-      Service[ChannelBuffer, ChannelBuffer] with mux.lease.Acting = { prms =>
-      val param.Stats(sr) = prms[param.Stats]
-      trans => new mux.ClientDispatcher(trans.cast[ChannelBuffer, ChannelBuffer], sr)
-    }
-  }
+  def serve(addr: SocketAddress, service: ServiceFactory[CB, CB]): ListeningServer =
+    exp.MuxServer.serve(addr, service)
 }
