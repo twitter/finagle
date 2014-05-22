7701c49dd8d86fcf7ddabbde56d642d935d92dcb
p-antoine <pierre.antoine.ganaye@gmail.com>
2014-05-22 09:04:22 +0200

    Correct indentation

--- finagle-zipkin/src/test/scala/com/twitter/finagle/zipkin/thrift/AnnotationTest.scala
+++ finagle-zipkin/src/test/scala/com/twitter/finagle/zipkin/thrift/AnnotationTest.scala
@@ -9,17 +9,17 @@ import org.junit.runner.RunWith
 @RunWith(classOf[JUnitRunner])
 class AnnotationTest extends FunSuite {
   test("ZipkinAnnotation should serialize properly") {
-		val ann = ZipkinAnnotation(Time.fromSeconds(123), "value", Endpoint(123, 123), Some(1.second))
-		val tann = ann.toThrift
+    val ann = ZipkinAnnotation(Time.fromSeconds(123), "value", Endpoint(123, 123), Some(1.second))
+    val tann = ann.toThrift
 
-		assert(tann.isSetHost)
-		assert(tann.host.ipv4 === ann.endpoint.ipv4)
-		assert(tann.host.port === ann.endpoint.port)
-		assert(tann.isSetValue)
-		assert(tann.value === ann.value)
-		assert(tann.isSetTimestamp)
-		assert(tann.timestamp === ann.timestamp.inMicroseconds)
-		assert(tann.isSetDuration)
-		assert(tann.duration === ann.duration.get.inMicroseconds)
+    assert(tann.isSetHost)
+    assert(tann.host.ipv4 === ann.endpoint.ipv4)
+    assert(tann.host.port === ann.endpoint.port)
+    assert(tann.isSetValue)
+    assert(tann.value === ann.value)
+    assert(tann.isSetTimestamp)
+    assert(tann.timestamp === ann.timestamp.inMicroseconds)
+    assert(tann.isSetDuration)
+    assert(tann.duration === ann.duration.get.inMicroseconds)
   }
 }
