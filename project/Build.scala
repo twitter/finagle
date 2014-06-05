2b271987aea95b34ab62249c287db6e97199709b
Brian Degenhardt <bdegenhardt@twitter.com>
2014-06-04 18:38:16 -0700

    [split] bump util to 6.17.1-SNAPSHOT
    RB_ID=376056
    TBR=true
    NO-QUEUE=true

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Finagle extends Build {
   val libVersion = "6.16.0"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.16.0"
+  val utilVersion = "6.17.0"
   val ostrichVersion = "9.5.0"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.9.1.Final"
