33bd89244bd37411a394b3f5dc1c077af1ed6db8
Steve Gury <stevegury@twitter.com>
2014-06-14 03:07:34 +0000

    [split] finagle-memcached: Expose the client type `KetamaClient` in the build() API
    Problem
    
    KetamaClientBuilder.build() doesn't expose the exact client type.
    
    Solution
    
    Expose the client type `KetamaClient` in the build() API.
    
    from github: https://github.com/twitter/finagle/pull/280
    Signed-off-by: Steve Gury <stevegury@twitter.com>- Problem
    
    RB_ID=384391

--- finagle-memcached/src/main/scala/com/twitter/finagle/memcached/Client.scala
+++ finagle-memcached/src/main/scala/com/twitter/finagle/memcached/Client.scala
@@ -926,7 +926,7 @@ case class KetamaClientBuilder private[memcached] (
   def ejectFailedHost(eject: Boolean): KetamaClientBuilder =
     copy(_ejectFailedHost = eject)
 
-  def build(): Client = {
+  def build(): KetamaClient = {
     val builder =
       (_clientBuilder getOrElse ClientBuilder().hostConnectionLimit(1).daemon(true))
         .codec(Memcached())
