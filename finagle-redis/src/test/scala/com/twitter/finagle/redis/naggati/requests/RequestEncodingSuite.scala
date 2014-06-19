dd712feb2adf37142e545def7885710a54b95299
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-06-19 16:18:28 -0500

    finagle-redis tests need refactoring to from specs2 to scalatest
    The refactoring required for replacing all specs tests with scalatests requires a few intermediate code reviews.
    This commit removes none of the original tests, but adds refacorted tests from the Naggati codec and the Client
    Integration tests.  Code review is needed before continuing.

---
+++ finagle-redis/src/test/scala/com/twitter/finagle/redis/naggati/requests/RequestEncodingSuite.scala
@@ -0,0 +1,26 @@
+package com.twitter.finagle.redis.protocol
+
+import com.twitter.finagle.redis.naggati.FinagleRedisRequestTest
+import com.twitter.finagle.redis.tags.CodecTest
+
+final class RequestEncodingSuite extends FinagleRedisRequestTest {
+
+  test("Correctly encode inline requests", CodecTest) {
+    val actualEncoding = codec.send(Get(foo))
+    val expectedEncoding = List("*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n")
+    assert(actualEncoding === expectedEncoding)
+  }
+
+  test("Correctly encode unified requests", CodecTest) {
+    val value = "bar\r\nbaz"
+    val actualEncoding = codec.send(Set(foo, string2ChanBuf(value)))
+    val expectedEncoding = List("*3\r\n$3\r\nSET\r\n$3\r\nfoo\r\n$%d\r\n%s\r\n".format(8, value))
+    assert(actualEncoding === expectedEncoding)
+  }
+
+  test("Correctly encode a HSet request with an empty string as value", CodecTest) {
+    val actualEncoding = codec.send(HSet(foo, bar, string2ChanBuf("")))
+    val expectedEncoding = List("*4\r\n$4\r\nHSET\r\n$3\r\nfoo\r\n$3\r\nbar\r\n$0\r\n\r\n")
+    assert(actualEncoding === expectedEncoding)
+  }
+}
