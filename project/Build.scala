790596c6055f8b67280eb11c9e8fb906be162614
Evan Meagher <evm@twitter.com>
2014-06-23 16:18:53 -0700

    [split] bump finagle to 6.18.1-SNAPSHOT
    RB_ID=391531
    TBR=true
    NO-QUEUE=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.17.0"
+  val libVersion = "6.18.0"
   val zkVersion = "3.3.4"
   val utilVersion = "6.18.0"
   val ostrichVersion = "9.5.2"
