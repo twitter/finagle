adfc22d180daf487fe93548a5e624faf4056ae3f
suncelesta <suncelesta@gmail.com>
2014-07-03 20:08:51 +0400

    EHLO sends domain/address literal

--- finagle-smtp/src/main/scala/com/twitter/finagle/smtp/Request.scala
+++ finagle-smtp/src/main/scala/com/twitter/finagle/smtp/Request.scala
@@ -1,11 +1,13 @@
 package com.twitter.finagle.smtp
 
+import java.net.InetAddress
+
 class Request(val cmd: String) {
   override def toString = cmd
 }
 
 object Request {
-  val Hello = new Request("EHLO") //Get information about the server
+  val Hello = new Request("EHLO " + InetAddress.getLocalHost.getHostName) //Get information about the server
   val Quit = new Request("QUIT")  //Close connection
   val Reset = new Request("RSET") //Reset mailing session, returning to initial state
   val Noop = new Request("NOOP")  //Wait an OK response from server
