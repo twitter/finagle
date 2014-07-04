25eb30ca1823a1591ccfa6f3a12b868b70747ea2
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-07-04 15:05:37 -0500

    Completed refactoring of all KEY command tests, along with all formerly 'Simple' tests in the ClientIntegrationTest

--- finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/FinagleRedisTest.scala
+++ finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/FinagleRedisTest.scala
@@ -1,13 +1,17 @@
 package com.twitter.finagle.redis.naggati
 
-import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.builder.{ClientBuilder, ServerBuilder}
 import com.twitter.finagle.redis.naggati.test.TestCodec
 import com.twitter.finagle.redis.protocol._
-import com.twitter.finagle.redis.util.{CBToString, RedisCluster, ReplyFormat, StringToChannelBuffer}
+import com.twitter.finagle.redis.util.{BytesToString, CBToString, RedisCluster, ReplyFormat, StringToChannelBuffer}
 import com.twitter.finagle.redis.{Redis, TransactionalClient}
-import com.twitter.util.Await
+import com.twitter.util.{Await, Future, Time}
 import org.jboss.netty.buffer.ChannelBuffer
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import java.net.InetSocketAddress
+import com.twitter.finagle.Service
+import com.twitter.finagle.redis.Client
+
 
 trait FinagleRedisTest extends FunSuite {
   protected def wrap(s: String): ChannelBuffer = StringToChannelBuffer(s)
@@ -45,13 +49,8 @@ trait FinagleRedisRequestTest extends FinagleRedisTest {
 
 trait FinagleRedisClientSuite extends FinagleRedisTest with BeforeAndAfterAll {
 
-  override def beforeAll(configMap: Map[String, Any]): Unit = {
-    RedisCluster.start()
-  }
-
-  override def afterAll(configMap: Map[String, Any]): Unit =  {
-    RedisCluster.stop()
-  }
+  override def beforeAll(configMap: Map[String, Any]): Unit = RedisCluster.start()
+  override def afterAll(configMap: Map[String, Any]): Unit = RedisCluster.stop()
 
   protected def withRedisClient(testCode: TransactionalClient => Any) {
     val client = TransactionalClient(
@@ -67,5 +66,80 @@ trait FinagleRedisClientSuite extends FinagleRedisTest with BeforeAndAfterAll {
     finally {
       client.release
     }
+  }
+}
+
+trait FinagleRedisClientServerIntegrationTest extends FinagleRedisTest with BeforeAndAfterAll {
+
+  private[this] lazy val svcClient = ClientBuilder()
+    .name("redis-client")
+    .codec(Redis())
+    .hosts(RedisCluster.hostAddresses())
+    .hostConnectionLimit(2)
+    .retries(2)
+    .build()
+
+  private[this] val service = new Service[Command, Reply] {
+    def apply(cmd: Command): Future[Reply] = {
+      svcClient(cmd)
+    }
+  }
+
+  private[this] val server = ServerBuilder()
+    .name("redis-server")
+    .codec(Redis())
+    .bindTo(new InetSocketAddress(0))
+    .build(service)
+
+  override def beforeAll(configMap: Map[String, Any]): Unit = RedisCluster.start()
+  override def afterAll(configMap: Map[String, Any]): Unit = RedisCluster.stop()
+
+  protected val OKStatusReply = StatusReply("OK")
+
+  protected def withRedisClient(testCode: Service[Command, Reply] => Any) {
+    val client = ClientBuilder()
+          .name("redis-client")
+          .codec(Redis())
+          .hosts(server.localAddress)
+          .hostConnectionLimit(1)
+          .retries(2)
+          .build()
+    try {
+      testCode(client)
+    }
+    finally {
+      Await.result(client(FlushDB))
+      client.close()
+    }
+  }
+
+  protected def assertMBulkReply(reply: Future[Reply], expects: List[String],
+    contains: Boolean = false) = Await.result(reply) match {
+      case MBulkReply(msgs) => contains match {
+        case true =>
+          assert(expects.isEmpty === false, "Test did no supply a list of expected replies.")
+          val newMsgs = ReplyFormat.toString(msgs)
+          expects.foreach({ msg =>
+            val doesMBulkReplyContainMessage = newMsgs.contains(msg)
+            assert(doesMBulkReplyContainMessage === true)
+          })
+        case false =>
+          val actualMessages = ReplyFormat.toChannelBuffers(msgs).map({ msg =>
+            CBToString(msg)
+          })
+          assert(actualMessages == expects)
+      }
+      case EmptyMBulkReply() => {
+        val isEmpty = true
+        val actualReply = expects.isEmpty
+        assert(actualReply === isEmpty)
+      }
+      case r: Reply => fail("Expected MBulkReply, got %s".format(r))
+      case _ => fail("Expected MBulkReply")
+    }
+
+  def assertBulkReply(reply: Future[Reply], expects: String) = Await.result(reply) match {
+    case BulkReply(msg) => assert(BytesToString(msg.array) === expects)
+    case _ => fail("Expected BulkReply")
   }
 }
--- finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/Tags.scala
+++ finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/Tags.scala
@@ -3,6 +3,6 @@ package com.twitter.finagle.redis.tags
 import org.scalatest.Tag
 
 object RedisTest extends Tag("com.twitter.finagle.redis.tags.RedisTest")
-object IntegrationTest extends Tag("com.twitter.finagle.redis.tags.IntegrationTest")
-object SlowTest extends Tag("com.twitter.finagle.redis.tags.SlowTest")
 object CodecTest extends Tag("com.twitter.finagle.redis.tags.CodecTest")
+object ClientTest extends Tag("com.twitter.finagle.redis.tags.ClientTest")
+object ClientServerTest extends Tag("com.twitter.finagle.redis.tags.ClientServerTest")
