5d6d2aded477f8cba94ffd691cdb8b096a35b158
Brian KimJohnson <briankimjohnson@gmail.com>
2014-07-05 02:42:18 -0400

    Convert ValidateThriftServiceSpec

--- finagle-thrift/src/test/scala/com/twitter/finagle/thrift/ValidateThriftService.scala
+++ finagle-thrift/src/test/scala/com/twitter/finagle/thrift/ValidateThriftService.scala
@@ -4,97 +4,101 @@ import com.twitter.finagle.{Service, WriteException}
 import com.twitter.util.{Await, Future, Promise, Return, Throw}
 import org.apache.thrift.TApplicationException
 import org.apache.thrift.protocol.{TBinaryProtocol, TMessage, TMessageType}
-import org.specs.SpecificationWithJUnit
-import org.specs.mock.Mockito
+import org.junit.runner.RunWith
+import org.mockito.Matchers
+import org.mockito.Mockito.{verify, when, times}
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
 
-class ValidateThriftServiceSpec extends SpecificationWithJUnit with Mockito {
+@RunWith(classOf[JUnitRunner])
+class ValidateThriftServiceSpec extends FunSuite with MockitoSugar with OneInstancePerTest{
   val protocolFactory = new TBinaryProtocol.Factory()
 
-  "ValidateThriftService" should {
-    val p = new Promise[Array[Byte]]
-    val service = mock[Service[ThriftClientRequest, Array[Byte]]]
-    service(any) returns p
-    service.isAvailable returns true
-    def newValidate() = new ValidateThriftService(service, new TBinaryProtocol.Factory())
-    val validate = newValidate()
-    val req = mock[ThriftClientRequest]
+  val p = new Promise[Array[Byte]]
+  val service = mock[Service[ThriftClientRequest, Array[Byte]]]
+  when(service(Matchers.any[ThriftClientRequest])).thenReturn(p)
+  when(service.isAvailable).thenReturn(true)
+  def newValidate() = new ValidateThriftService(service, new TBinaryProtocol.Factory())
+  val validate = newValidate()
+  val req = mock[ThriftClientRequest]
 
-    "query availability from underlying" in {
-      service.isAvailable returns true
-      validate.isAvailable must beTrue
-      there was one(service).isAvailable
-      service.isAvailable returns false
-      validate.isAvailable must beFalse
-      there were two(service).isAvailable
-    }
+  test("ValidateThriftService should query availability from underlying") {
+    when(service.isAvailable).thenReturn(true)
+    assert(validate.isAvailable)
+    verify(service).isAvailable
+    when(service.isAvailable).thenReturn(false)
+    assert(validate.isAvailable === false)
+    verify(service, times(2)).isAvailable
+  }
 
-    "handle no-exception messages" in {
+  test("ValidateThriftService should handle no-exception messages") {
+    val buf = new OutputBuffer(protocolFactory)
+    buf().writeMessageBegin(new TMessage("ok123", TMessageType.REPLY, 0))
+    buf().writeMessageEnd()
+    val res = validate(req)
+    assert(res.isDefined === false)
+    verify(service).apply(req)
+    p.setValue(buf.toArray)
+    assert(res.isDefined)
+    assert(validate.isAvailable)
+  }
+
+  test("ValidateThriftService should invalidate connection on bad TApplicationException") {
+    val codes = Seq(
+      TApplicationException.BAD_SEQUENCE_ID,
+      TApplicationException.INVALID_MESSAGE_TYPE,
+      TApplicationException.MISSING_RESULT,
+      TApplicationException.UNKNOWN,
+      TApplicationException.WRONG_METHOD_NAME)
+
+    for (typ <- codes) {
       val buf = new OutputBuffer(protocolFactory)
-      buf().writeMessageBegin(new TMessage("ok123", TMessageType.REPLY, 0))
+      buf().writeMessageBegin(new TMessage("ok123", TMessageType.EXCEPTION, 0))
+      val exc = new TApplicationException(typ, "wtf")
+      exc.write(buf())
       buf().writeMessageEnd()
-      val res = validate(req)
-      res.isDefined must beFalse
-      there was one(service).apply(req)
-      p.setValue(buf.toArray)
-      res.isDefined must beTrue
-      validate.isAvailable must beTrue
+      val validate = newValidate()
+      val arr = buf.toArray
+      when(service(Matchers.any[ThriftClientRequest])).thenReturn(Future.value(arr))
+      assert(validate.isAvailable)
+      val f = validate(req)
+      assert(f.isDefined)
+      assert(Await.result(f) === arr)
+      assert(validate.isAvailable === false)
+      val resp = validate(req).poll
+
+      assert(resp.isDefined)
+      assert(resp.get.isThrow)
+
+      val thrown = resp.get.asInstanceOf[Throw[Array[Byte]]].e
+      assert(thrown.isInstanceOf[WriteException])
+      assert(thrown.getCause.isInstanceOf[InvalidThriftConnectionException])
     }
+  }
 
-    "invalidate connection on bad TApplicationException" in {
-      val codes = Seq(
-        TApplicationException.BAD_SEQUENCE_ID,
-        TApplicationException.INVALID_MESSAGE_TYPE,
-        TApplicationException.MISSING_RESULT,
-        TApplicationException.UNKNOWN,
-        TApplicationException.WRONG_METHOD_NAME)
+  test("ValidateThriftService should not invalidate connection on OK TApplicationException") {
+    val codes = Seq(TApplicationException.INTERNAL_ERROR,
+      TApplicationException.UNKNOWN_METHOD)
 
-      for (typ <- codes) {
-        val buf = new OutputBuffer(protocolFactory)
-        buf().writeMessageBegin(new TMessage("ok123", TMessageType.EXCEPTION, 0))
-        val exc = new TApplicationException(typ, "wtf")
-        exc.write(buf())
-        buf().writeMessageEnd()
-        val validate = newValidate()
-        val arr = buf.toArray
-        service(any) returns Future.value(arr)
-        validate.isAvailable must beTrue
-        val f = validate(req)
-        f.isDefined must beTrue
-        Await.result(f) must be_==(arr)
-        validate.isAvailable must beFalse
-        val resp = validate(req).poll
-
-        resp.isDefined must beTrue
-        resp.get.isThrow must beTrue
-
-        val thrown = resp.get.asInstanceOf[Throw[Array[Byte]]].e
-        thrown.isInstanceOf[WriteException] must beTrue
-        thrown.getCause.isInstanceOf[InvalidThriftConnectionException] must beTrue
-      }
-    }
-
-    "not invalidate connection on OK TApplicationException" in {
-      val codes = Seq(TApplicationException.INTERNAL_ERROR,
-        TApplicationException.UNKNOWN_METHOD)
-
-      for (typ <- codes) {
-        val buf = new OutputBuffer(protocolFactory)
-        buf().writeMessageBegin(new TMessage("foobar", TMessageType.EXCEPTION, 0))
-        val exc = new TApplicationException(typ, "it's ok, don't worry about it!")
-        exc.write(buf())
-        buf().writeMessageEnd()
-        val validate = newValidate()
-        val arr = buf.toArray
-        service(any) returns Future.value(arr)
-        validate.isAvailable must beTrue
-        val f = validate(req)
-        f.isDefined must beTrue
-        Await.result(f) must be_==(arr)
-        validate.isAvailable must beTrue
-        validate(req).poll must beLike {
-          case Some(Return(_)) => true
-        }
-      }
+    for (typ <- codes) {
+      val buf = new OutputBuffer(protocolFactory)
+      buf().writeMessageBegin(new TMessage("foobar", TMessageType.EXCEPTION, 0))
+      val exc = new TApplicationException(typ, "it's ok, don't worry about it!")
+      exc.write(buf())
+      buf().writeMessageEnd()
+      val validate = newValidate()
+      val arr = buf.toArray
+      when(service(Matchers.any[ThriftClientRequest])).thenReturn(Future.value(arr))
+      assert(validate.isAvailable)
+      val f = validate(req)
+      assert(f.isDefined)
+      assert(Await.result(f) === arr)
+      assert(validate.isAvailable)
+      assert(validate(req).poll match {
+        case Some(Return(_)) => true
+        case _ => false
+      })
     }
   }
 }
