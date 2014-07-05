8757551e7d025d3bc1c373ab68644fb273ad521f
Brian KimJohnson <briankimjohnson@gmail.com>
2014-07-05 01:46:30 -0400

    Migration of tests in finagle-thrift and finagle-stream to ScalaTest
    Problem
    
    These two packages contain tests written in Specs2 and ScalaTest.
    
    Solution
    
    All tests in these packages were migrated to ScalaTest.  Additionally, the ignore facility
    in ScalaTest was used to message when flaky tests are disable.
    
    Result
    
    These packages now conform to the Finagle testing stadard.  Additionally, the ignore facility
    in ScalaTest was used to message when flaky tests are disabled.

---
+++ finagle-thrift/src/test/scala/com/twitter/finagle/thrift/ClientIdRequiredFilterTest.scala
@@ -0,0 +1,45 @@
+package com.twitter.finagle.thrift
+
+import com.twitter.finagle.Service
+import com.twitter.util.{Await, Future}
+import org.junit.runner.RunWith
+import org.mockito.Matchers
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+
+@RunWith(classOf[JUnitRunner])
+class ClientIdRequiredFilterTest extends FunSuite with MockitoSugar {
+
+  case class ClientIdRequiredFilterContext(underlying: Service[String,String]) {
+    lazy val service = new ClientIdRequiredFilter andThen underlying
+  }
+
+  val request = "request"
+  val response = Future.value("response")
+  val clientId = ClientId("test")
+  
+  test("ClientIdRequiredFilter passes through when ClientId exists") {
+    val c = ClientIdRequiredFilterContext(mock[Service[String,String]])
+    import c._
+
+    when(underlying(request)).thenReturn(response)
+    clientId.asCurrent {
+      val result = service(request)
+      assert(Await.result(result) === Await.result(response))
+      result
+    }
+  }
+
+  test("ClientIdRequiredFilter throws NoClientIdSpecifiedException when ClientId does not exist") {
+    val c = ClientIdRequiredFilterContext(mock[Service[String,String]])
+    import c._
+
+    ClientId.clear()
+    intercept[NoClientIdSpecifiedException]{
+      Await.result(service(request))
+    }
+    verify(underlying, times(0)).apply(Matchers.anyString())
+  }
+}
