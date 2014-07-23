bae622b4b4b5cdc743c580f50e02b0a2a0193a21
Adam Shannon <adam.shannon@banno.com>
2014-07-23 17:12:18 +0000

    [split] Removing specs from finagle/finagle-http tests. Problem
    In order to upgrade to 2.11 (and as part of a larger twitter initiative)
    specs has to be removed. This is one small step in doing so.
    
    Solution
    
    Remove specs, replace with the standard FunSuite / assert way of
    testing.
    
    Result.
    
    Hopefully nothing will change. Except for removing specs.
    
    Signed-off-by: Steve Gury <stevegury@twitter.com>
    
    RB_ID=411101

---
+++ finagle-http/src/test/scala/com/twitter/finagle/http/ProxyCredentialsTest.scala
@@ -0,0 +1,13 @@
+package com.twitter.finagle.http
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ProxyCredentialsTest extends FunSuite {
+  test("add Proxy-Authorization header") {
+    val creds = ProxyCredentials("foo", "bar")
+    assert(creds.basicAuthorization === "Basic Zm9vOmJhcg==")
+  }
+}
