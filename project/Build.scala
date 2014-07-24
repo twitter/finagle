a132116a2d33d5cdd9595a24fb69840ed50ff4a9
Neuman Vong <nvong@twitter.com>
2014-07-24 17:57:38 +0000

    [split] bump ostrich to 9.5.5-SNAPSHOT
    RB_ID=412609
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Finagle extends Build {
   val libVersion = "6.19.0"
   val zkVersion = "3.3.4"
   val utilVersion = "6.18.2"
-  val ostrichVersion = "9.5.3"
+  val ostrichVersion = "9.5.4"
   val jacksonVersion = "2.2.2"
   val nettyLib = "io.netty" % "netty" % "3.9.1.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion
