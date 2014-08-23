73b84b5b14f484f882178451b022e79f3bbdc1c6
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2014-08-23 01:29:36 -0500

    Refacotred FinagleRedisClientSuite trait to naming standard of FinalgeRedisClientTest

--- finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/commands/connection/ConnectionClientIntegrationSuite.scala
+++ finagle-redis/src/test/scala/com/twitter/finagle/redis/wip/commands/connection/ConnectionClientIntegrationSuite.scala
@@ -1,11 +1,11 @@
 package com.twitter.finagle.redis.integration
 
-import com.twitter.finagle.redis.naggati.FinagleRedisClientSuite
+import com.twitter.finagle.redis.naggati.FinagleRedisClientTest
 import com.twitter.finagle.redis.tags.{RedisTest, ClientTest}
 import com.twitter.util.Await
 import com.twitter.finagle.redis.util.{CBToString, StringToChannelBuffer}
 
-final class ConnectionClientIntegrationSuite extends FinagleRedisClientSuite {
+final class ConnectionClientIntegrationSuite extends FinagleRedisClientTest {
 
   test("Correctly perform the SELECT command", RedisTest, ClientTest) {
     withRedisClient { client =>
