73a47d75db0f8f7d3305fb145ad36edd81cb437f
Evan Meagher <evm@twitter.com>
2014-09-11 16:57:34 +0000

    [split] finagle-core: Add Scaladocs for ChannelBufferBuf and BufChannelBuffer
    RB_ID=452019

--- finagle-core/src/main/scala/com/twitter/finagle/netty3/Buf.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/netty3/Buf.scala
@@ -3,12 +3,24 @@ package com.twitter.finagle.netty3
 import com.twitter.io.Buf
 import org.jboss.netty.buffer.ChannelBuffer
 
+/**
+ * A [[com.twitter.io.Buf]] wrapper for
+ * Netty [[org.jboss.netty.buffer.ChannelBuffer ChannelBuffers]].
+ *
+ * @note Since `ChannelBuffer`s are mutable, modifying the wrapped buffer
+ * within `slice`s of a `ChannelBufferBuf` will modify the original wrapped
+ * `ChannelBuffer`. Similarly, modifications to the original buffer will be
+ * reflected in slices.
+ *
+ * @param buf The [[org.jboss.netty.buffer.ChannelBuffer]] to be wrapped in a
+ * [[com.twitter.io.Buf]] interface.
+ */
 case class ChannelBufferBuf(buf: ChannelBuffer) extends Buf {
   def write(bytes: Array[Byte], off: Int): Unit = {
     val dup = buf.duplicate()
     dup.readBytes(bytes, off, dup.readableBytes)
   }
-  
+
   def slice(i: Int, j: Int): Buf = {
     require(i >=0 && j >= 0, "Index out of bounds")
 
