e1bc225dc5da0d29089dc60d1903d777de02551a
Moses Nakamura <mnakamura@twitter.com>
2014-09-18 05:16:34 +0000

    [split] csl: publish the world
    RB_ID=450951
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,9 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.22.0"
+  val libVersion = "6.21.0"
   val zkVersion = "3.3.4"
-  val utilVersion = "6.21.1"
+  val utilVersion = "6.21.2"
   val ostrichVersion = "9.5.6"
   val jacksonVersion = "2.3.1"
   val nettyLib = "io.netty" % "netty" % "3.9.1.1.Final"
