6586b2f20fb2c5297622232edaff3f409088ddbe
Daniel Schobel <dschobel@twitter.com>
2014-10-03 19:05:17 +0000

    [split] sync finagle-memcached changes to finagle-memcachedx
    RB_ID=474019
    TBR=true

--- finagle-memcachedx/src/main/scala/com/twitter/finagle/Memcachedx.scala
+++ finagle-memcachedx/src/main/scala/com/twitter/finagle/Memcachedx.scala
@@ -7,15 +7,55 @@ import com.twitter.finagle.client._
 import com.twitter.finagle.dispatch.{SerialServerDispatcher, PipeliningDispatcher}
 import com.twitter.finagle.memcachedx.protocol.text.{
   MemcachedClientPipelineFactory, MemcachedServerPipelineFactory}
-import com.twitter.finagle.memcachedx.protocol.{Command, Response}
+import com.twitter.finagle.memcachedx.protocol.{Command, Response, RetrievalCommand, Values}
 import com.twitter.finagle.memcachedx.{Client => MClient, Server => MServer, _}
 import com.twitter.finagle.netty3._
 import com.twitter.finagle.pool.SingletonPool
 import com.twitter.finagle.server._
 import com.twitter.finagle.stats.{ClientStatsReceiver, StatsReceiver}
+import com.twitter.finagle.tracing._
 import com.twitter.finagle.util.DefaultTimer
 import com.twitter.hashing.KeyHasher
-import com.twitter.util.Duration
+import com.twitter.io.Buf
+import com.twitter.util.{Duration, Future}
+import scala.collection.mutable
+
+private object MemcachedxTraceInitializer {
+  object Module extends Stack.Simple[ServiceFactory[Command, Response]] {
+    val role = TraceInitializerFilter.role
+    val description = "Initialize traces for the client and record hits/misses"
+    def make(next: ServiceFactory[Command, Response])(implicit params: Params) = {
+      val param.Tracer(tracer) = get[param.Tracer]
+      val filter = new Filter(tracer)
+      filter andThen next
+    }
+  }
+
+  class Filter(tracer: Tracer) extends SimpleFilter[Command, Response] {
+    def apply(command: Command, service: Service[Command, Response]): Future[Response] = Trace.unwind {
+      Trace.pushTracerAndSetNextId(tracer)
+      val response = service(command)
+      command match {
+        case command: RetrievalCommand if Trace.isActivelyTracing =>
+          response onSuccess {
+            case Values(vals) =>
+              val cmd = command.asInstanceOf[RetrievalCommand]
+              val misses = mutable.Set.empty[String]
+              cmd.keys foreach { case Buf.Utf8(key) => misses += key }
+              vals foreach { value =>
+                val Buf.Utf8(key) = value.key
+                Trace.recordBinary(key, "Hit")
+                misses.remove(key)
+              }
+              misses foreach { Trace.recordBinary(_, "Miss") }
+            case _ =>
+          }
+        case _ =>
+      }
+      response
+    }
+  }
+}
 
 trait MemcachedxRichClient { self: Client[Command, Response] =>
   def newRichClient(group: Group[SocketAddress]): memcachedx.Client = memcachedx.Client(newClient(group).toService)
@@ -86,7 +126,8 @@ object MemcachedxClient extends DefaultClient[Command, Response](
   name = "memcached",
   endpointer = Bridge[Command, Response, Command, Response](
     MemcachedxTransporter, new PipeliningDispatcher(_)),
-  pool = (sr: StatsReceiver) => new SingletonPool(_, sr)
+  pool = (sr: StatsReceiver) => new SingletonPool(_, sr),
+  newTraceInitializer = MemcachedxTraceInitializer.Module
 ) with MemcachedxRichClient with MemcachedxKetamaClient
 
 private[finagle] object MemcachedxFailureAccrualClient {
@@ -115,7 +156,8 @@ private[finagle] class MemcachedxFailureAccrualClient(
       failureAccrualParams._1,
       failureAccrualParams._2,
       DefaultTimer.twitter, key, broker)
-  }
+  },
+  newTraceInitializer = MemcachedxTraceInitializer.Module
 ) with MemcachedxRichClient
 
 object MemcachedxListener extends Netty3Listener[Response, Command](
