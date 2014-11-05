f9ce2916dfb24a4f17e6466511a0728d5517c1dd
Moses Nakamura <mnakamura@twitter.com>
2014-11-05 23:38:10 +0000

    finagle-core: Made ServerChannelConfigurationTest more patient
    Problem
    
    "org.scalatest.exceptions.TestFailedException: Expected exception
    com.twitter.finagle.ChannelClosedException to be thrown, but
    com.twitter.finagle.ChannelWriteException was thrown."  This seems
    to indicate that we expect to get a ChannelClosedException but we
    actually weren't able to start writing bytes over the wire because
    we were still establishing a connection.
    
    Solution
    
    Made it more patient.
    
    RB_ID=504977

--- finagle-core/src/test/scala/com/twitter/finagle/builder/ServerChannelConfigurationTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/builder/ServerChannelConfigurationTest.scala
@@ -57,7 +57,7 @@ class ServerChannelConfigurationTest extends FunSuite {
     def apply(request: String) = Future.value(request)
   }
 
-  if (!sys.props.contains("SKIP_FLAKY")) test("close connection after max life time duration") {
+  test("close connection after max life time duration") {
     // create a server builder which will close connections in 2 seconds
     val address = new InetSocketAddress(0)
     val server = ServerBuilder()
@@ -76,7 +76,7 @@ class ServerChannelConfigurationTest extends FunSuite {
     // Issue a request which is NOT newline-delimited. Server should close connection
     // after waiting for 2 seconds for a new line
     intercept[ChannelClosedException] {
-      Await.result(client("123"), 5.seconds)
+      Await.result(client("123"), 15.seconds)
     }
     server.close()
   }
