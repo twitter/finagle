2d4999578dab8a0b3ef234dcdeab43432b1dc260
Evan Meagher <evm@twitter.com>
2014-11-19 18:57:32 +0000

    finagle-thriftmux: Add a Java test for {Client,Server}Builder usage
    RB_ID=517771

--- finagle-thriftmux/src/test/java/com/twitter/finagle/thriftmux/javatest/EndToEndTest.java
+++ finagle-thriftmux/src/test/java/com/twitter/finagle/thriftmux/javatest/EndToEndTest.java
@@ -1,10 +1,20 @@
 package com.twitter.finagle.thriftmux.javatest;
 
+import org.apache.thrift.protocol.TBinaryProtocol;
 import org.junit.Test;
 
+import java.net.InetSocketAddress;
+
 import com.twitter.finagle.ListeningServer;
-import com.twitter.finagle.thriftmux.thriftscala.TestService;
+import com.twitter.finagle.Service;
 import com.twitter.finagle.ThriftMux;
+import com.twitter.finagle.ThriftMuxClient;
+import com.twitter.finagle.ThriftMuxServer;
+import com.twitter.finagle.builder.ClientBuilder;
+import com.twitter.finagle.builder.ServerBuilder;
+import com.twitter.finagle.thrift.ClientId;
+import com.twitter.finagle.thriftmux.thriftscala.TestService;
+import com.twitter.finagle.thriftmux.thriftscala.TestService$FinagleService;
 import com.twitter.util.Future;
 
 import static junit.framework.Assert.assertEquals;
@@ -24,7 +34,6 @@ public class EndToEndTest {
 
   @Test
   public void testInterfaces() {
-    // ensure we are java friendly
     ListeningServer server = ThriftMux.server().serveIface(":*", new TestService.FutureIface() {
       public Future<String> query(String x) {
         return Future.value(x+x);
@@ -33,5 +42,36 @@ public class EndToEndTest {
 
     TestService.FutureIface client = ThriftMux.client().newIface(server, TestService.FutureIface.class);
     assertEquals(client.query("ok").get(), "okok");
+  }
+
+  @Test
+  public void testBuilders() {
+    InetSocketAddress addr = new InetSocketAddress(0);
+
+    TestService.FutureIface iface = new TestService.FutureIface() {
+      public Future<String> query(String x) {
+        return Future.value(x+x);
+      }
+    };
+
+    Service<byte[], byte[]> service = new TestService$FinagleService(
+      iface,
+      new TBinaryProtocol.Factory()
+    );
+
+   ServerBuilder.safeBuild(
+      service,
+      ServerBuilder.get()
+        .name("java-test-server")
+        .bindTo(addr)
+        .stack(ThriftMuxServer.get())
+    );
+
+   ClientBuilder.safeBuild(
+     ClientBuilder.get()
+       .name("java-test-client")
+       .hosts(addr)
+       .stack(ThriftMuxClient.withClientId(new ClientId("java-test-client")))
+   );
   }
 }
