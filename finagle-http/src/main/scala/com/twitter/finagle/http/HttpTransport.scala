21a9d6db8b08b9e5bfaefb666631c3be1c5ad232
Neuman Vong <nvong@twitter.com>
2014-11-24 02:21:03 +0000

    finagle-http/x: Un-hang EndToEndTest
    Problem
    
    When the connection manager observes a chunk appears outside of a
    request/response context, it throws an exception, which breaks out
    of the Future context because the caller did not catch it. This
    results in streamChunks "hanging".
    
    Solution
    
    We catch the exception thrown from ConnectionManager, converting
    it into a Throw.
    
    Result
    
    The channel write operation is interrupted correctly, allowing
    the Future computation depending on it to correctly handle
    failure.
    
    RB_ID=503637

--- finagle-http/src/main/scala/com/twitter/finagle/http/HttpTransport.scala
+++ finagle-http/src/main/scala/com/twitter/finagle/http/HttpTransport.scala
@@ -4,15 +4,15 @@ import com.twitter.finagle.transport.Transport
 import com.twitter.finagle.http.codec.ConnectionManager
 import com.twitter.util.{Future, Time}
 import java.net.SocketAddress
+import scala.util.control.NonFatal
 
 /**
  * A Transport with close behavior managed by ConnectionManager.
  */
-class HttpTransport(
-  self: Transport[Any, Any]
-) extends Transport[Any, Any] {
+class HttpTransport(self: Transport[Any, Any], manager: ConnectionManager)
+  extends Transport[Any, Any] {
 
-  private[this] val manager = new ConnectionManager()
+  def this(self: Transport[Any, Any]) = this(self, new ConnectionManager)
 
   def close(deadline: Time): Future[Unit] = self.close(deadline)
 
@@ -23,12 +23,15 @@ class HttpTransport(
         self.close()
     }
 
-  def write(m: Any): Future[Unit] = {
-    manager.observeMessage(m)
-    val f = self.write(m)
-    if (manager.shouldClose) f before self.close()
-    else f
-  }
+  def write(m: Any): Future[Unit] =
+    try {
+      manager.observeMessage(m)
+      val f = self.write(m)
+      if (manager.shouldClose) f before self.close()
+      else f
+    } catch {
+      case NonFatal(e) => Future.exception(e)
+    }
 
   def isOpen: Boolean = !manager.shouldClose && self.isOpen
 
---
+++ finagle-httpx/src/test/scala/com/twitter/finagle/httpx/HttpTransportTest.scala
@@ -0,0 +1,24 @@
+package com.twitter.finagle.httpx
+
+import com.twitter.concurrent.AsyncQueue
+import com.twitter.finagle.httpx.codec.ConnectionManager
+import com.twitter.finagle.transport.QueueTransport
+import com.twitter.util.Throw
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class HttpTransportTest extends FunSuite {
+  test("exceptions in connection manager stay within Future context") {
+    val exc = new IllegalArgumentException("boo")
+    val noop = new QueueTransport(new AsyncQueue[Any], new AsyncQueue[Any])
+    val trans = new HttpTransport(noop, new ConnectionManager {
+      override def observeMessage(message: Any) = throw exc
+    })
+
+    val f = trans.write(Unit)
+    assert(f.isDefined)
+    assert(f.poll === Some(Throw(exc)))
+  }
+}
