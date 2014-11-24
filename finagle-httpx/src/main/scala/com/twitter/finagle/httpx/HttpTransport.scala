4728ffa47a71ef3a64a3644aedb9e2f73deba4c8
Neuman Vong <nvong@twitter.com>
2014-11-24 02:21:03 +0000

    finagle-http/x: Un-hang EndToEndTest
    Problem
    
    When the connection manager observes a chunk appears outside of a
    request/response context, it throws an exception, which breaks out
    of the Future context because the caller did not catch it. This
    results in streamChunks "hanging".
    
    Solution
    
    We catch the exception thrown from ConnectionManager, converting
    it into a Throw.
    
    Result
    
    The channel write operation is interrupted correctly, allowing
    the Future computation depending on it to correctly handle
    failure.
    
    RB_ID=503637

--- finagle-httpx/src/main/scala/com/twitter/finagle/httpx/HttpTransport.scala
+++ finagle-httpx/src/main/scala/com/twitter/finagle/httpx/HttpTransport.scala
@@ -3,15 +3,15 @@ package com.twitter.finagle.httpx
 import com.twitter.finagle.transport.Transport
 import com.twitter.finagle.httpx.codec.ConnectionManager
 import com.twitter.util.{Future, Time}
+import scala.util.control.NonFatal
 
 /**
  * A Transport with close behavior managed by ConnectionManager.
  */
-class HttpTransport(
-  self: Transport[Any, Any]
-) extends Transport[Any, Any] {
+class HttpTransport(self: Transport[Any, Any], manager: ConnectionManager)
+  extends Transport[Any, Any] {
 
-  private[this] val manager = new ConnectionManager
+  def this(self: Transport[Any, Any]) = this(self, new ConnectionManager)
 
   def close(deadline: Time) = self.close(deadline)
 
@@ -22,12 +22,15 @@ class HttpTransport(
         self.close()
     }
 
-  def write(m: Any): Future[Unit] = {
-    manager.observeMessage(m)
-    val f = self.write(m)
-    if (manager.shouldClose) f before self.close()
-    else f
-  }
+  def write(m: Any): Future[Unit] =
+    try {
+      manager.observeMessage(m)
+      val f = self.write(m)
+      if (manager.shouldClose) f before self.close()
+      else f
+    } catch {
+      case NonFatal(e) => Future.exception(e)
+    }
 
   def isOpen = !manager.shouldClose && self.isOpen
 
