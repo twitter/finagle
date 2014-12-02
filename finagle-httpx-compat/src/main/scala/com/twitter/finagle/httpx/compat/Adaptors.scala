c1c7f82a573fbdeae30d770134e7b1bd0c064ae0
Neuman Vong <nvong@twitter.com>
2014-12-02 00:25:28 +0000

    [finagle-httpx-compat]: Netty client adaptor
    Problem
    
    We have the adaptors for services, but not for clients. To provide
    complete coverage we need an adaptor to convert an HTTPx client into
    a Netty client (related: RB_ID=497155).
    
    Solution
    
    A filter to wrap an HTTPx client so that it can be used with
    Netty types.
    
    Result
    
    Ability to use HTTPx clients with Netty types.
    
    RB_ID=527557
    TBR=true

--- finagle-httpx-compat/src/main/scala/com/twitter/finagle/httpx/compat/Adaptors.scala
+++ finagle-httpx-compat/src/main/scala/com/twitter/finagle/httpx/compat/Adaptors.scala
@@ -5,6 +5,7 @@ import com.twitter.finagle.httpx
 import com.twitter.finagle.{Service, Filter}
 import com.twitter.util.Future
 import org.jboss.netty.handler.codec.{http => netty}
+import java.net.InetSocketAddress
 
 /**
  * A abstract filter that adapts an arbitrary service to a Finagle HTTPx
@@ -57,4 +58,33 @@ object NettyAdaptor extends Adaptor[netty.HttpRequest, netty.HttpResponse] {
   private[compat] def out(r: netty.HttpResponse): Future[httpx.Response] =
     if (r.isChunked) Future.exception(NoStreaming)
     else Future.value(new httpx.Response { val httpResponse = r })
+}
+
+/**
+ * A abstract filter to adapt HTTPx clients into arbitrary clients.
+ */
+abstract class ClientAdaptor[Req, Rep]
+  extends Filter[Req, Rep, httpx.Request, httpx.Response] {
+  private[compat] def in(req: Req): Future[httpx.Request]
+  private[compat] def out(rep: httpx.Response): Future[Rep]
+  def apply(req: Req, next: Service[httpx.Request, httpx.Response]): Future[Rep] =
+    in(req) flatMap(next) flatMap(out)
+}
+
+/**
+ * A ClientAdaptor for Netty clients.
+ */
+object NettyClientAdaptor extends ClientAdaptor[netty.HttpRequest, netty.HttpResponse] {
+  val NoStreaming = new IllegalArgumentException("this client doesn't support streaming")
+
+  private[compat] def in(req: netty.HttpRequest): Future[httpx.Request] =
+    if (req.isChunked) Future.exception(NoStreaming)
+    else Future.value(new httpx.Request {
+      val httpRequest = req
+      lazy val remoteSocketAddress = new InetSocketAddress(0)
+    })
+
+  private[compat] def out(rep: httpx.Response): Future[netty.HttpResponse] =
+    if (rep.isChunked) Future.exception(NoStreaming)
+    else Future.value(rep.httpResponse)
 }
