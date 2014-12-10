4a08a18409cf6ca8b867a3df277d475b5661f4cf
Steve Gury <stevegury@twitter.com>
2014-12-10 22:23:25 +0000

    Update sbt projet definitions of (util, ostrich, scoorge, twitter-server)
    Problem
    
    In order to release our libraries for the open-source community, we need to update
    the version of those.
    
    Solution
    
    Update the version in the sbt project definition.
    While I was there, update also sbt to the latest version and increase the jvm
    configuration for the code cache.
    
    RB_ID=537603

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Finagle extends Build {
-  val libVersion = "6.22.0"
+  val libVersion = "6.22.2"
   val zkVersion = "3.3.4"
   val utilVersion = "6.22.2"
   val ostrichVersion = "9.6.3"
