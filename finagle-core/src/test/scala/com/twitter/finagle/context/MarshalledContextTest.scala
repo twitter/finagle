425cc1f621c273ff13b786bdcd1fbf30e635e89e
marius a. eriksen <marius@twitter.com>
2014-12-18 19:50:53 +0000

    finagle-core: remove Context.letEnv; add shadowing Context.let
    Problem
    
    Context.letEnv had the wrong semantics: the previous environment
    should not be discarded (except intentionally through Context.letEnv).
    This caused a bug where Client Ids were no longer propagated from
    legacy clients.
    
    Solution
    
    Introduce Context.let, with proper shadowing semantics.
    
    RB_ID=543820

--- finagle-core/src/test/scala/com/twitter/finagle/context/MarshalledContextTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/context/MarshalledContextTest.scala
@@ -38,8 +38,25 @@ class MarshalledContextTest extends FunSuite with AssertionsForJUnit {
    assert(ctx.marshal(env).toMap === Map(
      Buf.Utf8("a.key") -> Buf.Utf8("ok"),
      Buf.Utf8("b.key") -> Buf.U32BE(123)))
+
    env = env.bound(b, 321)
    assert(ctx.marshal(env).toMap === Map(
+     Buf.Utf8("a.key") -> Buf.Utf8("ok"),
+     Buf.Utf8("b.key") -> Buf.U32BE(321)))
+
+   assert(ctx.marshal(ctx.OrElse(env, ctx.Empty)).toMap === Map(
+     Buf.Utf8("a.key") -> Buf.Utf8("ok"),
+     Buf.Utf8("b.key") -> Buf.U32BE(321)))
+  }
+
+  test("Only marshal the most recent binding for a given key (OrElse)") {
+   val env1 = ctx.Empty.bound(a, "ok").bound(b, 123)
+   assert(ctx.marshal(env1).toMap === Map(
+     Buf.Utf8("a.key") -> Buf.Utf8("ok"),
+     Buf.Utf8("b.key") -> Buf.U32BE(123)))
+     
+   val env2 = ctx.Empty.bound(b, 321)
+   assert(ctx.marshal(ctx.OrElse(env2, env1)).toMap === Map(
      Buf.Utf8("a.key") -> Buf.Utf8("ok"),
      Buf.Utf8("b.key") -> Buf.U32BE(321)))
   }
@@ -60,6 +77,7 @@ class MarshalledContextTest extends FunSuite with AssertionsForJUnit {
       Buf.Utf8("bleep") -> Buf.Utf8("NOPE")))
   }
   
+
   test("Translucency: convert ok") {
     var env = ctx.Empty: ctx.Env
     
