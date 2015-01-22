c756991c3a9295f538ca0fbe154cf0c8ad5c680f
Oliver Gould <ver@twitter.com>
2015-01-22 23:21:23 +0000

    [finagle,t-s-i] support bound residuals on serverset names
    Problem:
    
    The serverset namer treats the whole name as a zk path, and so it cannot handle residual paths.
    
    Solution:
    
    Introduce a new twitter-internal serverset namer, com.twitter.fixedserverset,
    that expects serversets to be named in the form:
      /HOST/twitter/service/ROLE/ENV/JOB[:PORT]
    
    Names that do not meet this expectation are resolved negatively (and will fall
    back to the old com.twitter.serverset namer that uses the full path and does
    not support residuals).
    
    RB_ID=535469

--- finagle-serversets/src/main/scala/com/twitter/serverset.scala
+++ finagle-serversets/src/main/scala/com/twitter/serverset.scala
@@ -1,17 +1,57 @@
 package com.twitter
 
 import com.twitter.app.GlobalFlag
-import com.twitter.finagle.serverset2.Zk2Resolver
-import com.twitter.finagle.zookeeper.ZkResolver
-import com.twitter.finagle.{Addr, Dtab, NameTree, Namer, Resolver, Path, WeightedInetSocketAddress, Name}
-import com.twitter.util.Activity
-import java.net.InetSocketAddress
+import com.twitter.finagle.{Addr, Dtab, NameTree, Namer, Resolver, Path, Name}
+import com.twitter.util.{Var, Activity}
 
 object newZk extends GlobalFlag(
   true,
   "If set to true, the new zk2 com.twitter.finagle.Resolver is used. Otherwise, " +
   "an older, less reliable zookeeper client is used."
 )
+
+/**
+ * A namer for serverset paths of the form /zk-hosts/path... where zk-hosts is
+ * a zk connect string like 'zk.foo.com:2181'.  Naming is performed by way of a
+ * Resolver.  The specific zk resolver implementation is controlled by the
+ * `com.twitter.newZk` flag.
+ */
+private[twitter] trait BaseServersetNamer extends Namer {
+  private[this] val scheme = if (newZk()) "zk2" else "zk"
+
+  /** Resolve a resolver string to a Var[Addr]. */
+  protected[this] def resolve(spec: String): Var[Addr] = Resolver.eval(spec) match {
+    case Name.Bound(addr) => addr
+    case _ => Var.value(Addr.Neg)
+  }
+
+  protected[this] def resolveServerset(hosts: String, path: String) = 
+    resolve(s"$scheme!$hosts!$path")
+
+  protected[this] def resolveServerset(hosts: String, path: String, endpoint: String) = 
+    resolve(s"$scheme!$hosts!$path!$endpoint")
+
+  /** Bind a name. */
+  protected[this] def bind(path: Path): Option[Name.Bound]
+
+  // We have to involve a serverset roundtrip here to return a tree. We run the
+  // risk of invalidating an otherwise valid tree when there is a bad serverset
+  // on an Alt branch that would never be taken. A potential solution to this
+  // conundrum is to introduce some form of lazy evaluation of name trees.
+  def lookup(path: Path): Activity[NameTree[Name]] = bind(path) match {
+    case Some(name) =>
+      // We have to bind the name ourselves in order to know whether
+      // it resolves negatively.
+      Activity(name.addr map {
+        case Addr.Bound(_, _) => Activity.Ok(NameTree.Leaf(name))
+        case Addr.Neg => Activity.Ok(NameTree.Neg)
+        case Addr.Pending => Activity.Pending
+        case Addr.Failed(exc) => Activity.Failed(exc)
+      })
+
+    case None => Activity.value(NameTree.Neg)
+  }
+}
 
 /**
  * The serverset namer takes [[com.twitter.finagle.Path Paths]] of the form
@@ -33,42 +73,26 @@ object newZk extends GlobalFlag(
  * is the endpoint `http` of serverset `/twitter/service/cuckoo/prod/read` on
  * the ensemble `sdzookeeper.local.twitter.com:2181`.
  */
-class serverset extends Namer {
-  private[this] val whichZk = if (newZk()) "zk2" else "zk"
+class serverset extends BaseServersetNamer {
+  private[this] val idPrefix = Path.Utf8("$", "com.twitter.serverset")
 
-  // We have to involve a serverset roundtrip here to return a tree. We run the
-  // risk of invalidating an otherwise valid tree when there is a bad serverset
-  // on an Alt branch that would never be taken. A potential solution to this
-  // conundrum is to introduce some form of lazy evaluation of name trees.
-  def lookup(path: Path): Activity[NameTree[Name]] = path match {
+  protected[this] def bind(path: Path): Option[Name.Bound] = path match {
     case Path.Utf8(hosts, rest@_*) =>
-      val spec = if (rest.nonEmpty && (rest.last contains ":")) {
+      val addr = if (rest.nonEmpty && (rest.last contains ":")) {
         val Array(name, endpoint) = rest.last.split(":", 2)
-        val path = rest.init :+ name
-        "%s!%s!/%s!%s".format(whichZk, hosts,  path mkString "/", endpoint)
+        val zkPath = (rest.init :+ name).mkString("/", "/", "")
+        resolveServerset(hosts, zkPath, endpoint)
       } else {
-        "%s!%s!/%s".format(whichZk, hosts, rest mkString "/")
+        val zkPath = rest.mkString("/", "/", "")
+        resolveServerset(hosts, zkPath)
       }
 
-      val Name.Bound(va) = Resolver.eval(spec)
       // Clients may depend on Name.Bound ids being Paths which resolve
       // back to the same Name.Bound
-      val id = Path.Utf8("$", "com.twitter.serverset") ++ path
-      val name = Name.Bound(va, id)
+      val id = idPrefix ++ path
+      Some(Name.Bound(addr, id))
 
-      // We have to bind the name ourselves in order to know whether
-      // it resolves negatively.
-      Activity(va map {
-        case Addr.Bound(_, _) => Activity.Ok(NameTree.Leaf(name))
-        case Addr.Neg => Activity.Ok(NameTree.Neg)
-        case Addr.Pending => Activity.Pending
-        case Addr.Failed(exc) => Activity.Failed(exc)
-      })
-
-
-    case _ =>
-      Activity.exception(new Exception("Invalid com.twitter.namer path "+path.show))
+    case _ => None
   }
 
-  def enum(prefix: Path): Activity[Dtab] = Activity.value(Dtab.empty)
 }
