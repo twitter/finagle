3c51a771f6818ee1df1fc87c6b7740ba035495cb
Kevin Oliver <koliver@twitter.com>
2015-01-30 02:10:34 +0000

    finagle-core: Reduce logging in ClientRegistry
    Problem
    
    For large clusters, `c.t.f.client.ClientRegistry` is quite
    verbose when outputting the resolved `Addr`.
    
    Solution
    
    Reduce the amount logged in the common case of `Addr.Bound`.
    
    Existing behavior can be kept by setting log level to `Level.FINER`.
    
    RB_ID=567404

--- finagle-core/src/main/scala/com/twitter/finagle/client/ClientRegistry.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/client/ClientRegistry.scala
@@ -2,7 +2,7 @@ package com.twitter.finagle.client
 
 import com.twitter.finagle.loadbalancer.LoadBalancerFactory
 import com.twitter.finagle.util.StackRegistry
-import com.twitter.finagle.{Addr, param, Stack}
+import com.twitter.finagle.{Addr, param}
 import com.twitter.util.Future
 import java.util.logging.Level
 
@@ -21,10 +21,22 @@ private[twitter] object ClientRegistry extends StackRegistry {
       val LoadBalancerFactory.Dest(va) = params[LoadBalancerFactory.Dest]
       val param.Logger(log) = params[param.Logger]
 
-      val resolved = va.changes.filter(_ != Addr.Pending).toFuture
+      val resolved = va.changes.filter(_ != Addr.Pending).toFuture()
       resolved map { resolution =>
-        log.info(s"${name} params ${params}")
-        log.info(s"${name} resolved to ${resolution}")
+        // the full resolution can be rather verbose for large clusters,
+        // so be stingy with our output
+        log.fine(s"${name} params ${params}")
+        if (log.isLoggable(Level.FINER)) {
+          log.finer(s"${name} resolved to ${resolution}")
+        } else {
+          resolution match {
+            case bound: Addr.Bound =>
+              log.info(s"${name} resolved to Addr.Bound, current size=${bound.addrs.size}")
+            case _ =>
+              log.info(s"${name} resolved to ${resolution}")
+          }
+        }
+
         name
       }
     }
