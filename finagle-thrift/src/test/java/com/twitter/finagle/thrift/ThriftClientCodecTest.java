f4335c7f849e66467e3af7302f4e8650f6a4f71f
Tony Dong <tonyd@tellapart.com>
2015-02-09 11:55:14 -0800

    Add unit test for ThriftClientCodec.

---
+++ finagle-thrift/src/test/java/com/twitter/finagle/thrift/ThriftClientCodecTest.java
@@ -0,0 +1,31 @@
+/**
+ * @copyright - Copyright (C) 2015 TellApart, Inc. All Rights Reserved.
+ */
+package com.twitter.finagle.thrift;
+
+import com.twitter.finagle.stats.DefaultStatsReceiver;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import scala.Option;
+
+public class ThriftClientCodecTest {
+
+  @Test
+  public void testCodecFactoryCreation() {
+    // ThriftClientBufferedCodec
+    Assert.assertTrue(ThriftClientBufferedCodec.get() instanceof ThriftClientBufferedCodecFactory);
+    Assert.assertTrue(
+        ThriftClientBufferedCodec.apply() instanceof ThriftClientBufferedCodecFactory);
+    Assert.assertTrue(
+        ThriftClientBufferedCodec.apply(Protocols.factory(DefaultStatsReceiver.self()))
+            instanceof ThriftClientBufferedCodecFactory);
+
+    //ThriftClientFramedCodec
+    Assert.assertTrue(ThriftClientFramedCodec.get() instanceof ThriftClientFramedCodecFactory);
+    Assert.assertTrue(
+        ThriftClientFramedCodec
+            .apply(Option.<ClientId>empty()) instanceof ThriftClientFramedCodecFactory);
+  }
+}
