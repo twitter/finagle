294ebf79e35095ebe92d5f74ff26e5bf5a5c9f97
Kevin Oliver <koliver@twitter.com>
2015-05-12 16:37:25 +0000

    finagle-kestrel(x): Longer Awaits for tests
    Problem
    
    These tests had been marked as flakey.
    
    Solution
    
    Increase the time for Await in the case of a slow
    CI machine.
    
    RB_ID=660798

--- finagle-kestrelx/src/test/scala/com/twitter/finagle/kestrelx/integration/InterpreterServiceTest.scala
+++ finagle-kestrelx/src/test/scala/com/twitter/finagle/kestrelx/integration/InterpreterServiceTest.scala
@@ -7,9 +7,7 @@ import com.twitter.finagle.kestrelx.Server
 import com.twitter.finagle.kestrelx.protocol._
 import com.twitter.io.Buf
 import com.twitter.util.{Await, Time}
-
-import java.net.{InetSocketAddress, InetAddress}
-
+import java.net.{InetAddress, InetSocketAddress}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -19,7 +17,7 @@ class InterpreterServiceTest extends FunSuite {
   val queueName = Buf.Utf8("name")
   val value = Buf.Utf8("value")
 
-  def exec(fn: (Server, Service[Command, Response]) => Unit) {
+  def exec(fn: Service[Command, Response] => Unit) {
     val server: Server = new Server(new InetSocketAddress(InetAddress.getLoopbackAddress, 0))
     val address: InetSocketAddress = server.start().boundAddress.asInstanceOf[InetSocketAddress]
     val client: Service[Command, Response] = ClientBuilder()
@@ -28,31 +26,32 @@ class InterpreterServiceTest extends FunSuite {
       .hostConnectionLimit(1)
       .build()
 
-    fn(server, client)
+    fn(client)
 
     server.stop()
+    client.close()
   }
 
   test("InterpreterService should set & get") {
-    exec { case (server, client) =>
+    exec { client =>
       val result = for {
         _ <- client(Flush(queueName))
         _ <- client(Set(queueName, Time.now, value))
         r <- client(Get(queueName))
       } yield r
-      assert(Await.result(result, 1.second) === Values(Seq(Value(queueName, value))))
+      assert(Await.result(result, 5.seconds) === Values(Seq(Value(queueName, value))))
     }
   }
 
   test("InterpreterService: transactions should set & get/open & get/abort") {
-    exec { case (server, client) =>
+    exec { client =>
       val result = for {
         _ <- client(Set(queueName, Time.now, value))
         _ <- client(Open(queueName))
         _ <- client(Abort(queueName))
         r <- client(Open(queueName))
       } yield r
-      assert(Await.result(result, 1.second) === Values(Seq(Value(queueName, value))))
+      assert(Await.result(result, 5.seconds) === Values(Seq(Value(queueName, value))))
     }
   }
 }
