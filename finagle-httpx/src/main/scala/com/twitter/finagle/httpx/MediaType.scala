1664d98beda05e31adda255fa3781e1daba7ddc5
Yuri Schimke <yschimke@twitter.com>
2015-06-30 22:02:05 +0000

    finagle-httpx: Adds common mimetypes
    Problem
    
    MediaType doesn't define constants for image types.
    
    Solution
    
    Add extra constants to MediaType.
    
    RB_ID=700876

--- finagle-httpx/src/main/scala/com/twitter/finagle/httpx/MediaType.scala
+++ finagle-httpx/src/main/scala/com/twitter/finagle/httpx/MediaType.scala
@@ -3,15 +3,19 @@ package com.twitter.finagle.httpx
 object MediaType {
   // Common media types
   val Atom          = "application/atom+xml"
+  val Csv           = "application/csv"
+  val Gif           = "image/gif"
   val Html          = "text/html"
   val Iframe        = "application/iframe"
   val Javascript    = "application/javascript"
+  val Jpeg          = "image/jpeg"
   val Json          = "application/json"
   val OctetStream   = "application/octet-stream"
+  val Png           = "image/png"
   val Rss           = "application/rss+xml"
+  val Txt           = "text/plain"
   val WwwForm       = "application/x-www-form-urlencoded"
   val Xml           = "application/xml"
-  val Csv           = "application/csv"
   val Xls           = "application/vnd.ms-excel"
   val Zip           = "application/zip"
 }
