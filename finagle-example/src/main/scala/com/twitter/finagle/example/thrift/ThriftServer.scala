d7fa8433be397d303308487e9e31974965cc1945
Adam Cardenas <cevaris@gmail.com>
2015-07-10 16:21:38 -0600

    Updated fingalge-thrift server/client examples

--- finagle-example/src/main/scala/com/twitter/finagle/example/thrift/ThriftServer.scala
+++ finagle-example/src/main/scala/com/twitter/finagle/example/thrift/ThriftServer.scala
@@ -1,16 +1,30 @@
 package com.twitter.finagle.example.thrift
 
+import java.net.InetSocketAddress
+
+import org.apache.thrift.protocol.TBinaryProtocol
+
+import com.twitter.finagle.builder.{ServerBuilder, Server}
 import com.twitter.finagle.example.thriftscala.Hello
-import com.twitter.finagle.Thrift
+import com.twitter.finagle.thrift.ThriftServerFramedCodec
 import com.twitter.util.{Await, Future}
 
 object ThriftServer {
   def main(args: Array[String]) {
     //#thriftserverapi
-    val server = Thrift.serveIface("localhost:8080", new Hello[Future] {
-      def hi() = Future.value("hi")
-    })
-    Await.ready(server)
+    val impl = new Hello.FutureIface {
+      def hi() = {
+        Future("hi")
+      }
+    }
+
+    val service = new Hello.FinagledService(impl, new TBinaryProtocol.Factory())
+
+    val server: Server = ServerBuilder()
+      .bindTo(new InetSocketAddress(8080))
+      .codec(ThriftServerFramedCodec())
+      .name(Hello.toString)
+      .build(service)
     //#thriftserverapi
   }
 }
