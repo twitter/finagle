de123b8f9d074c4e345ebd67e1a0e870bb921544
Travis Brown <tbrown@twitter.com>
2015-08-27 22:20:46 +0000

    Release CSL stack
    RB_ID=735615

--- project/Build.scala
+++ project/Build.scala
@@ -13,10 +13,10 @@ object Finagle extends Build {
   val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
   val suffix = if (branch == "master") "" else "-SNAPSHOT"
 
-  val libVersion = "6.27.0" + suffix
-  val utilVersion = "6.26.0" + suffix
-  val ostrichVersion = "9.10.0" + suffix
-  val scroogeVersion = "3.20.0" + suffix
+  val libVersion = "6.28.0" + suffix
+  val utilVersion = "6.27.0" + suffix
+  val ostrichVersion = "9.11.0" + suffix
+  val scroogeVersion = "4.0.0" + suffix
 
   val nettyLib = "io.netty" % "netty" % "3.10.1.Final"
   val ostrichLib = "com.twitter" %% "ostrich" % ostrichVersion
