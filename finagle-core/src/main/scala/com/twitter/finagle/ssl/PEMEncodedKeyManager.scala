224cfd34c81c7e9f759e4a33215bbcd5bdfa04a7
Bill Dimmick <bdimmick@twitter.com>
2016-05-13 22:00:14 +0000

    finagle-core: remove the use of keytool in TLS setup
    Problem:
    Finagle shells out to keytool as part of its loading of keys for TLS setup.
    
    The Finagle TLS setup code currently calls keytool to convert a pkcs12 file to a java keystore for subsequent loading and use in TLS. This is redundant, as directly loading pkcs12 files as keystores has been supported in the JVM for some time. Also, it assumes that all processes being launched have JAVA_HOME in their path and pointing to a JDK (instead of a JRE).
    
    Solution:
    
    The call to keytool has been removed and the code now directly loads the pkcs12 file. Other minor code changes to use more appropriate classes for handling this data and updated comments to refelct these changes.
    
    RB_ID=832070

--- finagle-core/src/main/scala/com/twitter/finagle/ssl/PEMEncodedKeyManager.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/ssl/PEMEncodedKeyManager.scala
@@ -1,15 +1,13 @@
 package com.twitter.finagle.ssl
 
+import com.twitter.io.{TempDirectory, StreamIO}
 import java.io._
-import java.security.KeyStore
-import java.util.Random
+import java.security.{KeyStore, SecureRandom}
 import javax.net.ssl._
 
-import com.twitter.io.{Files, TempDirectory, StreamIO}
-
-/*
- * Take a PEM-encoded cert and key, and turn it into a Java keystore
- * which is then loaded into a resulting KeyManager
+/**
+ * Take a PEM-encoded cert and key and turn it into a PKCS#12 keystore
+ * via openssl, which is then loaded into a resulting KeyManager
  *
  * @param certificatePath the path to the PEM-encoded certificate
  * @param keyPath the path to the PEM-encoded private key
@@ -27,15 +25,15 @@ object PEMEncodedKeyManager {
     keyPath: String,
     caCertPath: Option[String]
   ): Array[KeyManager] =
-    makeKeystore(
-      Files.readBytes(new File(certificatePath)),
-      Files.readBytes(new File(keyPath)),
-      caCertPath map { filename => Files.readBytes(new File(filename)) }
-    )
+    asStream(keyPath) { keyStream =>
+      // if the chain is present, use it instead of the cert (chain contains cert)
+      asStream(caCertPath.getOrElse(certificatePath)) { certificateStream =>
+         makeKeystore(certificateStream, keyStream)
+      }
+    }
 
   private[this] def secret(length: Int): Array[Char] = {
-    val rng = new Random()
-    rng.setSeed(System.currentTimeMillis())
+    val rng = new SecureRandom()
     val b = new Array[Char](length)
 
     for (i <- 0 until length)
@@ -44,10 +42,18 @@ object PEMEncodedKeyManager {
     b
   }
 
+  private[this] def asStream[T](filename: String)(f: (InputStream) => T): T = {
+    val stream = new FileInputStream(filename)
+    try {
+      f(stream)
+    } finally {
+      stream.close()
+    }
+  }
+
   private[this] def makeKeystore(
-    certificate: Array[Byte],
-    key: Array[Byte],
-    caCert: Option[Array[Byte]]
+    certificateStream: InputStream,
+    keyStream: InputStream
   ) : Array[KeyManager] = {
 
     // Create a secure directory for the conversion
@@ -62,18 +68,12 @@ object PEMEncodedKeyManager {
     val fn = new String(secret(12))
     val pemPath = path + File.separator + "%s.pem".format(fn)
     val p12Path = path + File.separator + "%s.p12".format(fn)
-    val jksPath = path + File.separator + "%s.jks".format(fn)
 
     // Write out the certificate and key
     val f = new FileOutputStream(new File(pemPath))
-    // if the chain is present, use it instead of the cert (chain contains cert)
-    caCert match {
-      case Some(c) => StreamIO.copy(new ByteArrayInputStream(c), f)
-      case None => StreamIO.copy(new ByteArrayInputStream(certificate), f)
-    }
-    StreamIO.copy(new ByteArrayInputStream(key), f)
+    StreamIO.copy(certificateStream, f)
+    StreamIO.copy(keyStream, f)
     f.close()
-
 
     // Import the PEM-encoded certificate and key to a PKCS12 file
     Shell.run(
@@ -86,32 +86,19 @@ object PEMEncodedKeyManager {
       )
     )
 
-    // Convert the PKCS12 file into a Java keystore
-    Shell.run(
-      Array(
-        "keytool",
-        "-importkeystore",
-        "-srckeystore",  p12Path,
-        "-srcstoretype", "PKCS12",
-        "-destkeystore", jksPath,
-        "-trustcacerts",
-        "-srcstorepass", passwordStr,
-        "-keypass",      passwordStr,
-        "-storepass",    passwordStr
-      )
-    )
-
     // Read the resulting keystore
-    val input = new ByteArrayInputStream(Files.readBytes(new File(jksPath)))
-    val ks = KeyStore.getInstance("JKS")
-    val kmf = KeyManagerFactory.getInstance("SunX509")
-    ks.load(input, password)
-    kmf.init(ks, password)
+    val keystore = asStream(p12Path) { stream =>
+      val ks = KeyStore.getInstance("pkcs12")
+      ks.load(stream, password)
+      ks
+    }
 
     // Clean up by deleting the files and directory
-    Array(pemPath, p12Path, jksPath).foreach(new File(_).delete())
+    Seq(pemPath, p12Path).foreach(new File(_).delete())
     path.delete()
 
+    val kmf = KeyManagerFactory.getInstance("SunX509")
+    kmf.init(keystore, password)
     kmf.getKeyManagers
   }
 }
