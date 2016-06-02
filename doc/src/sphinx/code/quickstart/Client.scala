a60909d4996ff87d016187fe4abf2f899d466e75
Kevin Oliver <koliver@twitter.com>
2016-06-02 20:25:48 +0000

    finagle-doc: Fix up the quickstart
    Problem / Solution
    
    The quickstart had a few problems as noted in
    https://github.com/twitter/finagle/issues/477
    
    RB_ID=838282

--- doc/src/sphinx/code/quickstart/Client.scala
+++ doc/src/sphinx/code/quickstart/Client.scala
@@ -7,7 +7,7 @@ object Client extends App {
   val client: Service[http.Request, http.Response] = Http.newService("www.scala-lang.org:80")
   //#builder
   //#dispatch
-  val request  = http.Request(http.Method.Get, "/")
+  val request = http.Request(http.Method.Get, "/")
   request.host = "www.scala-lang.org"
   val response: Future[http.Response] = client(request)
   //#dispatch
--- doc/src/sphinx/code/quickstart/Proxy.scala
+++ doc/src/sphinx/code/quickstart/Proxy.scala
@@ -5,7 +5,7 @@ import com.twitter.util.Await
 //#app
 object Proxy extends App {
   val client: Service[Request, Response] =
-    Http.newService("www.google.com:80")
+    Http.newService("twitter.com:80")
 
   val server = Http.serve(":8080", client)
   Await.ready(server)
