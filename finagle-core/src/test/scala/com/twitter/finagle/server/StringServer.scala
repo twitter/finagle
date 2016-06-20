460324221433c95afa12a99c1b518e02ef511593
Moses Nakamura <mnakamura@twitter.com>
2016-06-20 22:29:08 +0000

    finagle-{core,netty4}: Adds tests for cutting the connection
    Problem
    
    Still trying to narrow down why netty4 doesn't see cut connections.  Feels like we're getting closer.
    
    Solution
    
    Added more tests
    
    RB_ID=843795

--- finagle-core/src/test/scala/com/twitter/finagle/server/StringServer.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/server/StringServer.scala
@@ -1,13 +1,21 @@
 package com.twitter.finagle.server
 
+import com.twitter.conversions.time._
 import com.twitter.finagle._
 import com.twitter.finagle.transport.Transport
 import com.twitter.finagle.dispatch.SerialServerDispatcher
 import com.twitter.finagle.netty3.Netty3Listener
 import com.twitter.io.Charsets
+import com.twitter.util.{Promise, Await}
+import java.net.{SocketAddress, Socket, InetSocketAddress, InetAddress}
 import org.jboss.netty.channel._
 import org.jboss.netty.handler.codec.frame.{Delimiters, DelimiterBasedFrameDecoder}
 import org.jboss.netty.handler.codec.string.{StringEncoder, StringDecoder}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.concurrent.{Eventually, IntegrationPatience}
+import org.scalatest.junit.JUnitRunner
+import scala.util.control.NonFatal
 
 private[finagle] object StringServerPipeline extends ChannelPipelineFactory {
   def getPipeline = {
@@ -37,4 +45,39 @@ private[finagle] trait StringServer {
   }
 
   val stringServer = Server()
+}
+
+@RunWith(classOf[JUnitRunner])
+class StringServerTest extends FunSuite with StringServer with Eventually with IntegrationPatience {
+  test("StringServer notices when the client cuts the connection") {
+    val p = Promise[String]()
+    @volatile var interrupted = false
+    p.setInterruptHandler { case NonFatal(t) =>
+      interrupted = true
+    }
+    @volatile var observedRequest: Option[String] = None
+
+    val service = new Service[String, String] {
+      def apply(request: String) = {
+        observedRequest = Some(request)
+        p
+      }
+    }
+
+    val server = stringServer.serve(
+      new InetSocketAddress(InetAddress.getLoopbackAddress, 0),
+      service)
+
+    val client = new Socket()
+    eventually { client.connect(server.boundAddress) }
+
+    client.getOutputStream.write("hello netty4!\n".getBytes("UTF-8"))
+    client.getOutputStream.flush()
+    eventually { assert(observedRequest == Some("hello netty4!")) }
+
+    client.close()
+    eventually { assert(interrupted) }
+
+    Await.ready(server.close(), 2.seconds)
+  }
 }
--- finagle-netty4/src/test/scala/com/twitter/finagle/netty4/Netty4ListenerTest.scala
+++ finagle-netty4/src/test/scala/com/twitter/finagle/netty4/Netty4ListenerTest.scala
@@ -24,6 +24,14 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class Netty4ListenerTest extends FunSuite with Eventually with IntegrationPatience {
 
+  object StringServerInit extends (ChannelPipeline => Unit) {
+    def apply(pipeline: ChannelPipeline): Unit = {
+      pipeline.addLast("line", new DelimiterBasedFrameDecoder(100, Delimiters.lineDelimiter(): _*))
+      pipeline.addLast("stringDecoder", new StringDecoder(Charsets.Utf8))
+      pipeline.addLast("stringEncoder", new StringEncoder(Charsets.Utf8))
+    }
+  }
+
   // a Transport whose reads and writes never complete
   private[netty4] class NullTransport[In,Out] extends Transport[In, Out] {
     val closeP = new Promise[Throwable]
@@ -74,14 +82,6 @@ class Netty4ListenerTest extends FunSuite with Eventually with IntegrationPatien
   test("frames pipeline messages and bridges transports and service dispatchers (aka it works end-to-end)") {
     val ctx = new StatsCtx { }
     import ctx._
-
-    object StringServerInit extends (ChannelPipeline => Unit) {
-      def apply(pipeline: ChannelPipeline): Unit = {
-        pipeline.addLast("line", new DelimiterBasedFrameDecoder(100, Delimiters.lineDelimiter(): _*))
-        pipeline.addLast("stringDecoder", new StringDecoder(Charsets.Utf8))
-        pipeline.addLast("stringEncoder", new StringEncoder(Charsets.Utf8))
-      }
-    }
 
     val p = Params.empty + Label("test") + Stats(sr)
     val listener = Netty4Listener[String, String](StringServerInit, p)
@@ -200,6 +200,47 @@ class Netty4ListenerTest extends FunSuite with Eventually with IntegrationPatien
     client3.getOutputStream.write(1)
 
     eventually { counterEquals("received_bytes")(3) }
+    Await.ready(server.close(), 2.seconds)
+  }
+
+  test("Netty4Listener notices when the client cuts the connection with autoread enabled") {
+    val ctx = new StatsCtx { }
+    import ctx._
+
+    // we need to turn backpressure off because netty doesn't notify you if the
+    // client cut the connection unless you're reading or writing.  disabling
+    // backpressure turns on autoread, so netty will alert us immediately that
+    // the connection was cut.
+    val params = Params.empty + Label("test") + Stats(sr) + BackPressure(false)
+    val listener = Netty4Listener[String, String](StringServerInit, params)
+
+    @volatile var observedRequest: Option[String] = None
+
+    val p = Promise[String]()
+    @volatile var interrupted = false
+    p.setInterruptHandler { case NonFatal(t) =>
+      interrupted = true
+    }
+    val service = new Service[String, String] {
+      def apply(request: String) = {
+        observedRequest = Some(request)
+        p
+      }
+    }
+
+    val serveTransport = (t: Transport[String, String]) => new SerialServerDispatcher(t, service)
+    val server = listener.listen(new InetSocketAddress(InetAddress.getLoopbackAddress, 0))(serveTransport(_))
+
+    val client = new Socket()
+    eventually { client.connect(server.boundAddress) }
+    client.getOutputStream.write("hello netty4!\n".getBytes("UTF-8"))
+    client.getOutputStream.flush()
+
+    eventually { assert(observedRequest == Some("hello netty4!")) }
+
+    client.close()
+    eventually { assert(interrupted) }
+
     Await.ready(server.close(), 2.seconds)
   }
 }
