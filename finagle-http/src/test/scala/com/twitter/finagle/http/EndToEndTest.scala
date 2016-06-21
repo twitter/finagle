e755f16dee90fc0dc17a342d840dc286ef5544b0
Alfonso Chartier <achartier@twitter.com>
2016-06-21 08:27:50 +0000

    Commenting out flaky test EndToEndTest (finagle/finagle-http/src/test/scala:scala)
    finagle/finagle-http/src/test/scala:scala failed in CI builds of the master branch and is believed to be flaky.
    
    A log of the failure is available: https://ton.smf1.twitter.com/ci/build_logs/source_ci/2016-06-20_22-58-04.html
    
    Please investigate the unit test and attempt to address the flakiness. Once you have either
    - fixed the test and uncommented the EndToEndTest class, or
    - removed the test
    
    resolve this ticket.
    
    If you believe that there is nothing wrong with your test and that this ticket has been filed in error,
    please contact ci-team@twitter.com.
    
    To reproduce locally (see http://go/iron-tryout for details):
    devprod/iron/scripts/user/iron-tryout.sh finagle/finagle-http/src/test/scala:scala 100
    
    For more information see http://go/ci-failures.
    
    Thank you.
    
    RB_ID=844713
    TBR=true

--- finagle-http/src/test/scala/com/twitter/finagle/http/EndToEndTest.scala
+++ finagle-http/src/test/scala/com/twitter/finagle/http/EndToEndTest.scala
@@ -1,12 +1,16 @@
 package com.twitter.finagle.http
 
+/* CSL-2943
 import com.twitter.finagle
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+*/
 
+/* CSL-2943
 @RunWith(classOf[JUnitRunner])
 class EndToEndTest extends AbstractEndToEndTest {
   def clientImpl(): finagle.Http.Client = finagle.Http.client
   def serverImpl(): finagle.Http.Server = finagle.Http.server
   def featureImplemented(feature: Feature): Boolean = true
 }
+*/
