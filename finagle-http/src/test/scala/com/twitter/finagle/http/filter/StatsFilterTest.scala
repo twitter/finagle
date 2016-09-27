7efadab21e2bbd097f2ad1d837c7dcca6f9b8dc3
Alessandro Vermeulen <github@spockz.nl>
2016-09-27 22:01:11 +0200

    Separate in two tests.

--- finagle-http/src/test/scala/com/twitter/finagle/http/filter/StatsFilterTest.scala
+++ finagle-http/src/test/scala/com/twitter/finagle/http/filter/StatsFilterTest.scala
@@ -12,17 +12,35 @@ import org.mockito.Mockito.{spy,verify,times}
 @RunWith(classOf[JUnitRunner])
 class StatsFilterTest extends FunSuite {
 
+  val service = new Service[Request, Response] {
+    def apply(request: Request): Future[Response] = {
+      val response = request.response
+      response.statusCode = 404
+      response.write("hello")
+      Future.value(response)
+    }
+  }
+
   test("increment stats") {
     val receiver = spy(new InMemoryStatsReceiver)
 
-    val filter = new StatsFilter(receiver) andThen new Service[Request, Response] {
-      def apply(request: Request): Future[Response] = {
-        val response = request.response
-        response.statusCode = 404
-        response.write("hello")
-        Future.value(response)
-      }
+    val filter = new StatsFilter(receiver) andThen service
+
+    Time.withCurrentTimeFrozen { _ =>
+      Await.result(filter(Request()))
     }
+
+    assert(receiver.counters(Seq("status", "404")) == 1)
+    assert(receiver.counters(Seq("status", "4XX")) == 1)
+    assert(receiver.stats(Seq("time", "404")) == Seq(0.0))
+    assert(receiver.stats(Seq("time", "4XX")) == Seq(0.0))
+    assert(receiver.stats(Seq("response_size")) == Seq(5.0))
+  }
+
+  test("status and time counters and stats are memoised") {
+    val receiver = spy(new InMemoryStatsReceiver)
+
+    val filter = new StatsFilter(receiver) andThen service
 
     Time.withCurrentTimeFrozen { _ =>
       Await.result(filter(Request()))
@@ -30,16 +48,10 @@ class StatsFilterTest extends FunSuite {
     }
 
     // Verify that the counters and stats were only created once
-    verify(receiver, times(1)).counter("status", "404")
-    verify(receiver, times(1)).counter("status", "4XX")
-    verify(receiver, times(1)).stat("time", "404")
-    verify(receiver, times(1)).stat("time", "4XX")
-    verify(receiver, times(1)).stat("response_size")
-
-    assert(receiver.counters(Seq("status", "404")) === 2)
-    assert(receiver.counters(Seq("status", "4XX")) === 2)
-    assert(receiver.stats(Seq("time", "404")) === Seq(0.0, 0.0))
-    assert(receiver.stats(Seq("time", "4XX")) === Seq(0.0, 0.0))
-    assert(receiver.stats(Seq("response_size")) === Seq(5.0, 5.0))
+    verify(receiver).counter("status", "404")
+    verify(receiver).counter("status", "4XX")
+    verify(receiver).stat("time", "404")
+    verify(receiver).stat("time", "4XX")
+    verify(receiver).stat("response_size")
   }
 }
