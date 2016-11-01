865e018a94565347c7eee1cd1fc222cf2beee81c
Masaru Nomura <massa.nomura@gmail.com>
2016-11-02 00:04:35 +0900

    [review fix] Fix ResponseConformanceFilterTest
    - remove netty dependency
    - move test(...) into a loop

--- finagle-http/src/test/scala/com/twitter/finagle/http/codec/ResponseConformanceFilterTest.scala
+++ finagle-http/src/test/scala/com/twitter/finagle/http/codec/ResponseConformanceFilterTest.scala
@@ -79,22 +79,24 @@ class ResponseConformanceFilterTest extends FunSuite {
     intercept[ReaderDiscarded] { Await.result(res.writer.write(Buf.Empty), 5.seconds) }
   }
 
-  test("response with status code {1xx, 204 and 304} must not have a message body nor Content-Length header field") {
-    def validate(status: Status) = {
+  List(Continue, SwitchingProtocols, Processing, NoContent, NotModified).foreach { status =>
+    test(s"response with status code ${status.code} must not have a message body nor " +
+         "Content-Length header field"
+    ) {
       val res = Response(Version.Http11, status)
       val response = fetchResponse(res)
 
       assert(response.status == status)
-      assert(response.getContent.readableBytes() == 0)
+      assert(response.length == 0)
       assert(!response.isChunked)
-      assert(response.headers().get(Fields.ContentLength) == null)
+      assert(response.headerMap.get(Fields.ContentLength).isEmpty)
     }
-
-    List(Continue, SwitchingProtocols, Processing, NoContent, NotModified).foreach(validate(_))
   }
 
-  test("response with status code {1xx, 204 and 304} must not have a message body nor Content-Length header field when non-empty body is returned") {
-    def validate(status: Status) = {
+  List(Continue, SwitchingProtocols, Processing, NoContent, NotModified).foreach { status =>
+    test(s"response with status code ${status.code} must not have a message body nor " +
+         "Content-Length header field when non-empty body is returned"
+    ) {
       val body = Buf.Utf8("some data")
       val res = Response(Version.Http11, status)
       res.content = body
@@ -102,16 +104,16 @@ class ResponseConformanceFilterTest extends FunSuite {
       val response = fetchResponse(res)
 
       assert(response.status == status)
-      assert(response.getContent().readableBytes() == 0)
+      assert(response.length == 0)
       assert(!response.isChunked)
-      assert(response.headers().get(Fields.ContentLength) == null)
+      assert(response.headerMap.get(Fields.ContentLength).isEmpty)
     }
-
-    List(Continue, SwitchingProtocols, Processing, NoContent, NotModified).foreach(validate(_))
   }
 
-  test("response with status code {1xx and 204} must not have a message body nor Content-Length header field when non-empty body with explicit Content-Length is returned") {
-    def validate(status: Status) = {
+  List(Continue, SwitchingProtocols, Processing, NoContent).foreach { status =>
+    test(s"response with status code ${status.code} must not have a message body nor " +
+         "Content-Length header field when non-empty body with explicit Content-Length is returned"
+    ) {
       val body = Buf.Utf8("some data")
       val res = Response(Version.Http11, status)
       res.content = body
@@ -120,15 +122,14 @@ class ResponseConformanceFilterTest extends FunSuite {
       val response = fetchResponse(res)
 
       assert(response.status == status)
-      assert(response.getContent().readableBytes() == 0)
+      assert(response.length == 0)
       assert(!response.isChunked)
-      assert(response.headers().get(Fields.ContentLength) == null)
+      assert(response.headerMap.get(Fields.ContentLength).isEmpty)
     }
-
-    List(Continue, SwitchingProtocols, Processing, NoContent).foreach(validate(_))
   }
 
-  test("response with status code 304 must not have a message body *BUT* Content-Length header field when non-empty body with explicit Content-Length is returned") {
+  test("response with status code 304 must not have a message body *BUT* Content-Length " +
+       "header field when non-empty body with explicit Content-Length is returned") {
     val body = Buf.Utf8("some data")
     val res = Response(Version.Http11, Status.NotModified)
     res.content = body
@@ -137,9 +138,9 @@ class ResponseConformanceFilterTest extends FunSuite {
     val response = fetchResponse(res)
 
     assert(response.status == Status.NotModified)
-    assert(response.getContent().readableBytes() == 0)
+    assert(response.length == 0)
     assert(!response.isChunked)
-    assert(response.headers().get(Fields.ContentLength) == body.length.toString)
+    assert(response.headerMap.get(Fields.ContentLength).contains(body.length.toString))
   }
 
   def fetchResponse(res: Response): Response = {
