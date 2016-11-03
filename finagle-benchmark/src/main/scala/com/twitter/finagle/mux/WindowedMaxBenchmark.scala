9c7c801f3b6bd0f507b51ccf9c6ef461e37d1b0f
Daniel Schobel <dschobel@twitter.com>
2016-11-03 16:10:15 +0000

    [finagle|util]-benchmark: fixup sbt invocation docs for jmh
    Problem / Solution
    
    fix sbt invocation docs for jmh benchmarks.
    
    RB_ID=887525

--- finagle-benchmark/src/main/scala/com/twitter/finagle/mux/WindowedMaxBenchmark.scala
+++ finagle-benchmark/src/main/scala/com/twitter/finagle/mux/WindowedMaxBenchmark.scala
@@ -6,7 +6,7 @@ import com.twitter.finagle.benchmark.StdBenchAnnotations
 import org.openjdk.jmh.annotations._
 import scala.util.Random
 
-// ./sbt 'project finagle-benchmark' 'run .*WindowedMaxBenchmark.*'
+// ./sbt 'project finagle-benchmark' 'jmh:run WindowedMaxBenchmark'
 @State(Scope.Benchmark)
 @Threads(16)
 @Warmup(iterations = 10, time = 3, timeUnit = TimeUnit.SECONDS)
