c14037a65c1a3b1e53544ad6ecb5a7ba67dc1511
Kevin Oliver <koliver@twitter.com>
2016-11-08 19:25:49 +0000

    finagle-core: Performance improvements to Refcounted{Service,Factory}
    Problem / Solution
    
    `RefcountedService` and `RefcountedFactory` have an easy opportunity
    for removing allocations on the fast path by lifting the closure into
    a member variable.
    
    RB_ID=888561

--- finagle-core/src/main/scala/com/twitter/finagle/service/RefcountedService.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/service/RefcountedService.scala
@@ -2,20 +2,23 @@ package com.twitter.finagle.service
 
 import com.twitter.finagle.{Service, ServiceProxy}
 import com.twitter.finagle.util.AsyncLatch
-import com.twitter.util.{Future, Promise, Time}
+import com.twitter.util.{Future, Promise, Time, Try}
 
 /**
  * A [[com.twitter.finagle.Service]] that delays closure until all outstanding
  * requests have been completed.
  */
 private[finagle] class RefcountedService[Req, Rep](underlying: Service[Req, Rep])
-  extends ServiceProxy[Req, Rep](underlying)
-{
-  protected[this] val replyLatch = new AsyncLatch
+  extends ServiceProxy[Req, Rep](underlying) {
 
-  override def apply(request: Req) = {
+  private[this] val replyLatch = new AsyncLatch()
+
+  private[this] val decrLatchFn: Try[Rep] => Unit =
+    _ => replyLatch.decr()
+
+  override def apply(request: Req): Future[Rep] = {
     replyLatch.incr()
-    underlying(request) ensure { replyLatch.decr() }
+    underlying(request).respond(decrLatchFn)
   }
 
   override final def close(deadline: Time): Future[Unit] = {
