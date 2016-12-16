78380f2e84e5a60ebc6a107df25445d1a2207743
Jillian Crossley <jillianc@twitter.com>
2016-12-16 23:54:01 +0000

    finagle/finagle-core: More documentation in PendingRequestFilter and rejections counter
    Problem
    
    It's confusing for users when they have a pending request limit
    configured, and the "/pending" stat does not suggest the client
    is respecting this limit. It's also unclear what the difference
    is between "/pending" and "/dispatcher/..../pending"
    
    Solution
    
    Add docs to clarify what stats will show the effecs of PendingRequestFilter,
    and add a counter for rejections above the limit.
    
    RB_ID=898184

--- finagle-core/src/main/scala/com/twitter/finagle/service/PendingRequestFilter.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/service/PendingRequestFilter.scala
@@ -1,6 +1,7 @@
 package com.twitter.finagle.service
 
 import com.twitter.finagle._
+import com.twitter.finagle.stats.StatsReceiver
 import com.twitter.util.Future
 import java.util.concurrent.RejectedExecutionException
 import java.util.concurrent.atomic.AtomicInteger
@@ -8,6 +9,16 @@ import java.util.concurrent.atomic.AtomicInteger
 /**
  * A module which allows clients to limit the number of pending
  * requests per connection.
+ *
+ * The effects of this filter are reflected in the stat
+ * "dispatcher/.../pending", which is a sum over per-host connections
+ * of dispatched requests between client and server. If the limit is
+ * configured as L and there are N per-host connections,
+ * "dispatcher/.../pending" will be no greater than L * N.
+ *
+ * Note that the "pending" stat recorded in c.t.f.StatsFilter can be
+ * greater than L * N because requests can be between c.t.f.StatsFilter and
+ * c.t.f.PendingRequestFilter when the stat is read.
  */
 object PendingRequestFilter {
 
@@ -23,17 +34,19 @@ object PendingRequestFilter {
   }
 
   private[finagle] def module[Req, Rep]: Stackable[ServiceFactory[Req, Rep]] =
-    new Stack.Module1[Param, ServiceFactory[Req, Rep]] {
+    new Stack.Module2[Param, param.Stats, ServiceFactory[Req, Rep]] {
+
       val role = PendingRequestFilter.role
       val description = "Restrict number of pending requests"
 
       // n.b. we can't simply compose the `PendingRequestFilter` onto the `next`
       // service factory since we need a distinct filter instance to provide
       // distinct state per-session.
-      def make(_param: Param, next: ServiceFactory[Req, Rep]) = _param match {
+      def make(_param: Param, _stats: param.Stats, next: ServiceFactory[Req, Rep]) = _param match {
         case Param(Some(limit)) =>
-          next.map(new PendingRequestFilter[Req, Rep](limit).andThen(_))
-
+          val param.Stats(stats) = _stats
+          next.map(new PendingRequestFilter[Req, Rep](
+            limit, stats.scope("pending_requests")).andThen(_))
         case Param(None) => next
       }
     }
@@ -45,11 +58,17 @@ object PendingRequestFilter {
 /**
  * A filter which limits the number of pending requests to a service.
  */
-private[finagle] class PendingRequestFilter[Req, Rep](limit: Int) extends SimpleFilter[Req, Rep] {
+private[finagle] class PendingRequestFilter[Req, Rep](
+    limit: Int,
+    stats: StatsReceiver)
+  extends SimpleFilter[Req, Rep] {
+
   import PendingRequestFilter._
 
   if (limit < 1)
     throw new IllegalArgumentException(s"request limit must be greater than zero, saw $limit")
+
+  private[this] val rejections = stats.counter("rejected")
 
   private[this] val pending = new AtomicInteger(0)
   private[this] val decFn: Any => Unit = { _: Any => pending.decrementAndGet() }
@@ -62,6 +81,7 @@ private[finagle] class PendingRequestFilter[Req, Rep](limit: Int) extends Simple
     // from a session operating at its limits.
     if (pending.incrementAndGet() > limit) {
       pending.decrementAndGet()
+      rejections.incr()
       Future.exception(Failure.rejected(PendingRequestsLimitExceeded))
     } else {
       service(req).respond(decFn)
