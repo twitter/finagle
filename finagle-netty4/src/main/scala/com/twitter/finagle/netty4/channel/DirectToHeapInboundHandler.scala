cf4fa6873379fb52952a0a120ab98ecc48d17a73
Vladimir Kostyukov <vkostyukov@twitter.com>
2017-02-03 06:03:40 +0000

    finagle-netty4: Make DirectToHeap handler ByteBufHolder-aware
    Problem / Solution
    
    We want to open a way to either prefer direct byte buffers or enable pooling
    by default. One way or another we need to make our `DirectToHeap` handler a
    bit smarter. Being aware of `ByteBufHolder` types means we could move
    `DirectToHeap` to the last resort handler for HTTP pipelines thereby making
    sure HTTP payloads will always be released and copied off heap before entering
    the Finagle land.
    
    RB_ID=906602

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/DirectToHeapInboundHandler.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/DirectToHeapInboundHandler.scala
@@ -1,12 +1,15 @@
 package com.twitter.finagle.netty4.channel
 
-import io.netty.buffer.ByteBuf
+import io.netty.buffer.{ByteBuf, ByteBufAllocator, ByteBufHolder}
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
 
 /**
  * An inbound channel handler that copies direct byte buffers onto the JVM heap
  * and gives them a deterministic lifecycle.
+ *
+ * @note This handler recognizes both ByteBuf's and ByteBufHolder's (think of HTTP
+ *       messages extending ByteBufHolder's in Netty).
  *
  * @note If your protocol manages ref-counting or if you are delegating ref-counting
  *       to application space you don't need this handler in your pipeline. Every
@@ -15,13 +18,19 @@ import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
  */
 @Sharable
 object DirectToHeapInboundHandler extends ChannelInboundHandlerAdapter {
+  private[this] final def copyOnHeap(bb: ByteBuf, alloc: ByteBufAllocator): ByteBuf = {
+    try alloc.heapBuffer(bb.readableBytes, bb.capacity).writeBytes(bb)
+    finally bb.release()
+  }
+
   override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = msg match {
     case bb: ByteBuf if bb.isDirect =>
-      val heapBuf = ctx.alloc().heapBuffer(bb.readableBytes, bb.capacity)
-      heapBuf.writeBytes(bb)
+      ctx.fireChannelRead(copyOnHeap(bb, ctx.alloc()))
 
-      bb.release()
-      ctx.fireChannelRead(heapBuf)
+    case bbh: ByteBufHolder if bbh.content().isDirect =>
+      val onHeapContent = copyOnHeap(bbh.content(), ctx.alloc())
+      ctx.fireChannelRead(bbh.replace(onHeapContent))
+
     case _ => ctx.fireChannelRead(msg)
   }
 }
