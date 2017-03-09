1ffa5dc1873f1b5f6101973578761c5eb90cc181
Vladimir Kostyukov <vkostyukov@twitter.com>
2017-03-09 09:08:25 +0000

    finagle-netty4: Default to Unpooled in DirectToHeap handler
    Problem / Solution
    
    Before enabling pooling in `finagle-netty4` we need to make sure pooled
    buffers aren't leaking into the user space (either being returned from
    a client or being consumed in a server). Defaulting `DirectToHeap` to
    use `Unpooled` while allocating a destination buffer would do the trick.
    
    RB_ID=910217

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/DirectToHeapInboundHandler.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/DirectToHeapInboundHandler.scala
@@ -1,12 +1,21 @@
 package com.twitter.finagle.netty4.channel
 
-import io.netty.buffer.{ByteBuf, ByteBufAllocator, ByteBufHolder, EmptyByteBuf}
+import io.netty.buffer.{ByteBuf, ByteBufHolder, EmptyByteBuf, Unpooled}
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
 
 /**
  * An inbound channel handler that copies direct byte buffers onto the JVM heap
- * and gives them a deterministic lifecycle.
+ * and gives them a deterministic lifecycle. This handler also makes sure to
+ * use the unpooled byte buffer (regardless of what channel's allocator is) as
+ * its destination thereby defining a clear boundaries between pooled and unpooled
+ * environments.
+ *
+ * @note This handler will bypass all heap buffers since there is no way to tell
+ *       if they are pooled or not. However, given that it's not expected to
+ *       receive a heap buffer on the inbound path when pooling is enabled, the
+ *       "bypass heap buffers" code path only exists to accommodate pipelines
+ *       with unpooled allocators.
  *
  * @note If the input buffer is direct but not readable it's still guaranteed to be
  *       released and replaced with a heap buffer of equivalent capacity.
@@ -22,8 +31,8 @@ import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
 @Sharable
 object DirectToHeapInboundHandler extends ChannelInboundHandlerAdapter {
   // Assuming that `bb.capacity` isn't zero.
-  private[this] final def copyOnHeap(bb: ByteBuf, alloc: ByteBufAllocator): ByteBuf = {
-    try alloc.heapBuffer(bb.readableBytes, bb.capacity).writeBytes(bb)
+  private[this] final def copyOnHeap(bb: ByteBuf): ByteBuf = {
+    try Unpooled.buffer(bb.readableBytes, bb.capacity).writeBytes(bb)
     finally bb.release()
   }
 
@@ -31,14 +40,14 @@ object DirectToHeapInboundHandler extends ChannelInboundHandlerAdapter {
     case bb: EmptyByteBuf =>
       ctx.fireChannelRead(bb)
 
-    case bb: ByteBuf if bb.isDirect =>
-      ctx.fireChannelRead(copyOnHeap(bb, ctx.alloc))
+    case bb: ByteBuf if bb.isDirect  =>
+      ctx.fireChannelRead(copyOnHeap(bb))
 
     case bbh: ByteBufHolder if bbh.content.isInstanceOf[EmptyByteBuf] =>
       ctx.fireChannelRead(bbh)
 
     case bbh: ByteBufHolder if bbh.content.isDirect =>
-      val onHeapContent = copyOnHeap(bbh.content, ctx.alloc)
+      val onHeapContent = copyOnHeap(bbh.content)
       ctx.fireChannelRead(bbh.replace(onHeapContent))
 
     case _ => ctx.fireChannelRead(msg)
