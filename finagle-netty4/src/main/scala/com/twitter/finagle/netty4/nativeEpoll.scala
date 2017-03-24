11c09b94aa097e457b98bf3844b668ca7da0d47b
Daniel Schobel <dschobel@twitter.com>
2017-03-24 21:56:12 +0000

    finagle-netty4: register native epoll status in global registry
    Problem / Solution
    
    Replace logging with registry for native epoll status.
    
    RB_ID=912409

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/nativeEpoll.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/nativeEpoll.scala
@@ -2,18 +2,17 @@ package com.twitter.finagle.netty4
 
 import com.twitter.finagle.server.ServerInfo
 import com.twitter.finagle.toggle.Toggle
-import com.twitter.logging.Logger
+import com.twitter.util.registry.GlobalRegistry
 import io.netty.channel.epoll.Epoll
 
 private[netty4] object nativeEpoll {
-  private[this] val log = Logger.get
   private[this] val underlying: Toggle[Int] =
     Toggles("com.twitter.finagle.netty4.UseNativeEpoll")
 
   // evaluated once per VM for consistency between listeners, transporters + worker pool.
   lazy val enabled: Boolean = {
     val res = underlying(ServerInfo().id.hashCode) && Epoll.isAvailable
-    log.info(s"native epoll enabled: $res")
+    GlobalRegistry.get.put(Seq("library", "netty4", "native epoll enabled"), res.toString)
     res
   }
 }
