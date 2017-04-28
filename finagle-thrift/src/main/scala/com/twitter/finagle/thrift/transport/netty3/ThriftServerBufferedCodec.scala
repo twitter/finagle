531b444fb5e59f73445f9335b327fd208e3eaccf
Jillian Crossley <jillianc@twitter.com>
2017-04-28 19:33:22 +0000

    finagle-thrift: Remove deprecated ThriftServerBufferedCodec
    Problem / Solution
    
    c.t.f.thrift.transport.netty3.ThriftServerBufferedCodec and
    c.t.f.thrift.transport.netty3.ThriftServerBufferedCodecFactory
    are deprecated. Remove them.
    
    RB_ID=915656

--- finagle-thrift/src/main/scala/com/twitter/finagle/thrift/transport/netty3/ThriftServerBufferedCodec.scala
+++ finagle-thrift/src/main/scala/com/twitter/finagle/thrift/transport/netty3/ThriftServerBufferedCodec.scala
@@ -1,7 +1,5 @@
 package com.twitter.finagle.thrift.transport.netty3
 
-import com.twitter.finagle.thrift.{Protocols, ThriftServerFramedCodec}
-import com.twitter.finagle.{CodecFactory, ServerCodecConfig}
 import org.apache.thrift.protocol.TProtocolFactory
 import org.jboss.netty.channel.{ChannelPipeline, ChannelPipelineFactory}
 
@@ -16,44 +14,4 @@ case class ThriftServerBufferedPipelineFactory(protocolFactory: TProtocolFactory
       new ThriftBufferedTransportDecoder(protocolFactory))
     pipeline
   }
-}
-
-/**
- * ThriftServerBufferedCodec implements a buffered thrift transport.
- */
-@deprecated("Use the com.twitter.finagle.Thrift object to build a server", "2017-02-10")
-object ThriftServerBufferedCodec {
-  /**
-   * Create a
-   * [[com.twitter.finagle.thrift.transport.netty3.ThriftServerBufferedCodecFactory]],
-   * using the binary protocol factory.
-   */
-  def apply(): ThriftServerBufferedCodecFactory = new ThriftServerBufferedCodecFactory
-
-  /**
-   * Create a [[com.twitter.finagle.thrift.transport.netty3.ThriftServerBufferedCodecFactory]]
-   * using the protocol factory.
-   */
-  def apply(protocolFactory: TProtocolFactory): ThriftServerBufferedCodecFactory =
-    new ThriftServerBufferedCodecFactory(protocolFactory)
-}
-
-@deprecated("Use the com.twitter.finagle.Thrift object to build a server", "2017-02-10")
-class ThriftServerBufferedCodecFactory(protocolFactory: TProtocolFactory)
-  extends CodecFactory[Array[Byte], Array[Byte]]#Server
-{
-  def this() = this(Protocols.binaryFactory())
-  /**
-   * Create a [[com.twitter.finagle.thrift.transport.netty3.ThriftServerBufferedCodec]]
-   * with a default TBinaryProtocol.
-   */
-  def apply(config: ServerCodecConfig): ThriftServerBufferedCodec = {
-    new ThriftServerBufferedCodec(protocolFactory, config)
-  }
-}
-
-@deprecated("Use the com.twitter.finagle.Thrift object to build a server", "2017-02-10")
-class ThriftServerBufferedCodec(protocolFactory: TProtocolFactory, config: ServerCodecConfig)
-    extends ThriftServerFramedCodec(config) {
-  override def pipelineFactory = ThriftServerBufferedPipelineFactory(protocolFactory)
 }
