bfa0753f4bec4a876afee49017b57f79d28fee01
Jillian Crossley <jillianc@twitter.com>
2017-05-16 01:16:24 +0000

    finagle-netty4: Log read/write TimeoutExceptions at DEBUG level
    Problem
    
    If the read/write timeout is configured on the transport and is
    exceeded, ReadTimeoutExceptions and WriteTimeoutExceptions are
    expected and thus shouldn't be logged at WARNING level.
    
    Solution
    
    Log these exceptions at DEBUG level.
    
    RB_ID=916822

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/ChannelStatsHandler.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/ChannelStatsHandler.scala
@@ -6,6 +6,7 @@ import com.twitter.util.{Duration, Monitor, Stopwatch}
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.{ChannelDuplexHandler, ChannelHandlerContext, ChannelPromise}
+import io.netty.handler.timeout.TimeoutException
 import io.netty.util.AttributeKey
 import java.io.IOException
 import java.util.concurrent.atomic.LongAdder
@@ -119,6 +120,7 @@ private[netty4] class ChannelStatsHandler(statsReceiver: StatsReceiver)
     if (!Monitor.isActive) {
       val level = cause match {
         case _: IOException => Level.FINE
+        case _: TimeoutException => Level.FINE
         case f: Failure => f.logLevel
         case _ => Level.WARNING
       }
