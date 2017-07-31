48a6a2bd68a67f2327f50d16c611872b5931c80c
Vinod Mehra <vmehra@twitter.com>
2017-07-31 22:33:01 +0000

    finagle-memcached: Introducing PartitioningService
    Summary: [For complete context of this change, please see https://phabricator.twitter.biz/D74074]
    
    Problem
    
    Currently the finagle client doesn't support partitioned clusters. Therefore clients like
    memcached, that need partitioning support, end up creating extra facade around the finagle
    client to add the partitioning logic.
    
    Solution
    
    Introducing the new PartitioningService which can be added into the finagle client stack in order
    to make it aware of partitioning of the downstream service. By moving the partitioning logic
    into the finagle stack, we can apply the standard naming mechanism used by other finagle clients.
    The PartitioningService is applied after BindingFactory. Therefore, wily names passed into
    finagle are translated into a concrete destination at request time, properly taking the dtab
    into account. The role of PartitioningService is to dispatch client requests to the corresponding
    endpoint stack, based on the request cache keys and currently available nodes. The new module
    is introduced above the load balancing logic in the stack, in order to preserve the existing
    concurrent load balancing used for each endpoint across its connections. Note that for batched
    requests, a single request may need to be dispatched to multiple nodes. The PartitioningService
    is therefore also responsible for making multiple endpoint calls, and merging values resulting
    from these calls if necessary.
    
    Result
    
    PartitioningService sets us up to implement specific partitioning services (e.g. for Ketama,
    memcache etc) that can be plugged into the finagle client stack to support partitioning.
    
    JIRA Issues: FES-76
    
    Differential Revision: https://phabricator.twitter.biz/D75143

---
+++ finagle-memcached/src/main/scala/com/twitter/finagle/memcached/partitioning/PartitioningService.scala
@@ -0,0 +1,69 @@
+package com.twitter.finagle.memcached.partitioning
+
+import com.twitter.finagle._
+import com.twitter.util._
+
+/**
+ * PartitioningService is responsible for request-key(s) based partition selection. Specially
+ * built clients (e.g. memcached), that require partitioning support, insert it after the
+ * BindingFactory and before LoadBalancerFactory. The only necessary configuration is
+ * [[com.twitter.finagle.loadbalancer.LoadBalancerFactory.Dest]], which represents a changing
+ * collection of addresses that the LoadBalancerFactory needs to operate on. The PartitioningService
+ * manages the association of request key(s) to shards. At request time it examines the key(s)
+ * present in the request and determines the partition(s) it belongs to. It then modifies the
+ * original [[com.twitter.finagle.loadbalancer.LoadBalancerFactory.Dest]] param and therefore
+ * narrows it down to the matching partition(s). As a result the per partition LoadBalancerFactories
+ * operate only on a specific partition.
+ */
+private[finagle] abstract class PartitioningService[Req, Rep] extends Service[Req, Rep] {
+
+  /**
+   * Returns the partition that the request belongs to. All keys in the request are assumed to
+   * belong to the same partition. Therefore the implementation can pick one of the keys and find
+   * the partition the key belongs to.
+   */
+  protected def getPartitionFor(partitionedRequest: Req): Future[Service[Req, Rep]]
+
+  /**
+   * Extracts the necessary information (e.g. keys) that is needed for the partitioning logic and
+   * group them by partition. The keys present in the request could belong to multiple partitions.
+   * The implementations will examine the keys, group them by partition, clone the request into
+   * per-partition requests and return the sequence of partitioned requests
+   *
+   * @param request: Incoming batched request
+   * @return Sequence of partitioned requests
+   */
+  protected def partitionRequest(request: Req): Seq[Req]
+
+  /**
+   * This method is used for the batched request case. When the keys belong to multiple partitions,
+   * the request is forked and sent to multiple partitions in parallel. After the responses come
+   * back, this method is used to combine the response into a single Response, so that the combined
+   * result can be returned back to the caller. Typically the implementations will be combining the
+   * response key-value maps into a merged key-value map.
+   *
+   * @param responses: per partition responses
+   * @return merged response
+   */
+  protected def mergeResponses(responses:Seq[Rep]): Rep
+
+  final def apply(request: Req): Future[Rep] = {
+    // Note that the services will be constructed in the implementing classes. So the
+    // implementations will be responsible for closing them too.
+    partitionRequest(request) match {
+      case Seq(_) =>
+        // single partition request (all keys belong to the same partition)
+        getPartitionFor(request).flatMap(_(request))
+      case servicesSeq =>
+        // multiple partitions
+        Future.collect(
+          servicesSeq.map { partitionedRequest =>
+            getPartitionFor(partitionedRequest).flatMap(_(partitionedRequest))
+          }
+        ).map(mergeResponses)
+    }
+  }
+}
+
+
+
---
+++ finagle-memcached/src/test/scala/com/twitter/finagle/memcached/unit/partitioning/PartitioningServiceTest.scala
@@ -0,0 +1,304 @@
+package com.twitter.finagle.memcached.unit.partitioning
+
+import com.twitter.conversions.time._
+import com.twitter.finagle.client.{StackClient, StringClient}
+import com.twitter.finagle.loadbalancer.LoadBalancerFactory
+import com.twitter.finagle.memcached.partitioning.PartitioningService
+import com.twitter.finagle.naming.BindingFactory
+import com.twitter.finagle.param.Stats
+import com.twitter.finagle.server.StringServer
+import com.twitter.finagle.stats.InMemoryStatsReceiver
+import com.twitter.finagle.util.DefaultTimer
+import com.twitter.finagle.{Address, _}
+import com.twitter.util._
+import java.net.{InetAddress, InetSocketAddress}
+import java.util.concurrent.ConcurrentHashMap
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import scala.collection.mutable
+
+class PartitioningServiceTest
+  extends FunSuite
+  with StringClient
+  with StringServer
+  with BeforeAndAfterEach
+{
+  private[this] val EchoDelimiter = ':'
+  private[this] val failingHosts = new mutable.HashSet[String]()
+  private[this] val slowHosts = new mutable.HashSet[String]()
+
+  private[this] var servers: Seq[(ListeningServer, InetSocketAddress, Int, Int)] = _
+  private[this] var client: Service[String,String] = _
+
+  override def beforeEach(): Unit = {
+    failingHosts.clear()
+    slowHosts.clear()
+  }
+
+  override def afterEach(): Unit = {
+    client.close()
+    servers.foreach(_._1.close())
+  }
+
+  private[this] def createCluster(size: Int): Seq[(ListeningServer, InetSocketAddress, Int, Int)] = {
+    def echoService(servername: String): Service[String, String] = Service.mk[String, String](
+      req => {
+        if (failingHosts.contains(servername)) {
+          Future.exception(new RuntimeException(s"$servername failed!"))
+        } else if (slowHosts.contains(servername)) {
+          Future.sleep(5.seconds)(DefaultTimer).before(
+            Future.value(s"Response from $servername: after sleep")
+          )
+        } else {
+          // sending back the hostname along with the request value, so that the caller can
+          // assert that the request landed on the correct host
+          Future.value(req + EchoDelimiter + servername) // $port:$hostname
+        }
+      }
+    )
+    // create a cluster of multiple servers, listening on unique port numbers
+    1 to size map { i =>
+      val addr = new InetSocketAddress(InetAddress.getLoopbackAddress, 0)
+      val server = stringServer.serve(addr, echoService(servername = s"server#$i"))
+      val boundAddress = server.boundAddress.asInstanceOf[InetSocketAddress]
+      val port = boundAddress.getPort
+      (server, boundAddress, port, i)
+    }
+  }
+
+  private[this] def createClient(
+    sr: InMemoryStatsReceiver
+  ): Service[String,String] = {
+    val dest: Name = Name.bound(servers.map(s => Address(s._2)): _*)
+
+    // create a partitioning aware finagle client by inserting the PartitioningService appropriately
+    val newClientStack =
+      StackClient.newStack[String, String]
+        .insertAfter(
+          BindingFactory.role,
+          SimplePartitioningService.module
+        )
+
+    stringClient
+      .withStack(newClientStack)
+      .withRequestTimeout(1.seconds)
+      .configured(Stats(sr))
+      .newService(dest, "client")
+  }
+
+  test("partitioning service makes the non-batched requests stick to the matching port") {
+    val sr = new InMemoryStatsReceiver
+    val numServers = 21
+    servers = createCluster(numServers)
+    client = createClient(sr)
+
+    assert(servers.length == numServers)
+    assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers)
+
+    // using multiple iterations to test repeatability
+    0 until 5 foreach { i =>
+      // send the client requests concurrently to test race conditions. One request per client.
+      val resFutures: Seq[Future[String]] = servers.map(s => client(s"${s._3}"))
+
+      // wait for all three to finish
+      Await.result(Future.join(resFutures), 1.second)
+
+      servers zip resFutures foreach { case (s, resFuture) =>
+        val res = Await.result(resFuture, 1.second)
+        assert(res == s"${s._3}${EchoDelimiter}server#${s._4}", s"i=$i $s res=$res")
+      }
+      assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers, s"i=$i")
+    }
+  }
+
+  test("partitioning service makes the batched requests stick to the matching port") {
+    // for this test, a batched request is a delimiter separated string of multiple sub requests
+    val sr = new InMemoryStatsReceiver
+    val numServers = 11
+    servers = createCluster(numServers)
+    client = createClient(sr)
+
+    assert(servers.length == numServers)
+    assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers)
+
+    // using multiple iterations to test repeatability
+    0 until 5 foreach { i =>
+      val batchedRequest: String = servers.map(s => s"${s._3}").mkString(
+        SimplePartitioningService.RequestDelimiter
+      )
+      val batchedResponse = Await.result(client(batchedRequest), 1.second)
+      val responses = batchedResponse.split(SimplePartitioningService.ResponseDelimiter)
+      assert(responses.length == numServers)
+
+      val responseMap: Map[String, String] = responses.map { response =>
+        val portAndName = response.split(EchoDelimiter)
+        assert(portAndName.length == 2)
+        val port = portAndName(0)
+        val name = portAndName(1)
+        port -> name
+      }.toMap
+      assert(responseMap.size == numServers)
+
+      servers foreach { s =>
+        val respondedByShard = responseMap.getOrElse(s._3.toString, fail())
+        assert(respondedByShard == s"server#${s._4}")
+      }
+      assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers, s"i=$i")
+    }
+  }
+
+  test("batched request with failing hosts") {
+    val sr = new InMemoryStatsReceiver
+    val numServers = 5
+    servers = createCluster(numServers)
+    client = createClient(sr)
+    failingHosts.add("server#3")
+
+    assert(servers.length == numServers)
+    assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers)
+
+    val batchedRequest: String = servers.map(s => s"${s._3}").mkString(
+      SimplePartitioningService.RequestDelimiter
+    )
+    intercept[ChannelClosedException] {
+      Await.result(client(batchedRequest), 1.second)
+    }
+  }
+
+  test("batched request with slow hosts") {
+    val sr = new InMemoryStatsReceiver
+    val numServers = 5
+    servers = createCluster(numServers)
+    client = createClient(sr)
+    slowHosts.add("server#2")
+
+    assert(servers.length == numServers)
+    assert(sr.counters(Seq("client", "loadbalancer", "adds")) == numServers)
+
+    Time.withCurrentTimeFrozen { timeControl =>
+      val batchedRequest: String = servers.map(s => {s._3 }).mkString(
+        SimplePartitioningService.RequestDelimiter
+      )
+      intercept[IndividualRequestTimeoutException] {
+        val future = client(batchedRequest)
+        timeControl.advance(2.seconds)
+        Await.result(future, 3.second)
+      }
+    }
+  }
+}
+
+/**
+ * This partitioning service uses a simple string based matching ("request -> server port) to pin
+ * requests with the specific partitions. To send a given request to a specific named
+ * partition, caller needs to set the request to that name.
+ */
+private[this] object SimplePartitioningService {
+
+  // Using the following delimiters to simulate batched requests. When request string contains
+  // multiple delimited strings, it will be treated as a batched request, with each string segment
+  // to be served with matching partition.
+  val RequestDelimiter = ";"
+
+  // When request was batched the responses will be collected and returned back after combining
+  // together using the delimiter
+  val ResponseDelimiter = ";"
+
+  def module: Stackable[ServiceFactory[String, String]] = {
+    new Stack.Module[ServiceFactory[String, String]] {
+      val role = Stack.Role("SimplePartitioning")
+      val description = "Maintains host stickiness based on name"
+
+      override def parameters: Seq[Stack.Param[_]] = Seq.empty
+
+      def make(
+        params: Stack.Params,
+        next: Stack[ServiceFactory[String, String]]
+      ): Stack[ServiceFactory[String, String]] = {
+        val LoadBalancerFactory.Dest(dest: Var[Addr]) = params[LoadBalancerFactory.Dest]
+        val service = new SimplePartitioningService(next, params, dest)
+        Stack.Leaf(role, ServiceFactory.const(service))
+      }
+    }
+  }
+}
+
+/**
+ * This service makes the request stick to the partition using the port that matches the request.
+ * So requests can choose the partition they intend to use.
+ *
+ * BatchedRequests: multiple sub-requests delimited by RequestDelimiter. The responses are
+ * concatenated using ResponseDelimiter.
+ *
+ * This example is just for testing and proof of concept. In a real use case the batched requests
+ * will contain multiple keys and the responses will be key-value maps. The merged response
+ * will combine the smaller maps into a single key-value map before returning the response to the
+ * caller.
+ */
+private[this] class SimplePartitioningService (
+    underlying: Stack[ServiceFactory[String, String]],
+    params: Stack.Params,
+    dest: Var[Addr]
+) extends PartitioningService[String, String] {
+
+  import SimplePartitioningService._
+
+  val unresolvedPartition: Future[Service[String, String]] = {
+    val modifiedParams = params + LoadBalancerFactory.Dest(Var.value(Addr.Neg))
+    val next: ServiceFactory[String, String] = underlying.make(modifiedParams)
+    next()
+  }
+
+  // map port numbers to the services representing downstream partitions
+  private[this] val serviceMap = new ConcurrentHashMap[String, Future[Service[String, String]]]()
+
+  {
+    // initialize the map of port -> service representing a shard
+    val serviceMapVar = dest.map {
+      case Addr.Bound(addresses: Set[Address], _) =>
+        addresses.map {
+          case addr@Address.Inet(ia: InetSocketAddress, _) =>
+            val service = mkService(addr)
+            serviceMap.put(ia.getPort.toString, service)
+            (ia.getPort.toString, service)
+          case _ =>
+            throw new IllegalStateException
+        }.toMap
+      case _ =>
+        Map.empty
+    }
+    serviceMapVar.changes.register(Witness({_ => }))
+  }
+
+  private[this] def mkService(addr: Address.Inet): Future[Service[String, String]] = {
+    val modifiedParams = params + LoadBalancerFactory.Dest(Var.value(Addr.Bound(addr)))
+    val next = underlying.make(modifiedParams)
+    next().map { svc =>
+      new ServiceProxy(svc) {
+        override def close(deadline: Time): Future[Unit] = {
+          Future.join(Seq(next.close(deadline), super.close(deadline)))
+        }
+      }
+    }
+  }
+
+  override protected def getPartitionFor(request: String): Future[Service[String, String]] = {
+    serviceMap.get(request) match {
+      case service: Future[Service[String, String]] =>
+        service
+      case null =>
+        unresolvedPartition
+    }
+  }
+
+  override protected def partitionRequest(batchedRequest: String): Seq[String] = {
+    // assuming all sub-requests are unique (one request per partition). If not the following code
+    // will need to group requests by partition by using getPartitionFor method
+    batchedRequest.split(RequestDelimiter).map(_.trim).toSeq
+  }
+
+  override protected def mergeResponses(responses: Seq[String]): String = {
+    // responses contain the request keys. So just concatenate. In a real implementation this will
+    // typically be a key-value map.
+    responses mkString ResponseDelimiter
+  }
+}
