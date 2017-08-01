0f55d952c62b281084858561926b7a431d2b6e7e
Bryce Anderson <banderson@twitter.com>
2017-08-01 17:30:23 +0000

    finagle-netty4: Extract generic parts of Netty4Transporter
    Summary: NOTE: Unfortunately it's difficult to see, but the vast majority of this change is moving the `Netty4Transporter.build` logic into the helper class, copy paste. The difference is in how the Promise is satisfied, and it has been made into a generic type `T` instead of a `ChannelTransport[In, Out]`, which is not refined at the use site in `Netty4Transporter`. The builder returns a `Future[T]` instead of a raw `T` to facilitate the push model. This allows extracting a `Service` from the `ServiceFactory` (a Future) and initializing the session before the pipeline starts to get data (it waits until the `Future` has resolved).
    
    Problem / Solution
    
    Much of the logic in Netty4Transporter is not specific to the
    Transporter model so to prepare for the experimental push-based API we
    extract the majority of it so that it can be shared.
    
    JIRA Issues: CSL-4935
    
    Differential Revision: https://phabricator.twitter.biz/D75423

---
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/ConnectionBuilder.scala
@@ -0,0 +1,148 @@
+package com.twitter.finagle.netty4
+
+import com.twitter.finagle.{
+  CancelledConnectionException,
+  ConnectionFailedException,
+  Failure,
+  ProxyConnectException,
+  Stack
+}
+import com.twitter.finagle.client.{LatencyCompensation, Transporter}
+import com.twitter.finagle.netty4.Netty4Transporter.Backpressure
+import com.twitter.finagle.param.Stats
+import com.twitter.finagle.transport.Transport
+import com.twitter.logging.Level
+import com.twitter.util.{Future, Promise, Stopwatch}
+import io.netty.bootstrap.Bootstrap
+import io.netty.channel.{
+  Channel,
+  ChannelFuture,
+  ChannelFutureListener,
+  ChannelInitializer,
+  ChannelOption
+}
+import io.netty.channel.epoll.EpollSocketChannel
+import io.netty.channel.socket.nio.NioSocketChannel
+import java.lang.{Boolean => JBool, Integer => JInt}
+import java.net.SocketAddress
+import java.nio.channels.UnresolvedAddressException
+import scala.util.control.NonFatal
+
+/**
+ * Utility class for building new connections using the Netty4 pipeline model.
+ *
+ * @param init `ChannelInitializer` responsible for setting up the initial pipeline before
+ *             the connection is established and will receive the
+ * @param addr Destination `SocketAddress` for new connections.
+ * @param params Configuration parameters.
+ */
+private final class ConnectionBuilder(
+  init: ChannelInitializer[Channel],
+  addr: SocketAddress,
+  params: Stack.Params
+) {
+
+  private[this] val Stats(statsReceiver) = params[Stats]
+  private[this] val connectLatencyStat = statsReceiver.stat("connect_latency_ms")
+  private[this] val failedConnectLatencyStat = statsReceiver.stat("failed_connect_latency_ms")
+  private[this] val cancelledConnects = statsReceiver.counter("cancelled_connects")
+
+  // Exports N4-related metrics under `finagle/netty4`.
+  exportNetty4MetricsAndRegistryEntries()
+
+  /**
+   * Creates a new connection then passes it to the provided builder function, returning
+   * the result asynchronously.
+   *
+   * @note Unless the `Future` returned from this method is interrupted (vide infra), the
+   *       ownership of the `Channel` is transferred to the builder function meaning it's
+   *       the responsibility of the user to manage resources from that point forward.
+   *       In the case of interrupt, the `Channel` is forcibly closed to ensure cleanup of
+   *       the spawned channel.
+   */
+  def build[T](builder: Channel => Future[T]): Future[T] = {
+    trackReferenceLeaks.init
+    val Transport.Options(noDelay, reuseAddr) = params[Transport.Options]
+    val LatencyCompensation.Compensation(compensation) = params[LatencyCompensation.Compensation]
+    val Transporter.ConnectTimeout(connectTimeout) = params[Transporter.ConnectTimeout]
+    val Transport.BufferSizes(sendBufSize, recvBufSize) = params[Transport.BufferSizes]
+    val Backpressure(backpressure) = params[Backpressure]
+    val param.Allocator(allocator) = params[param.Allocator]
+
+    // max connect timeout is ~24.8 days
+    val compensatedConnectTimeoutMs =
+      (compensation + connectTimeout).inMillis.min(Int.MaxValue)
+
+    val channelClass =
+      if (nativeEpoll.enabled) classOf[EpollSocketChannel]
+      else classOf[NioSocketChannel]
+
+    val bootstrap =
+      new Bootstrap()
+        .group(params[param.WorkerPool].eventLoopGroup)
+        .channel(channelClass)
+        .option(ChannelOption.ALLOCATOR, allocator)
+        .option[JBool](ChannelOption.TCP_NODELAY, noDelay)
+        .option[JBool](ChannelOption.SO_REUSEADDR, reuseAddr)
+        .option[JBool](ChannelOption.AUTO_READ, !backpressure) // backpressure! no reads on transport => no reads on the socket
+        .option[JInt](ChannelOption.CONNECT_TIMEOUT_MILLIS, compensatedConnectTimeoutMs.toInt)
+        .handler(init)
+
+    val Transport.Liveness(_, _, keepAlive) = params[Transport.Liveness]
+    keepAlive.foreach(bootstrap.option[JBool](ChannelOption.SO_KEEPALIVE, _))
+    sendBufSize.foreach(bootstrap.option[JInt](ChannelOption.SO_SNDBUF, _))
+    recvBufSize.foreach(bootstrap.option[JInt](ChannelOption.SO_RCVBUF, _))
+
+    val elapsed = Stopwatch.start()
+    val nettyConnectF = bootstrap.connect(addr)
+
+    val transportP = Promise[T]
+    // Try to cancel the connect attempt if the transporter's promise is interrupted.
+    // If the future is already complete, the channel will be closed by the interrupt
+    // handler that overwrites this one in the success branch of the ChannelFutureListener
+    // installed below.
+    transportP.setInterruptHandler {
+      case _ => nettyConnectF.cancel(true /* mayInterruptIfRunning */ )
+    }
+
+    nettyConnectF.addListener(new ChannelFutureListener {
+      def operationComplete(channelF: ChannelFuture): Unit = {
+        val latency = elapsed().inMilliseconds
+        if (channelF.isCancelled()) {
+          cancelledConnects.incr()
+          transportP.setException(
+            Failure(
+              cause = new CancelledConnectionException,
+              flags = Failure.Interrupted | Failure.Restartable,
+              logLevel = Level.DEBUG
+            )
+          )
+        } else if (channelF.cause != null) {
+          failedConnectLatencyStat.add(latency)
+          transportP.setException(channelF.cause match {
+            // there is no need to retry on unresolved address
+            case e: UnresolvedAddressException => e
+            // there is no need to retry if proxy connect failed
+            case e: ProxyConnectException => e
+            // the rest of failures could benefit from retries
+            case NonFatal(e) => Failure.rejected(new ConnectionFailedException(e, addr))
+          })
+        } else {
+          connectLatencyStat.add(latency)
+          val result = builder(channelF.channel)
+          result.proxyTo(transportP)
+          // On cancellation we should be aggressive with cleanup, both forcing the channel
+          // closed and interrupting `result` with the exception to ensure we don't leak
+          // connections.
+          transportP.setInterruptHandler {
+            case t =>
+              channelF.channel().close()
+              result.raise(t)
+          }
+        }
+      }
+    })
+
+    transportP
+  }
+}
--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/Netty4Transporter.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/Netty4Transporter.scala
@@ -1,29 +1,16 @@
 package com.twitter.finagle.netty4
 
-import com.twitter.finagle.client.{LatencyCompensation, Transporter}
+import com.twitter.finagle.client.Transporter
 import com.twitter.finagle.decoder.Framer
 import com.twitter.finagle.netty4.channel._
 import com.twitter.finagle.netty4.transport.ChannelTransport
-import com.twitter.finagle.param.Stats
 import com.twitter.finagle.transport.Transport
-import com.twitter.finagle.{
-  CancelledConnectionException,
-  ConnectionFailedException,
-  Failure,
-  ProxyConnectException,
-  Stack
-}
+import com.twitter.finagle.Stack
 import com.twitter.io.Buf
-import com.twitter.logging.Level
-import com.twitter.util.{Future, Promise, Stopwatch}
-import io.netty.bootstrap.Bootstrap
+import com.twitter.util.Future
 import io.netty.channel._
-import io.netty.channel.epoll.EpollSocketChannel
-import io.netty.channel.socket.nio.NioSocketChannel
-import java.lang.{Boolean => JBool, Integer => JInt}
+
 import java.net.SocketAddress
-import java.nio.channels.UnresolvedAddressException
-import scala.util.control.NonFatal
 
 private[finagle] object Netty4Transporter {
 
@@ -49,89 +36,13 @@ private[finagle] object Netty4Transporter {
       new ChannelTransport(ch)
     }
   )(implicit mOut: Manifest[Out]): Transporter[In, Out] = new Transporter[In, Out] {
-    private[this] val Stats(statsReceiver) = params[Stats]
 
-    private[this] val connectLatencyStat = statsReceiver.stat("connect_latency_ms")
-    private[this] val failedConnectLatencyStat = statsReceiver.stat("failed_connect_latency_ms")
-    private[this] val cancelledConnects = statsReceiver.counter("cancelled_connects")
+    private[this] val factory = new ConnectionBuilder(init, addr, params)
 
     def remoteAddress: SocketAddress = addr
 
-    // Exports N4-related metrics under `finagle/netty4`.
-    exportNetty4MetricsAndRegistryEntries()
-
-    def apply(): Future[Transport[In, Out]] = {
-      trackReferenceLeaks.init
-      val Transport.Options(noDelay, reuseAddr) = params[Transport.Options]
-      val LatencyCompensation.Compensation(compensation) = params[LatencyCompensation.Compensation]
-      val Transporter.ConnectTimeout(connectTimeout) = params[Transporter.ConnectTimeout]
-      val Transport.BufferSizes(sendBufSize, recvBufSize) = params[Transport.BufferSizes]
-      val Backpressure(backpressure) = params[Backpressure]
-      val param.Allocator(allocator) = params[param.Allocator]
-
-      // max connect timeout is ~24.8 days
-      val compensatedConnectTimeoutMs =
-        (compensation + connectTimeout).inMillis.min(Int.MaxValue)
-
-      val channelClass =
-        if (nativeEpoll.enabled) classOf[EpollSocketChannel]
-        else classOf[NioSocketChannel]
-
-      val bootstrap =
-        new Bootstrap()
-          .group(params[param.WorkerPool].eventLoopGroup)
-          .channel(channelClass)
-          .option(ChannelOption.ALLOCATOR, allocator)
-          .option[JBool](ChannelOption.TCP_NODELAY, noDelay)
-          .option[JBool](ChannelOption.SO_REUSEADDR, reuseAddr)
-          .option[JBool](ChannelOption.AUTO_READ, !backpressure) // backpressure! no reads on transport => no reads on the socket
-          .option[JInt](ChannelOption.CONNECT_TIMEOUT_MILLIS, compensatedConnectTimeoutMs.toInt)
-          .handler(init)
-
-      val Transport.Liveness(_, _, keepAlive) = params[Transport.Liveness]
-      keepAlive.foreach(bootstrap.option[JBool](ChannelOption.SO_KEEPALIVE, _))
-      sendBufSize.foreach(bootstrap.option[JInt](ChannelOption.SO_SNDBUF, _))
-      recvBufSize.foreach(bootstrap.option[JInt](ChannelOption.SO_RCVBUF, _))
-
-      val elapsed = Stopwatch.start()
-      val nettyConnectF = bootstrap.connect(addr)
-
-      val transportP = Promise[Transport[In, Out]]()
-      // try to cancel the connect attempt if the transporter's promise is interrupted.
-      transportP.setInterruptHandler {
-        case _ => nettyConnectF.cancel(true /* mayInterruptIfRunning */ )
-      }
-
-      nettyConnectF.addListener(new ChannelFutureListener {
-        def operationComplete(channelF: ChannelFuture): Unit = {
-          val latency = elapsed().inMilliseconds
-          if (channelF.isCancelled()) {
-            cancelledConnects.incr()
-            transportP.setException(
-              Failure(
-                cause = new CancelledConnectionException,
-                flags = Failure.Interrupted | Failure.Restartable,
-                logLevel = Level.DEBUG
-              )
-            )
-          } else if (channelF.cause != null) {
-            failedConnectLatencyStat.add(latency)
-            transportP.setException(channelF.cause match {
-              // there is no need to retry on unresolved address
-              case e: UnresolvedAddressException => e
-              // there is no need to retry if proxy connect failed
-              case e: ProxyConnectException => e
-              // the rest of failures could benefit from retries
-              case NonFatal(e) => Failure.rejected(new ConnectionFailedException(e, addr))
-            })
-          } else {
-            connectLatencyStat.add(latency)
-            transportP.setValue(Transport.cast[In, Out](transportFactory(channelF.channel())))
-          }
-        }
-      })
-
-      transportP
+    def apply(): Future[Transport[In, Out]] = factory.build { ch =>
+      Future(Transport.cast[In, Out](transportFactory(ch)))
     }
 
     override def toString: String = "Netty4Transporter"
