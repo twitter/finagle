ace2573d91237170a54b688503557647c931af7e
Daniel Schobel <dschobel@twitter.com>
2017-08-17 23:16:34 +0000

    twitter-server: make shadow admin server compatible with native transport
    Summary: Problem
    
    Twitter-server's shadow admin server expliciltly configures a NIO workerpool
    which is incompatible when the native transport is configured within Finagle.
    
    Solution
    
    Check native transport configuration and instantiate the correct worker pool
    type.
    
    JIRA Issues: CSL-5082
    
    Differential Revision: https://phabricator.twitter.biz/D82326

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/nativeEpoll.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/nativeEpoll.scala
@@ -4,7 +4,7 @@ import com.twitter.finagle.server.ServerInfo
 import com.twitter.finagle.toggle.Toggle
 import io.netty.channel.epoll.Epoll
 
-private[netty4] object nativeEpoll {
+object nativeEpoll {
   private[this] val underlying: Toggle[Int] =
     Toggles("com.twitter.finagle.netty4.UseNativeEpollV2")
 
