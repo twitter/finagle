7de28ac5e67eaba7a7e123ab01f3412530f45636
Jillian Crossley <jillianc@twitter.com>
2017-08-23 14:34:20 +0000

    finagle-core: Mark ServerBuilderTest#registers server with bound address as flaky
    Summary: Problem / Solution
    
    The test fails often. Mark it as flaky for now and fix later.
    
    JIRA Issues: CSL-5119
    
    Differential Revision: https://phabricator.twitter.biz/D83712

--- finagle-core/src/test/scala/com/twitter/finagle/builder/ServerBuilderTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/builder/ServerBuilderTest.scala
@@ -24,25 +24,26 @@ class ServerBuilderTest
     with IntegrationPatience
     with MockitoSugar {
 
-  test(s"registers server with bound address") {
-    val simple = new SimpleRegistry()
+  if (!sys.props.contains("SKIP_FLAKY"))
+    test(s"registers server with bound address") {
+      val simple = new SimpleRegistry()
 
-    GlobalRegistry.withRegistry(simple) {
-      val server = ServerBuilder()
-        .stack(StringServer.server)
-        .bindTo(new InetSocketAddress(InetAddress.getLoopbackAddress, 0))
-        .name("test")
-        .build(Service.const(Future.value("hi")))
+      GlobalRegistry.withRegistry(simple) {
+        val server = ServerBuilder()
+          .stack(StringServer.server)
+          .bindTo(new InetSocketAddress(InetAddress.getLoopbackAddress, 0))
+          .name("test")
+          .build(Service.const(Future.value("hi")))
 
-      val entries = simple.toSet
-      val specified = entries.filter(_.key.startsWith(Seq("server", "string", "test")))
-      // Entries are in the form: Entry(List(server, fancy, test, /127.0.0.1:58904, RequestStats, unit),MILLISECONDS)
-      val entry = specified.head // data is repeated as entry.key, just take the first
-      val hostAndPort = entry.key.filter(_.contains("127.0.0.1")).head
-      assert(!hostAndPort.contains(":0"), "unbounded address in server registry")
-      server.close()
+        val entries = simple.toSet
+        val specified = entries.filter(_.key.startsWith(Seq("server", "string", "test")))
+        // Entries are in the form: Entry(List(server, fancy, test, /127.0.0.1:58904, RequestStats, unit),MILLISECONDS)
+        val entry = specified.head // data is repeated as entry.key, just take the first
+        val hostAndPort = entry.key.filter(_.contains("127.0.0.1")).head
+        assert(!hostAndPort.contains(":0"), "unbounded address in server registry")
+        server.close()
+      }
     }
-  }
 
   test("#configured[P](P)(Stack.Param[P]) should pass name through") {
     val sb = ServerBuilder()
