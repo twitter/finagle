f28705d556077cbd56b19ce90eba35be7203ad07
Daniel Schobel <dschobel@twitter.com>
2017-08-29 18:34:20 +0000

    finagle: bump netty to 4.1.14
    Summary: Problem / Solution
    
    Update Netty to 4.1.14.
    
    JIRA Issues: CSL-4834
    
    Differential Revision: https://phabricator.twitter.biz/D84542

--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ val scroogeVersion = "4.19.0" + suffix
 
 val libthriftVersion = "0.5.0-7"
 
-val netty4Version = "4.1.12.Final"
+val netty4Version = "4.1.14.Final"
 
 // zkVersion should be kept in sync with the 'util-zk' dependency version
 val zkVersion = "3.5.0-alpha"
@@ -38,7 +38,7 @@ val netty4LibsTest = Seq(
 )
 val netty4Http = "io.netty" % "netty-codec-http" % netty4Version
 val netty4Http2 = "io.netty" % "netty-codec-http2" % netty4Version
-val netty4StaticSsl = "io.netty" % "netty-tcnative-boringssl-static" % "2.0.1.Final" % "test"
+val netty4StaticSsl = "io.netty" % "netty-tcnative-boringssl-static" % "2.0.5.Final" % "test"
 val jacksonVersion = "2.8.4"
 val jacksonLibs = Seq(
   "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion,
--- finagle-netty4/src/test/scala/com/twitter/finagle/netty4/channel/HandlerEventTest.scala
+++ finagle-netty4/src/test/scala/com/twitter/finagle/netty4/channel/HandlerEventTest.scala
@@ -3,14 +3,11 @@ package com.twitter.finagle.netty4.channel
 import com.twitter.finagle.stats.InMemoryStatsReceiver
 import io.netty.channel._
 import io.netty.channel.embedded.EmbeddedChannel
-import java.net.SocketAddress
 import io.netty.channel.nio.NioEventLoopGroup
+import java.net.SocketAddress
 import org.scalatest.FunSuite
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-import org.scalatest.mock.MockitoSugar
+import org.scalatest.mockito.MockitoSugar
 
-@RunWith(classOf[JUnitRunner])
 class HandlerEventTest extends FunSuite with MockitoSugar {
 
   // verify that custom channel handlers don't swallow pipeline events.
@@ -88,15 +85,6 @@ class HandlerEventTest extends FunSuite with MockitoSugar {
       assert(handler.writeFired, "suppressed Write event")
     }
 
-    // note: we don't test disconnects because the channel types
-    // we care about are connection oriented and disconnect
-    // isn't a meaningful operation for them so netty turns them into
-    // closes.
-    test(s"$name doesn't suppress Close event") {
-      pipeline.close()
-      assert(handler.closeFired, "suppressed Close event")
-    }
-
     test(s"$name doesn't suppress Deregister event") {
       pipeline.deregister()
       assert(handler.deregisterFired, "suppressed Deregister event")
@@ -115,6 +103,15 @@ class HandlerEventTest extends FunSuite with MockitoSugar {
     test(s"$name doesn't suppress Bind event") {
       pipeline.bind(mock[SocketAddress])
       assert(handler.bindFired, "suppressed Bind event")
+    }
+
+    // note: we don't test disconnects because the channel types
+    // we care about are connection oriented and disconnect
+    // isn't a meaningful operation for them so netty turns them into
+    // closes.
+    test(s"$name doesn't suppress Close event") {
+      pipeline.close()
+      assert(handler.closeFired, "suppressed Close event")
     }
   }
 
