3ce475dbdb800a635bbed944139b9690418e09d9
Ryan O'Neill <ryano@twitter.com>
2017-10-30 20:50:11 +0000

    finagle-netty4: Make Netty4Transporter and Netty4Listener Accessible
    Summary: Problem / Solution
    
    For external users attempting to extend Finagle running on Netty 4,
    currently those users need to add code to the `com.twitter.finagle`
    namespace due to the access levels of `Netty4Transporter` and
    `Netty4Listener`. These entities should be made public instead so that
    external users may develop protocols without this workaround.
    
    JIRA Issues: CSL-5431
    
    Differential Revision: https://phabricator.twitter.biz/D105627

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/Netty4Listener.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/Netty4Listener.scala
@@ -9,9 +9,9 @@ import io.netty.channel._
 import java.lang.{Integer => JInt}
 import java.net.SocketAddress
 
-private[finagle] object Netty4Listener {
+object Netty4Listener {
 
-  val TrafficClass: ChannelOption[JInt] = ChannelOption.newInstance("trafficClass")
+  private[finagle] val TrafficClass: ChannelOption[JInt] = ChannelOption.newInstance("trafficClass")
 
   /**
    * A [[com.twitter.finagle.Stack.Param]] used to configure the ability to
@@ -60,7 +60,7 @@ private[finagle] object Netty4Listener {
  * @see [[com.twitter.finagle.transport.Transport]]
  * @see [[com.twitter.finagle.param]]
  */
-private[finagle] case class Netty4Listener[In, Out, Ctx <: TransportContext](
+case class Netty4Listener[In, Out, Ctx <: TransportContext](
   pipelineInit: ChannelPipeline => Unit,
   params: Stack.Params,
   setupMarshalling: ChannelInitializer[Channel] => ChannelHandler,
