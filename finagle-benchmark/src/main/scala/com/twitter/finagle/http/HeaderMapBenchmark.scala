dfdc9bf5ed781e091f0d26a9905fc4fd69349702
Vladimir Kostyukov <vkostyukov@twitter.com>
2017-12-06 01:18:26 +0000

    finagle-http: Replace DefaultHeaderMap.canonicalName with custom hashing
    Summary: Problem / Solution
    
    Instead of using a canonical name (i.e., `toLowerCase(Locale.US)` to support a case-insensitive
    nature of headers, we can roll our own (inspired by the Netty 3 implementation) hash function
    and equals method for the `HashMap`. This allows eliminating the need for allocating a string
    and going through `toLowerCase` logic, which happened to be quite expensive compute-wise.
    
    The new version features:
    
     - 1.4x throughput improvements for `createAndAdd`
     - 3x throughput improvements for `get`
    
    JIRA Issues: CSL-5322
    
    Differential Revision: https://phabricator.twitter.biz/D117065

--- finagle-benchmark/src/main/scala/com/twitter/finagle/http/HeaderMapBenchmark.scala
+++ finagle-benchmark/src/main/scala/com/twitter/finagle/http/HeaderMapBenchmark.scala
@@ -10,22 +10,22 @@ abstract class HeaderMapBenchmark extends StdBenchAnnotations {
 
   protected def newMap(): HeaderMap
 
-  // We supply 18 random strings of the length of 3 and build a 9-element
+  // We supply 18 random strings of the length of 14 and build a 9-element
   // header map of them. The 10th element is foo -> bar so we can reliably
   // query it in the benchmark.
-  private val map = Iterator.fill(9 * 2)(Random.alphanumeric.take(3).mkString)
+  private val map = Iterator.fill(9 * 2)(Random.alphanumeric.take(14).mkString)
     .grouped(2)
     .foldLeft(newMap())((map, h) => map.add(h.head, h.last))
-    .add("foo", "bar")
+    .add("Content-Length", "100")
 
   @Benchmark
   def create(): HeaderMap = newMap()
 
   @Benchmark
-  def get(): Option[String] = map.get("foo")
+  def get(): Option[String] = map.get("Content-Length")
 
   @Benchmark
-  def createAndAdd(): HeaderMap = newMap().add("foo", "bar")
+  def createAndAdd(): HeaderMap = newMap().add("Content-Length", "100")
 
   @Benchmark
   def iterate(b: Blackhole): Unit = map.foreach(h => b.consume(h))
