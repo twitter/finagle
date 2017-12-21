f1eed124742421adc60d47e13c8e4b0a6ac6b691
Mikael Högqvist Tabor <hoegqvist@gmail.com>
2017-12-21 16:54:59 +0100

    finagle-redis: Redis cluster management
    Problem
    
    The current finagle-redis client does not have support for the CLUSTER commands introduced in Redis 3.0.0.
    
    Solution
    
    This adds support for a number of new cluster commands that are necessary to setup and manage a redis cluster.
    
    - CLUSTER ADDSLOT, assigns a storage slot to a server
    - CLUSTER SETSLOT, updates the state of a slot
    - CLUSTER SLOTS, returns a list of slots known to the server
    - CLUSTER GETKEYSINSLOT, returns keys stored in a slot
    - CLUSTER REPLICATE, changes a node to become a replica of a server
    - CLUSTER MEET, introduces cluster nodes to each other
    - CLUSTER NODES, returns the list of cluster nodes known to the server
    - CLUSTER INFO, returns the cluster specific info
    - MIGRATE, used to transfer keys to another node
    
    Result
    
    Using the introduced commands it is possible to setup a redis cluster for testing and live re-sharding. This is the first step in implementing the redis cluster client protocol. The next step is to handle operation redirects, i.e. when the server returns ASK or MOVED.

---
+++ finagle-redis/src/main/scala/com/twitter/finagle/redis/ClusterClient.scala
@@ -0,0 +1,27 @@
+package com.twitter.finagle.redis
+
+import com.twitter.finagle.redis.protocol._
+import com.twitter.finagle.ServiceFactory
+
+private[redis] object ClusterClient {
+  /**
+   * Construct a client client from a single host.
+   * @param host a String of host:port combination.
+   */
+  def apply(host: String): ClusterClient = {
+    ClusterClient(com.twitter.finagle.Redis.newClient(host))
+  }
+
+  /**
+   * Construct a cluster client from a single Service.
+   */
+  def apply(raw: ServiceFactory[Command, Reply]): ClusterClient =
+    new ClusterClient(raw)
+}
+
+private[redis] class ClusterClient(factory: ServiceFactory[Command, Reply])
+    extends BaseClient(factory)
+    with BasicServerCommands
+    with KeyCommands
+    with StringCommands
+    with ClusterCommands
--- finagle-redis/src/main/scala/com/twitter/finagle/redis/util/TestServer.scala
+++ finagle-redis/src/main/scala/com/twitter/finagle/redis/util/TestServer.scala
@@ -1,6 +1,6 @@
 package com.twitter.finagle.redis.util
 
-import java.net.InetSocketAddress
+import java.net.{InetSocketAddress, Socket}
 import java.io.{BufferedWriter, FileWriter, PrintWriter, File}
 import com.twitter.finagle.Redis
 import com.twitter.finagle.redis.Client
@@ -74,10 +74,12 @@ object RedisMode {
   case object Sentinel extends RedisMode
   case object Cluster extends RedisMode
 }
+
 class ExternalRedis(mode: RedisMode = RedisMode.Standalone) {
   private[this] val rand = new Random
   private[this] var process: Option[Process] = None
-  private[this] val forbiddenPorts = 6300.until(7300)
+  private[this] val forbiddenPorts = 6300.until(7300) ++ 55535.until(65535)
+  private[this] val possiblePorts = 49152.until(55535)
   var address: Option[InetSocketAddress] = None
 
   private[this] def assertRedisBinaryPresent() {
@@ -88,27 +90,57 @@ class ExternalRedis(mode: RedisMode = RedisMode.Standalone) {
   }
 
   private[this] def findAddress() {
-    var tries = 100
+    var tries = possiblePorts.size-1
     while (address.isEmpty && tries >= 0) {
-      address = Some(RandomSocket.nextAddress())
-      if (forbiddenPorts.contains(address.get.getPort)) {
-        address = None
-        tries -= 1
-        Thread.sleep(5)
+      val addr = new InetSocketAddress(possiblePorts(tries))
+      val socket = new Socket
+
+      try {
+        socket.setReuseAddress(true)
+        socket.bind(addr)
+        address = Some(addr)
+      } catch {
+        case exc: Exception =>
+          address = None
+          tries -= 1
+          Thread.sleep(5) 
+      } finally {
+        socket.close()
       }
     }
     address.getOrElse { sys.error("Couldn't get an address for the external redis instance") }
   }
 
   protected def createConfigFile(port: Int): File = {
-    val f = File.createTempFile("redis-" + rand.nextInt(1000), ".tmp")
-    f.deleteOnExit()
-    val out = new PrintWriter(new BufferedWriter(new FileWriter(f)))
-    val conf = "port %s".format(port)
+    val confFile = File.createTempFile("redis-" + rand.nextInt(1000), ".tmp")
+    val nodesFile = File.createTempFile("redis-nodes-" + rand.nextInt(1000), ".tmp")
+    val appendFile = File.createTempFile("redis-append-" + rand.nextInt(1000), ".aof")
+    val dbFile = File.createTempFile("redis-db-" + rand.nextInt(1000), ".db")
+
+    confFile.deleteOnExit()
+    nodesFile.deleteOnExit()
+    appendFile.deleteOnExit()
+    dbFile.deleteOnExit()
+
+    val out = new PrintWriter(new BufferedWriter(new FileWriter(confFile)))
+    var conf = "port %s".format(port)
+
+    if (mode == RedisMode.Cluster) {
+      conf += s"""
+cluster-enabled yes
+cluster-config-file ${nodesFile.getAbsolutePath}
+cluster-node-timeout 5000
+appendonly yes
+dir ${appendFile.getParent}
+appendfilename ${appendFile.getName}
+dbfilename ${dbFile.getName}
+"""
+    }
+
     out.write(conf)
     out.println()
     out.close()
-    f
+    confFile
   }
 
   def start() {
