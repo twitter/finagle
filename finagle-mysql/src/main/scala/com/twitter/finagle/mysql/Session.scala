a06f7d672dce4b9e131356634f18e168e68f3692
David Rusek <drusek@twitter.com>
2018-10-24 22:09:11 +0000

    finagle-mysql: introduce session
    Problem
    
    Many MySQL operations require keeping session state, this recently came up in
    the context of advisory locks. As it turns out, there are several uses for this
    functionality. The current workaround is to do all session work within a
    transaction but this has the effect of wrapping all session operations in a
    transaction, which may not be the desired behavior.  Since this is a common
    pattern in MySQL we should expose the ability for users to "check-out" a session
    for the duration of some series of operations.
    
    Solution
    
    Provide a method, `session` that allows a user to reserve a client connection so
    that they may perform operations that require client/server state. This also
    adds the ability for the user to `discard` a session if they have determined that
    an operation has put the session in an unrecoverable state.
    
    JIRA Issues: CSL-7018
    
    TBR=true
    
    Differential Revision: https://phabricator.twitter.biz/D219322

---
+++ finagle-mysql/src/main/scala/com/twitter/finagle/mysql/Session.scala
@@ -0,0 +1,17 @@
+package com.twitter.finagle.mysql
+
+import com.twitter.util.Future
+
+/**
+ * Explicitly manage the lifecycle of a MySQL session.
+ *
+ * @see Client#session()
+ */
+trait Session {
+  /**
+   * Discard the session. The connection will become unusable and will not be returned to the pool.
+   * This can be used when an error is detected at the application level that signifies that the
+   * session is no longer usable or that the server state is unknown or inconsistent.
+   */
+  def discard(): Future[Unit]
+}
---
+++ finagle-mysql/src/test/scala/com/twitter/finagle/mysql/unit/SessionsTest.scala
@@ -0,0 +1,144 @@
+package com.twitter.finagle.mysql.unit
+
+import com.twitter.finagle.mysql._
+import com.twitter.finagle.stats.NullStatsReceiver
+import com.twitter.util.{Await, Awaitable, Future, Time}
+import com.twitter.conversions.time._
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{spy, times, verify}
+import org.scalatest.mockito.MockitoSugar
+import org.scalatest.FunSuite
+
+class SessionsTest extends FunSuite with MockitoSugar {
+  private val sqlQuery = "SELECT * FROM FOO"
+
+  private[this] def await[T](t: Awaitable[T]): T = Await.result(t, 5.seconds)
+
+  test("sessions uses a single service") {
+    val service = spy(new MockService())
+    val factory = spy(new MockServiceFactory(service))
+    val client = Client(factory, NullStatsReceiver, supportUnsigned = false)
+
+    val result = client.session { session =>
+      for {
+        _ <- session.query(sqlQuery)
+        _ <- session.query(sqlQuery)
+      } yield "success"
+    }
+
+    assert(await(result) == "success")
+    assert(service.requests ==
+      List(
+        sqlQuery,
+        sqlQuery
+      ).map(QueryRequest(_))
+    )
+
+    verify(factory, times(1)).apply()
+    verify(factory, times(0)).close(any[Time])
+    verify(service, times(1)).close(any[Time])
+  }
+
+  test("sessions with nested transactions uses a single service") {
+    val service = spy(new MockService())
+    val factory = spy(new MockServiceFactory(service))
+    val client = Client(factory, NullStatsReceiver, supportUnsigned = false)
+
+    val result = client.session { session =>
+      for {
+        _ <- session.query("LOCK TABLES FOO WRITE")
+        r <- session.transaction { tx =>
+          for {
+            _ <- tx.query(sqlQuery)
+            _ <- tx.query(sqlQuery)
+          } yield "success"
+        }
+        _ <- session.query("UNLOCK TABLES")
+      } yield r
+    }
+
+    assert(await(result) == "success")
+    assert(service.requests ==
+      List(
+        "LOCK TABLES FOO WRITE",
+        "START TRANSACTION",
+        sqlQuery,
+        sqlQuery,
+        "COMMIT",
+        "UNLOCK TABLES"
+      ).map(QueryRequest(_))
+    )
+
+    verify(factory, times(1)).apply()
+    verify(factory, times(0)).close(any[Time])
+    verify(service, times(1)).close(any[Time])
+  }
+
+  test("sessions with nested transaction and discard") {
+    val service = spy(new MockService())
+    val factory = spy(new MockServiceFactory(service))
+    val client = Client(factory, NullStatsReceiver, supportUnsigned = false)
+
+    val result = client.session { session =>
+      val inner = for {
+        _ <- session.query("LOCK TABLES FOO WRITE")
+        r <- session.transaction { tx =>
+          for {
+            _ <- tx.query(sqlQuery)
+            _ <- tx.query(sqlQuery)
+            _ <- Future.exception(new Exception())
+          } yield "success"
+        }
+        _ <- session.query("UNLOCK TABLES")
+      } yield r
+
+      inner.onFailure(_ => session.discard())
+    }
+
+    intercept[Exception] { await(result) }
+    assert(service.requests ==
+      List(
+        "LOCK TABLES FOO WRITE",
+        "START TRANSACTION",
+        sqlQuery,
+        sqlQuery,
+        "ROLLBACK"
+      ).map(QueryRequest(_)) :+
+      PoisonConnectionRequest
+    )
+
+    verify(factory, times(1)).apply()
+    verify(factory, times(0)).close(any[Time])
+    verify(service, times(1)).close(any[Time])
+  }
+
+  test("discarded sessions poison the connection") {
+    val service = spy(new MockService())
+    val factory = spy(new MockServiceFactory(service))
+    val client = Client(factory, NullStatsReceiver, supportUnsigned = false)
+
+    client.session { session =>
+      session.discard()
+    }
+
+    assert(service.requests == List(PoisonConnectionRequest))
+
+    verify(factory, times(1)).apply()
+    verify(factory, times(0)).close(any[Time])
+    verify(service, times(1)).close()
+  }
+
+  test("released sessions are returned to the pool") {
+    val service = spy(new MockService())
+    val factory = spy(new MockServiceFactory(service))
+    val client = Client(factory, NullStatsReceiver, supportUnsigned = false)
+
+    client.session{ session =>
+      Future.value("foo")
+    }
+
+    verify(factory, times(1)).apply()
+    verify(factory, times(0)).close(any[Time])
+    verify(service, times(1)).close(any[Time])
+  }
+}
