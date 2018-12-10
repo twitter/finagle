29d80c88db634bee169b12fcc31632cd230f2d9a
Bryce Anderson <banderson@twitter.com>
2018-12-10 17:06:12 +0000

    finagle-netty4: Remove the ServerBridge from the Netty pipeline after activation
    Problem / Solution
    
    After the ServerBridge stage has installed the Transport and initiated
    server execution it doesn't need to be in the pipeline, so lets eject
    it after it's done its work to keep things lean and clean.
    
    Differential Revision: https://phabricator.twitter.biz/D250416

--- finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/ServerBridge.scala
+++ finagle-netty4/src/main/scala/com/twitter/finagle/netty4/channel/ServerBridge.scala
@@ -14,6 +14,9 @@ private[netty4] class ServerBridge[In, Out, Ctx <: TransportContext](
     extends ChannelInboundHandlerAdapter {
 
   override def channelActive(ctx: ChannelHandlerContext): Unit = {
+    // We immediately eject ourselves from the pipeline as we don't have
+    // any more value to provide.
+    ctx.pipeline.remove(this)
     val transport = transportFac(ctx.channel())
     serveTransport(transport)
   }
