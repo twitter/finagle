870c86393c59f8df1beda9174654261ade4ed19d
Kevin Oliver <koliver@twitter.com>
2019-02-27 23:13:20 +0000

    finagle-stats-core: Add MetricsView.of
    Problem
    
    There is no built-in functionality for merging two `MetricsViews`.
    
    Solution
    
    Introduce `MetricsView.of`.
    
    JIRA Issues: CSL-7167
    
    Differential Revision: https://phabricator.twitter.biz/D279199

--- finagle-stats-core/src/main/scala/com/twitter/finagle/stats/MetricsView.scala
+++ finagle-stats-core/src/main/scala/com/twitter/finagle/stats/MetricsView.scala
@@ -1,6 +1,7 @@
 package com.twitter.finagle.stats
 
-import java.util
+import java.util.{Map => JMap}
+import scala.collection.JavaConverters._
 
 /**
  * Provides snapshots of metrics values.
@@ -8,23 +9,56 @@ import java.util
 private[stats] trait MetricsView {
 
   /**
-   * A snapshot of instantaneous values for all gauges.
+   * A read-only snapshot of instantaneous values for all gauges.
    */
-  def gauges: util.Map[String, Number]
+  def gauges: JMap[String, Number]
 
   /**
-   * A snapshot of instantaneous values for all counters.
+   * A read-only snapshot of instantaneous values for all counters.
    */
-  def counters: util.Map[String, Number]
+  def counters: JMap[String, Number]
 
   /**
-   * A snapshot of instantaneous values for all histograms.
+   * A read-only snapshot of instantaneous values for all histograms.
    */
-  def histograms: util.Map[String, Snapshot]
+  def histograms: JMap[String, Snapshot]
 
   /**
-   * A snapshot of verbosity levels attached to each metric. For the sake of efficiency,
-   * metrics with verbosity [[Verbosity.Default]] aren't included into a returned map.
+   * A read-only snapshot of verbosity levels attached to each metric.
+   * For the sake of efficiency, metrics with verbosity [[Verbosity.Default]]
+   * aren't included into a returned map.
    */
-  def verbosity: util.Map[String, Verbosity]
+  def verbosity: JMap[String, Verbosity]
+}
+
+private[stats] object MetricsView {
+
+  private def merge[T](map1: JMap[String, T], map2: JMap[String, T]): JMap[String, T] = {
+    val merged = new java.util.HashMap[String, T](map1)
+    map2.asScala.foreach {
+      case (k, v) =>
+        merged.putIfAbsent(k, v)
+    }
+    java.util.Collections.unmodifiableMap(merged)
+  }
+
+  /**
+   * Creates a combined view over the inputs. Note that order matters here
+   * and should any key be duplicated, the value from the first view (`view1`)
+   * is used.
+   */
+  def of(view1: MetricsView, view2: MetricsView): MetricsView = new MetricsView {
+    def gauges: JMap[String, Number] =
+      merge(view1.gauges, view2.gauges)
+
+    def counters: JMap[String, Number] =
+      merge(view1.counters, view2.counters)
+
+    def histograms: JMap[String, Snapshot] =
+      merge(view1.histograms, view2.histograms)
+
+    def verbosity: JMap[String, Verbosity] =
+      merge(view1.verbosity, view2.verbosity)
+  }
+
 }
---
+++ finagle-stats-core/test/scala/com/twitter/finagle/stats/MetricsViewTest.scala
@@ -0,0 +1,83 @@
+package com.twitter.finagle.stats
+
+import java.util
+import java.util.Collections
+import org.scalatest.FunSuite
+import scala.collection.JavaConverters._
+
+class MetricsViewTest extends FunSuite {
+
+  private val EmptySnapshot: Snapshot = new Snapshot {
+    def count: Long = 0L
+    def sum: Long = 0L
+    def max: Long = 0L
+    def min: Long = 0L
+    def average: Double = 0L
+    def percentiles: IndexedSeq[Snapshot.Percentile] = IndexedSeq.empty
+  }
+
+  private class Impl(
+    val gauges: util.Map[String, Number] = Collections.emptyMap[String, Number],
+    val counters: util.Map[String, Number] = Collections.emptyMap[String, Number],
+    val histograms: util.Map[String, Snapshot] = Collections.emptyMap[String, Snapshot],
+    val verbosity: util.Map[String, Verbosity] = Collections.emptyMap[String, Verbosity])
+      extends MetricsView
+
+  private val a1 = new Impl(
+    gauges = Collections.singletonMap("a", 1),
+    counters = Collections.singletonMap("a", 1),
+    histograms = Collections.singletonMap("a", EmptySnapshot),
+    verbosity = Collections.singletonMap("a", Verbosity.Debug)
+  )
+
+  private val b2 = new Impl(
+    gauges = Collections.singletonMap("b", 2),
+    counters = Collections.singletonMap("b", 2),
+    histograms = Collections.singletonMap("b", EmptySnapshot),
+    verbosity = Collections.singletonMap("b", Verbosity.Debug)
+  )
+
+  test("of") {
+    val aAndB = MetricsView.of(a1, b2)
+    assert(Map("a" -> 1, "b" -> 2) == aAndB.gauges.asScala)
+    assert(Map("a" -> 1, "b" -> 2) == aAndB.counters.asScala)
+    assert(Map("a" -> EmptySnapshot, "b" -> EmptySnapshot) == aAndB.histograms.asScala)
+    assert(Map("a" -> Verbosity.Debug, "b" -> Verbosity.Debug) == aAndB.verbosity.asScala)
+  }
+
+  test("of handles duplicates") {
+    val c = new Impl(
+      gauges = Collections.singletonMap("a", 2),
+      counters = Collections.singletonMap("a", 2),
+      histograms = Collections.singletonMap("a", EmptySnapshot),
+      verbosity = Collections.singletonMap("a", Verbosity.Default)
+    )
+    val aAndC = MetricsView.of(a1, c)
+    assert(Map("a" -> 1) == aAndC.gauges.asScala)
+    assert(Map("a" -> 1) == aAndC.counters.asScala)
+    assert(Map("a" -> EmptySnapshot) == aAndC.histograms.asScala)
+    assert(Map("a" -> Verbosity.Debug) == aAndC.verbosity.asScala)
+
+    val cAndA = MetricsView.of(c, a1)
+    assert(Map("a" -> 2) == cAndA.gauges.asScala)
+    assert(Map("a" -> 2) == cAndA.counters.asScala)
+    assert(Map("a" -> EmptySnapshot) == cAndA.histograms.asScala)
+    assert(Map("a" -> Verbosity.Default) == cAndA.verbosity.asScala)
+  }
+
+  test("of ignores empty maps") {
+    val empty = new Impl()
+    val aAndEmpty = MetricsView.of(a1, empty)
+    assert(Map("a" -> 1) == aAndEmpty.gauges.asScala)
+    assert(Map("a" -> 1) == aAndEmpty.counters.asScala)
+    assert(Map("a" -> EmptySnapshot) == aAndEmpty.histograms.asScala)
+    assert(Map("a" -> Verbosity.Debug) == aAndEmpty.verbosity.asScala)
+
+    val emptyAndA = MetricsView.of(empty, a1)
+    assert(Map("a" -> 1) == emptyAndA.gauges.asScala)
+    assert(Map("a" -> 1) == emptyAndA.counters.asScala)
+    assert(Map("a" -> EmptySnapshot) == emptyAndA.histograms.asScala)
+    assert(Map("a" -> Verbosity.Debug) == emptyAndA.verbosity.asScala)
+  }
+
+}
