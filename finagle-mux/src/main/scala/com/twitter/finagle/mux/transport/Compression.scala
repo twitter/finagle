1deb86ceeab0a87c5fffa982a470597796d65d71
Moses Nakamura <mnakamura@twitter.com>
2019-09-18 01:33:32 +0000

    finagle-mux: Provides utilities for negotiating compression
    Problem
    
    When we enble negotiating compression, we need to communicate clearly between
    the client and server what each party wants the other to do.
    
    Solution
    
    Provide utilities for encoding and decoding header values for the client and
    the server for when we start negotiating compression.
    
    GitOrigin-RevId: b5fbcd9d24a1d8ebaa99e2eeec2711fcf52f55b1

---
+++ finagle-mux/src/main/scala/com/twitter/finagle/mux/transport/Compression.scala
@@ -0,0 +1,98 @@
+package com.twitter.finagle.mux.transport
+
+import io.netty.channel.ChannelHandler
+import io.netty.handler.codec.compression.{Lz4FrameEncoder, Lz4FrameDecoder}
+
+object Compression {
+  private[this] val isValidCharacter: Char => Boolean = { c: Char =>
+    ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
+  }
+
+  val Lz4String = "lz4"
+
+  /**
+   * Used for configuring the local preferences for compression and
+   * decompression.
+   */
+  case class LocalPreferences(compression: LocalSetting, decompression: LocalSetting)
+
+  /**
+   * Used for communicating the preferences of the client for compression and
+   * decompression over the wire.
+   */
+  case class PeerPreferences(compression: PeerSetting, decompression: PeerSetting)
+
+  /**
+   * The setting used for configuring the local preferences for either compression or
+   * decompression.
+   *
+   * @param level how much the party desires to compress
+   * @param transformers the ways the client can compress or decompress things
+   */
+  case class LocalSetting(level: CompressionLevel, transformers: Seq[ByteTransformer])
+
+  /**
+   * The setting used for transmitting over the wire the preferences of the
+   * client for either compression or decompression.
+   *
+   * @param level how much the party desires to compress
+   * @param transformerNames the names of the ways the client can compress or
+   *        decompress things
+   */
+  case class PeerSetting(level: CompressionLevel, transformerNames: Seq[String])
+
+  /**
+   * A named factory that makes a byte transformer that can be installed in the
+   * Netty pipeline.
+   *
+   * This must be called every time you make a new transformer for a differen stream.
+   * Transformers cannot share state between dfiferent streams.
+   */
+  sealed abstract class ByteTransformer(val name: String) {
+    protected[mux] def apply(): ChannelHandler
+
+    require(
+      name.forall(isValidCharacter),
+      s"Transformer $name contained an illegal character (alphanumeric ascii characters are valid)")
+  }
+
+  def lz4Compressor(highCompression: Boolean): ByteTransformer = new ByteTransformer(Lz4String) {
+    override protected[mux] def apply(): ChannelHandler =
+      new Lz4FrameEncoder( /* highCompression */ highCompression)
+  }
+
+  def lz4Decompressor(): ByteTransformer = new ByteTransformer(Lz4String) {
+    override protected[mux] def apply(): ChannelHandler = new Lz4FrameDecoder()
+  }
+
+  private[mux] def createTestByteTransformer(name: String): ByteTransformer =
+    new ByteTransformer(name) {
+      override protected[mux] def apply(): ChannelHandler = ???
+    }
+
+  /**
+   * The default preferences if compression format is not specified.
+   *
+   * Is unable to use compression for either the request or the response.
+   * May change in the future.
+   */
+  val DefaultLocal: LocalPreferences = LocalPreferences(
+    LocalSetting(CompressionLevel.Off, Nil),
+    LocalSetting(CompressionLevel.Off, Nil)
+  )
+
+  /**
+   * Is unable to use compression for either the request or the response.
+   */
+  val PeerCompressionOff: PeerPreferences =
+    PeerPreferences(PeerSetting(CompressionLevel.Off, Nil), PeerSetting(CompressionLevel.Off, Nil))
+
+  /**
+   * The default preferences of the remote peer if compression format is not
+   * specified.
+   *
+   * Is unable to use compression for either the request or the response.
+   * May change in the future.
+   */
+  val DefaultPeer: PeerPreferences = PeerCompressionOff
+}
---
+++ finagle-mux/src/test/scala/com/twitter/finagle/mux/transport/CompressionNegotiationTest.scala
@@ -0,0 +1,117 @@
+package com.twitter.finagle.mux.transport
+
+import com.twitter.finagle.mux.transport.Compression._
+import com.twitter.finagle.mux.transport.CompressionNegotiation._
+import com.twitter.io.Buf
+import org.scalatest.FunSuite
+
+class CompressionNegotiationTest extends FunSuite {
+  test(s"Can encode and decode preferences") {
+    CompressionTest.configurations.foreach { preferences =>
+      val preferencesString = preferences.toString
+      val encoded = ClientHeader.encode(preferences)
+      assert(ClientHeader.decode(encoded) == CompressionTest.toPeerPreferences(preferences))
+    }
+  }
+
+  test(s"Can encode and decode compression formats") {
+    Seq(
+      CompressionFormats(None, None),
+      CompressionFormats(Some("foo"), None),
+      CompressionFormats(None, Some("foo")),
+      CompressionFormats(Some("foo"), Some("bar"))
+    ).foreach { compressionFormats =>
+      val formatString = compressionFormats.toString
+      val encoded = ServerHeader.encode(compressionFormats)
+      assert(ServerHeader.decode(encoded) == compressionFormats)
+    }
+  }
+
+  test(s"Can't make a LocalSetting with a transformer with an illegal character in the name") {
+    Seq(';', ',', ':').foreach { badChar =>
+      intercept[IllegalArgumentException] {
+        LocalSetting(CompressionLevel.Off, Seq(createTestByteTransformer("foo" + badChar)))
+      }
+    }
+
+    // just want to check we do OK for all of these
+    Seq('a', 'l', 'z', 'A', 'L', 'Z', '0', '5', '9').foreach { goodChar =>
+      LocalSetting(CompressionLevel.Off, Seq(createTestByteTransformer("foo" + goodChar)))
+    }
+  }
+
+  private[this] def validateState(
+    compressionFormat: Option[String],
+    server: LocalSetting,
+    client: PeerSetting
+  ): Unit = compressionFormat match {
+    case Some(format) =>
+      assert(server.transformers.exists(_.name == format))
+      assert(client.transformerNames.contains(format))
+      assert(client.level != CompressionLevel.Off)
+      assert(server.level != CompressionLevel.Off)
+      assert(client.level == CompressionLevel.Desired || server.level == CompressionLevel.Desired)
+    case None =>
+      assert(
+        client.level == CompressionLevel.Off ||
+          server.level == CompressionLevel.Off ||
+          (client.level == CompressionLevel.Accepted &&
+            server.level == CompressionLevel.Accepted) ||
+          server.transformers.map(_.name).intersect(client.transformerNames).isEmpty
+      )
+  }
+
+  test("Compression negotiation passes some sniff tests") {
+    for {
+      server <- CompressionTest.configurations
+      rawClient <- CompressionTest.configurations
+      client = CompressionTest.toPeerPreferences(rawClient)
+      result = negotiate(server, client)
+    } {
+      val requestCompression = result.request
+      val responseCompression = result.response
+      validateState(requestCompression, server.decompression, client.compression)
+      validateState(responseCompression, server.compression, client.decompression)
+    }
+  }
+
+  test("Fails in a forwards-compatible way for not understanding preferences") {
+    assert(ClientHeader.decode(Buf.Utf8("foo")) == Compression.PeerCompressionOff)
+  }
+
+  test("Fails hard when it doesn't understand the negotiated compression formats") {
+    intercept[IllegalArgumentException] {
+      ServerHeader.decode(Buf.Utf8("foo"))
+    }
+  }
+}
+
+object CompressionTest {
+  private[this] val identityTransformer = createTestByteTransformer("identity")
+  private[this] val reverseTransformer = createTestByteTransformer("reverse")
+  private[this] val combinations = Seq(
+    Seq(identityTransformer, reverseTransformer),
+    Seq(reverseTransformer, identityTransformer),
+    Seq(identityTransformer),
+    Seq(reverseTransformer),
+    Nil
+  )
+
+  private[this] def createTests(level: CompressionLevel): Seq[LocalSetting] =
+    combinations.map(LocalSetting(level, _))
+  private[this] val off = createTests(CompressionLevel.Off)
+  private[this] val accepting = createTests(CompressionLevel.Accepted)
+  private[this] val desiring = createTests(CompressionLevel.Desired)
+
+  private[this] val settings = off ++ accepting ++ desiring
+  val configurations = for {
+    left <- settings
+    right <- settings
+  } yield LocalPreferences(left, right)
+
+  private[this] def toPeerSetting(local: LocalSetting): PeerSetting =
+    PeerSetting(local.level, local.transformers.map(_.name))
+
+  def toPeerPreferences(local: LocalPreferences): PeerPreferences =
+    PeerPreferences(toPeerSetting(local.compression), toPeerSetting(local.decompression))
+}
