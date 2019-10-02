a46ef1e133907216db9751e5a5eae9705119aa5d
Jing Yan <jyan@twitter.com>
2019-10-02 15:51:25 +0000

    finagle-mysql: Specify Finagle Stat Client from import
    Problem
    Sbt Scala 2.13 build doesn't recognize shadowed import
    
    Solution
    Specify full class name from import
    
    GitOrigin-RevId: 0c058dec0b88bb79edf9ee2a88162d9624363c1c

--- finagle-mysql/src/test/scala/com/twitter/finagle/mysql/unit/MetricsTest.scala
+++ finagle-mysql/src/test/scala/com/twitter/finagle/mysql/unit/MetricsTest.scala
@@ -1,7 +1,7 @@
 package com.twitter.finagle.mysql
 
 import com.twitter.conversions.DurationOps._
-import com.twitter.finagle.stats._
+import com.twitter.finagle.stats.{Client => _, _}
 import com.twitter.util.{Await, Awaitable, Future}
 import java.util.concurrent.atomic.LongAdder
 import org.mockito.Mockito._
