389ebb0aa1b240b1adfa8b918ea4e80a6efa9f18
Vladimir Kostyukov <vkostyukov@twitter.com>
2019-10-18 22:06:21 +0000

    Revert "finagle-core: aperture balancer status check"
    Problem
    
    The commit 81e6b01050c235b1b3207022d962b582993cab63 changes the way we classify some
    of the connect failures on a client, allowing them to bubble up under `logical/failures`. This might
    affect SR for people.
    
    Solution
    
    Revert patch.
    
    JIRA Issues: CSL-8961
    
    Differential Revision: https://phabricator.twitter.biz/D387215

--- finagle-core/src/main/scala/com/twitter/finagle/loadbalancer/aperture/Aperture.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/loadbalancer/aperture/Aperture.scala
@@ -302,12 +302,8 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
      */
     def indices: Set[Int]
 
-    /*
-     * Returns the best status of nodes within `indices`
-     */
-    def status: Status
-
     def additionalMetadata: Map[String, Any]
+
   }
 
   /**
@@ -317,7 +313,6 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
   protected class EmptyVector(initAperture: Int) extends BaseDist(Vector.empty, initAperture) {
     require(vector.isEmpty, s"vector must be empty: $vector")
     def indices: Set[Int] = Set.empty
-    def status: Status = Status.Closed
     def pick(): Node = failingNode(emptyException)
     def needsRebuild: Boolean = false
     def additionalMetadata: Map[String, Any] = Map.empty
@@ -373,7 +368,7 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
     // by `token` which is deterministic across rebuilds but random
     // globally, since `token` is assigned randomly per process
     // when the node is created.
-    protected val vec: Vector[Node] = statusOrder(vector.sortBy(nodeToken))
+    protected val vec = statusOrder(vector.sortBy(nodeToken))
     protected def bound: Int = logicalAperture
     protected def emptyNode: Node = failingNode(emptyException)
     protected def rng: Rng = self.rng
@@ -389,22 +384,6 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
     }
 
     def indices: Set[Int] = (0 until logicalAperture).toSet
-
-    def status: Status = {
-      // acceptable race condition here with `adjust` that can update
-      // _logicalAperture
-      val logicalAperture = this.logicalAperture
-
-      var i = 0
-      var status: Status = Status.Closed
-      while (i < logicalAperture && status != Status.Open) {
-        status = Status.best(status, vec(i).factory.status)
-
-        i += 1
-      }
-
-      status
-    }
 
     // To reduce the amount of rebuilds needed, we rely on the probabilistic
     // nature of p2c pick. That is, we know that only when a significant
@@ -442,7 +421,7 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
     // and how we calculate the `apertureWidth`.
     override def min: Int = math.min(Aperture.MinDeterministicAperture, vector.size)
 
-    // DeterministicAperture does not dynamically adjust the aperture based on load
+    // If we don't allow the aperture to be dynamic based on the load, we just use the min value.
     override def logicalAperture: Int = min
 
     // Translates the logical `aperture` into a physical one that
@@ -493,25 +472,6 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
         val weight = ring.weight(i, offset, width)
         (i, weight, addr)
       }
-    }
-
-    // Save an array-version of the indices that we can traverse over index-wise for
-    // a zero allocation `status` implementation.
-    //
-    // DeterministicAperture has `coord` and `logicalAperture` fixed on construction,
-    // used to compute `indicies`. We can safely cache the resulting set into `seqIndices`.
-    // This cache will be recomputed on subsequent rebuilds.
-    private[this] val seqIndices: Array[Int] = indices.toArray
-    def status: Status = {
-      var i = 0
-      var status: Status = Status.Closed
-      while (i < seqIndices.length && status != Status.Open) {
-        status = Status.best(status, vector(seqIndices(i)).factory.status)
-
-        i += 1
-      }
-
-      status
     }
 
     // We log the contents of the aperture on each distributor rebuild when using
@@ -606,6 +566,4 @@ private[loadbalancer] trait Aperture[Req, Rep] extends Balancer[Req, Rep] { self
     gauges.foreach(_.remove())
     coordObservation.close(deadline).before { super.close(deadline) }
   }
-
-  override def status: Status = dist.status
 }
