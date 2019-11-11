e305e39c9804218f367ab4f8e4ed7a83f5896c89
Martijn Hoekstra <martijn.hoekstra@cargoxs.com>
2019-11-11 15:00:38 +0100

    rm HashSet based header map

--- finagle-base-http/src/main/scala-2.12-/com/twitter/finagle/http/headers/DefaultHeaderMap.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.finagle.http.headers
-
-import com.twitter.finagle.http.HeaderMap
-
-object DefaultHeaderMap {
-  def apply(headers: (String, String)*): HeaderMap = JTreeMapBackedHeaderMap(headers: _*)
-}
