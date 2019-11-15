630671cbe56bf1320f705d3c7b50627f907bc300
Moses Nakamura <mnakamura@twitter.com>
2019-11-15 15:27:23 +0000

    util: Add a tool to make metrics lazy
    Problem
    
    Sometimes we don't know in advance if we're going to be using certain metrics.
    However, we don't want to have to change our programming model to have to know
    in advance if things are lazy.
    
    Solution
    
    Provide a LazyStatsReceiver that will automatically make metrics lazy without
    having to rewrite where they're used.
    
    JIRA Issues: CSL-9070
    
    Differential Revision: https://phabricator.twitter.biz/D398898

--- finagle-benchmark/src/main/scala/com/twitter/finagle/stats/StatsReceiverBenchmark.scala
+++ finagle-benchmark/src/main/scala/com/twitter/finagle/stats/StatsReceiverBenchmark.scala
@@ -14,52 +14,86 @@ abstract class StatsReceiverBenchmark extends StdBenchAnnotations {
 
   @annotations.Benchmark
   def scope(): StatsReceiver =
-    state.stats.scope(state.next())
+    state.stats.scope(state.nextString())
 
   @annotations.Benchmark
   def counter(): Counter =
-    state.stats.counter(state.next())
+    state.stats.counter(state.nextString())
 
   @annotations.Benchmark
   def stat(): Stat =
-    state.stats.stat(state.next())
+    state.stats.stat(state.nextString())
+
+  @annotations.Benchmark
+  def incr(): Unit = state.counter.incr()
+
+  @annotations.Benchmark
+  def add(): Unit = state.stat.add(state.nextNatural())
+
+  @annotations.Benchmark
+  def counterAndIncr(): Counter = {
+    val ctr = state.stats.counter(state.nextString())
+    ctr.incr()
+    ctr
+  }
+
+  @annotations.Benchmark
+  def statAndAdd(): Stat = {
+    val st = state.stats.stat(state.nextString())
+    st.add(state.nextNatural())
+    st
+  }
 
   @annotations.Benchmark
   def scopeAndScope(): StatsReceiver =
-    state.stats.scope(state.next()).scope(state.next())
+    state.stats.scope(state.nextString()).scope(state.nextString())
 
   @annotations.Benchmark
   def scopeAndCounter(): Counter =
-    state.stats.scope(state.next()).counter(state.next())
+    state.stats.scope(state.nextString()).counter(state.nextString())
 
   @annotations.Benchmark
   def scopeAndStat(): Stat =
-    state.stats.scope(state.next()).stat(state.next())
+    state.stats.scope(state.nextString()).stat(state.nextString())
 
   @annotations.Benchmark
   def scopedCounter(): Counter =
-    state.stats.counter(state.next(), state.next())
+    state.stats.counter(state.nextString(), state.nextString())
 
   @annotations.Benchmark
   def scopedStat(): Stat =
-    state.stats.stat(state.next(), state.next())
+    state.stats.stat(state.nextString(), state.nextString())
 }
 
 object StatsReceiverBenchmark {
 
   val Names = Array.fill(1000)(Random.alphanumeric.take(12).mkString)
+  val Naturals = Array.fill(1000)(Random.nextInt().toFloat)
 
-  class State(val stats: StatsReceiver) {
+  abstract class State(val stats: StatsReceiver) {
     private var i = 0
-    def next(): String =
+    def nextString(): String =
       try Names(i)
       finally i = if (i + 1 == Names.length) 0 else i + 1
+
+    private var j = 0
+    def nextNatural(): Float = {
+      try Naturals(j)
+      finally j = if (j + 1 == Naturals.length) 0 else j + 1
+    }
+
+    val counter: Counter = stats.counter("foo")
+
+    val stat: Stat = stats.stat("bar")
   }
 
   final class MetricsState extends State(new MetricsStatsReceiver(Metrics.createDetached()))
 
   final class MemoizingState
       extends State(new Memoizing(new MetricsStatsReceiver(Metrics.createDetached())))
+
+  final class LazyState
+      extends State(new LazyStatsReceiver(new MetricsStatsReceiver(Metrics.createDetached())))
 
   final class Memoizing(val self: StatsReceiver)
       extends StatsReceiver
@@ -108,4 +142,8 @@ class MetricsStatsReceiverBenchmark extends StatsReceiverBenchmark {
 
 class MemoizingStatsReceiverBenchmark extends StatsReceiverBenchmark {
   protected val state = new StatsReceiverBenchmark.MemoizingState
+}
+
+class LazyStatsReceiverBenchmark extends StatsReceiverBenchmark {
+  protected val state = new StatsReceiverBenchmark.LazyState
 }
