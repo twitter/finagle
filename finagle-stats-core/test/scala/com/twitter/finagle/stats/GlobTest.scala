ca47c880775432b17870ea721d50e2c62d09fa18
Vladimir Kostyukov <vkostyukov@twitter.com>
2019-12-06 22:19:40 +0000

    finagle-stats: Faster Glob matching
    Problem
    
    Verbosity levels can be adjusted via a glob expression passed as a flag or a tunable. We evaluate it
    on every request to `/metrics.json` and want to do better than converting to a more powerful
    regex construct.
    
    Solution
    
    Introduce `Glob` matcher, which is at least 10x faster than what we're doing before.
    
    GitOrigin-RevId: 4c175fa86658bc44cef30c2f95efe143b92bd467

---
+++ finagle-stats-core/test/scala/com/twitter/finagle/stats/GlobTest.scala
@@ -0,0 +1,56 @@
+package com.twitter.finagle.stats
+
+import org.scalatest.FunSuite
+
+class GlobTest extends FunSuite {
+
+  test("exact") {
+    assert(Glob("foobar")("foobar"))
+    assert(!Glob("foobar")("foobarr"))
+    assert(!Glob("fooba")("foobar"))
+  }
+
+  test("single wildcard") {
+    assert(Glob("*")(""))
+    assert(Glob("*")("foo"))
+    assert(Glob("foo*")("foobar"))
+    assert(Glob("foo*")("foo"))
+    assert(!Glob("foo*")("bar"))
+    assert(Glob("*foo")("foo"))
+    assert(Glob("*foo")("barfoo"))
+    assert(!Glob("*foo")("foobar"))
+  }
+
+  test("two wildcards") {
+    assert(Glob("**")(""))
+    assert(Glob("*foo*")("foo"))
+    assert(Glob("*foo*")("barfoobaz"))
+    assert(Glob("*foo*")("foobar"))
+    assert(Glob("*foo*")("barfoo"))
+    assert(!Glob("*foo*")("bar"))
+  }
+
+  test("two segments") {
+    assert(Glob("foo*bar")("foobar"))
+    assert(Glob("foo*bar")("foobazbar"))
+    assert(!Glob("foo*bar")("foo"))
+    assert(!Glob("foo*bar")("bar"))
+  }
+
+  test("comma (or)") {
+    assert(Glob("foo,bar")("foo"))
+    assert(Glob("foo,bar")("bar"))
+    assert(!Glob("foo,bar")("baz"))
+    assert(Glob("foo,*")("baz"))
+    assert(Glob("foo,*")("foo"))
+    assert(Glob("*,foo")("foo"))
+    assert(Glob("*,foo")("bar"))
+    assert(Glob("*,foo")("foo"))
+    assert(Glob("foo*foo,*bar")("foofoo"))
+    assert(Glob("foo*foo,*bar")("foodfoo"))
+    assert(Glob("foo*foo,*bar")("foobar"))
+    assert(Glob("foo*foo,*bar")("bar"))
+    assert(Glob("foo*foo,*bar")("barbar"))
+    assert(!Glob("foo*foo,*bar")("barbaz"))
+  }
+}
