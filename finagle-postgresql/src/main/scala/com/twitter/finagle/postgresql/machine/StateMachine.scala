82817c72c4850b75c744b4124f0031fb28bf5282
plaflamme <philippe@hopper.com>
2020-04-12 16:18:06 -0400

    Convert Sync to a state machine.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
@@ -15,4 +15,13 @@ object StateMachine {
   case class TransitionAndSend[S, M <: FrontendMessage](state: S, msg: M)(implicit val encoder: MessageEncoder[M]) extends TransitionResult[S, Nothing]
   case class Transition[S](state: S) extends TransitionResult[S, Nothing]
   case class Complete[R](value: R, signal: Future[Messages.ReadyForQuery]) extends TransitionResult[Nothing, R]
+
+  /** A machine that sends a single frontend message and expects a ReadyForQuery response */
+  def singleMachine[M <: Messages.FrontendMessage: MessageEncoder, R](msg: M)(f: Messages.ReadyForQuery => R): StateMachine[Unit, R] = new StateMachine[Unit, R] {
+    override def start: TransitionResult[Unit, R] = TransitionAndSend((), msg)
+    override def receive(state: Unit, msg: BackendMessage): TransitionResult[Unit, R] = msg match {
+      case r: Messages.ReadyForQuery => Complete(f(r), Future.value(r))
+      case msg => sys.error(s"unexpected response $msg")
+    }
+  }
 }
