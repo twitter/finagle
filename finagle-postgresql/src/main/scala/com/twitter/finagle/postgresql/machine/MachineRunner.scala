2107d4d00eacfd6e5d9eb0f06af5b68121af1392
plaflamme <philippe@hopper.com>
2020-04-12 14:13:57 -0400

    Simplify state machine.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/MachineRunner.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/MachineRunner.scala
@@ -7,18 +7,15 @@ import com.twitter.util.Future
 
 case class MachineRunner[S, R](transport: Transport[Packet, Packet], machine: StateMachine[S, R]) {
 
-  var state: S = machine.init
+  var state: S = _
 
   private[this] def readMsg = transport.read().map(Messages.BackendMessage.read)
 
   private[this] def step(transition: StateMachine.TransitionResult[S, R]): Future[R] = transition match {
-    case StateMachine.Send(s, msg) =>
+    case StateMachine.Transition(s, action) =>
       state = s
-      transport.write(msg.write) before readAndStep
+      action.fold(Future.Done) { msg => transport.write(msg.write) } before readAndStep
     case StateMachine.Complete(result) => Future.value(result)
-    case StateMachine.Noop(s) =>
-      state = s
-      readAndStep
   }
   private[this] def readAndStep =
     readMsg.flatMap { msg => step(machine.receive(state, msg)) }
