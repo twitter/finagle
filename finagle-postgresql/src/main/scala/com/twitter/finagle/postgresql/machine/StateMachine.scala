98395fb46401a786ffe2f6e40538d46bd89eb0e2
plaflamme <philippe@hopper.com>
2020-04-13 18:20:53 -0400

    Split transitions and actions.
    This cleans up the abstractions: one is responsible for transitioning the state machine's state and the other captures the action that should happen as a consequence of the transition.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
@@ -17,19 +17,23 @@ trait StateMachine[+R <: Response] {
   def receive(state: State, msg: BackendMessage): StateMachine.TransitionResult[State, R]
 }
 object StateMachine {
+
+  sealed trait Action[+R <: Response]
+  case object NoOp extends Action[Nothing]
+  case class Send[M <: FrontendMessage](msg: M)(implicit val encoder: MessageEncoder[M]) extends Action[Nothing]
+  case class Respond[R <: Response](value: Try[R]) extends Action[R]
+
   sealed trait TransitionResult[+S, +R <: Response]
-  case class TransitionAndSend[S, M <: FrontendMessage](state: S, msg: M)(implicit val encoder: MessageEncoder[M]) extends TransitionResult[S, Nothing]
-  case class Transition[S](state: S) extends TransitionResult[S, Nothing]
-  case class Respond[S, R <: Response](state: S, value: Try[R]) extends TransitionResult[S, R]
+  case class Transition[S, R <: Response](state: S, action: Action[R]) extends TransitionResult[S, R]
   case class Complete[R <: Response](ready: ReadyForQuery, response: Option[Try[R]]) extends TransitionResult[Nothing, R]
 
   /** A machine that sends a single frontend message and expects a ReadyForQuery response */
   def singleMachine[M <: FrontendMessage: MessageEncoder, R <: Response](msg: M)(f: BackendMessage.ReadyForQuery => R): StateMachine[R] = new StateMachine[R] {
     override type State = Unit
-    override def start: TransitionResult[State, R] = TransitionAndSend((), msg)
+    override def start: TransitionResult[State, R] = Transition((), Send(msg))
     override def receive(state: State, msg: BackendMessage): TransitionResult[State, R] = msg match {
       case r: BackendMessage.ReadyForQuery => Complete(r, Some(Return(f(r))))
-      case e: BackendMessage.ErrorResponse => Respond(Unit, Throw(PgSqlServerError(e)))
+      case e: BackendMessage.ErrorResponse => Transition(Unit, Respond(Throw(PgSqlServerError(e))))
       case msg => throw PgSqlStateMachineError("anonymous", (), msg)
     }
   }
