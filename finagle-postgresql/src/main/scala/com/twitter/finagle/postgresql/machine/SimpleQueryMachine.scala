9cd3c95b01b2329f4268f71695f0d8ea25116d4e
plaflamme <philippe@hopper.com>
2020-04-12 22:17:18 -0400

    Adds error handling
    This also forces machines to result in a `Try[Response]` type.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/SimpleQueryMachine.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/SimpleQueryMachine.scala
@@ -2,28 +2,35 @@ package com.twitter.finagle.postgresql.machine
 
 import com.twitter.finagle.postgresql.BackendMessage
 import com.twitter.finagle.postgresql.BackendMessage.EmptyQueryResponse
+import com.twitter.finagle.postgresql.BackendMessage.ErrorResponse
 import com.twitter.finagle.postgresql.BackendMessage.ReadyForQuery
-import com.twitter.finagle.postgresql.BackendResponse
 import com.twitter.finagle.postgresql.FrontendMessage
+import com.twitter.finagle.postgresql.PgSqlServerError
+import com.twitter.finagle.postgresql.PgSqlStateMachineError
 import com.twitter.finagle.postgresql.Response
 import com.twitter.util.Future
+import com.twitter.util.Return
+import com.twitter.util.Throw
+import com.twitter.util.Try
 
-class SimpleQueryMachine(query: String) extends StateMachine[SimpleQueryMachine.State, Response] {
+class SimpleQueryMachine(query: String) extends StateMachine[SimpleQueryMachine.State, Response.BackendResponse] {
 
   import SimpleQueryMachine._
 
-  override def start: StateMachine.TransitionResult[SimpleQueryMachine.State, Response] =
+  override def start: StateMachine.TransitionResult[SimpleQueryMachine.State, Response.BackendResponse] =
     StateMachine.TransitionAndSend(Init, FrontendMessage.Query(query))
 
-  override def receive(state: SimpleQueryMachine.State, msg: BackendMessage): StateMachine.TransitionResult[SimpleQueryMachine.State, Response] = (state, msg) match {
-    case (Init, EmptyQueryResponse) => StateMachine.Transition(Complete(BackendResponse(EmptyQueryResponse)))
-    case (Complete(response), r: ReadyForQuery) => StateMachine.Complete(response, Future.value(r))
-    case (state, msg) => sys.error(s"unhandled state $state event $msg")
+  override def receive(state: SimpleQueryMachine.State, msg: BackendMessage): StateMachine.TransitionResult[SimpleQueryMachine.State, Response.BackendResponse] = (state, msg) match {
+    case (Init, EmptyQueryResponse) => StateMachine.Transition(Complete(Return(Response.BackendResponse(EmptyQueryResponse))))
+    case (Complete(response), r: ReadyForQuery) => StateMachine.Respond(response, Future.value(r))
+    case (_, e: ErrorResponse) => StateMachine.Transition(Complete(Throw(PgSqlServerError(e))))
+    case (state, msg) =>
+      StateMachine.Respond(Throw(PgSqlStateMachineError("SimpleQueryMachine", state.toString, msg)), Future.exception(new RuntimeException))
   }
 }
 
 object SimpleQueryMachine {
   sealed trait State
   case object Init extends State
-  case class Complete(r: Response) extends State
+  case class Complete(response: Try[Response.BackendResponse]) extends State
 }
--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
@@ -2,25 +2,32 @@ package com.twitter.finagle.postgresql.machine
 
 import com.twitter.finagle.postgresql.BackendMessage
 import com.twitter.finagle.postgresql.FrontendMessage
+import com.twitter.finagle.postgresql.PgSqlServerError
+import com.twitter.finagle.postgresql.PgSqlStateMachineError
+import com.twitter.finagle.postgresql.Response
 import com.twitter.finagle.postgresql.transport.MessageEncoder
 import com.twitter.util.Future
+import com.twitter.util.Return
+import com.twitter.util.Throw
+import com.twitter.util.Try
 
-trait StateMachine[S, R] {
+trait StateMachine[S, +R <: Response] {
   def start: StateMachine.TransitionResult[S, R]
   def receive(state: S, msg: BackendMessage): StateMachine.TransitionResult[S, R]
 }
 object StateMachine {
-  sealed trait TransitionResult[+S, +R]
+  sealed trait TransitionResult[+S, +R <: Response]
   case class TransitionAndSend[S, M <: FrontendMessage](state: S, msg: M)(implicit val encoder: MessageEncoder[M]) extends TransitionResult[S, Nothing]
   case class Transition[S](state: S) extends TransitionResult[S, Nothing]
-  case class Complete[R](value: R, signal: Future[BackendMessage.ReadyForQuery]) extends TransitionResult[Nothing, R]
+  case class Respond[R <: Response](value: Try[R], signal: Future[BackendMessage.ReadyForQuery]) extends TransitionResult[Nothing, R]
 
   /** A machine that sends a single frontend message and expects a ReadyForQuery response */
-  def singleMachine[M <: FrontendMessage: MessageEncoder, R](msg: M)(f: BackendMessage.ReadyForQuery => R): StateMachine[Unit, R] = new StateMachine[Unit, R] {
+  def singleMachine[M <: FrontendMessage: MessageEncoder, R <: Response](msg: M)(f: BackendMessage.ReadyForQuery => R): StateMachine[Unit, R] = new StateMachine[Unit, R] {
     override def start: TransitionResult[Unit, R] = TransitionAndSend((), msg)
     override def receive(state: Unit, msg: BackendMessage): TransitionResult[Unit, R] = msg match {
-      case r: BackendMessage.ReadyForQuery => Complete(f(r), Future.value(r))
-      case msg => sys.error(s"unexpected response $msg")
+      case r: BackendMessage.ReadyForQuery => Respond(Return(f(r)), Future.value(r))
+      case e: BackendMessage.ErrorResponse => Respond(Throw(PgSqlServerError(e)), Future.exception(new RuntimeException)) // TODO: what happens when the signal is an exception?
+      case msg => Respond(Throw(PgSqlStateMachineError("anonymous", "n/a", msg)), Future.exception(new RuntimeException))
     }
   }
 }
