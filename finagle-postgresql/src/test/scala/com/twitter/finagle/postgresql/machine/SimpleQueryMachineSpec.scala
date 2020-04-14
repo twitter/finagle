986d330f6f480a3b83d446906c466c9498abee38
plaflamme <philippe@hopper.com>
2020-04-14 18:20:38 -0400

    Adds basic test setup for result sets.

--- finagle-postgresql/src/test/scala/com/twitter/finagle/postgresql/machine/SimpleQueryMachineSpec.scala
+++ finagle-postgresql/src/test/scala/com/twitter/finagle/postgresql/machine/SimpleQueryMachineSpec.scala
@@ -1,13 +1,22 @@
 package com.twitter.finagle.postgresql.machine
 
 import com.twitter.finagle.postgresql.BackendMessage
+import com.twitter.finagle.postgresql.BackendMessage.DataRow
 import com.twitter.finagle.postgresql.BackendMessage.EmptyQueryResponse
+import com.twitter.finagle.postgresql.BackendMessage.FieldDescription
+import com.twitter.finagle.postgresql.BackendMessage.Format
+import com.twitter.finagle.postgresql.BackendMessage.Oid
+import com.twitter.finagle.postgresql.BackendMessage.RowDescription
 import com.twitter.finagle.postgresql.FrontendMessage
 import com.twitter.finagle.postgresql.Response
 import com.twitter.finagle.postgresql.machine.StateMachine.Complete
 import com.twitter.finagle.postgresql.machine.StateMachine.Respond
 import com.twitter.finagle.postgresql.machine.StateMachine.Send
 import com.twitter.finagle.postgresql.machine.StateMachine.Transition
+import com.twitter.io.Buf
+import com.twitter.io.Reader
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen
 import org.scalacheck.Prop.forAll
 import org.specs2.ScalaCheck
 
@@ -63,6 +72,66 @@ class SimpleQueryMachineSpec extends MachineSpec[Response] with ScalaCheck {
         receive(readyForQuery),
         checkCompletes
       )
+    }
+
+    // TODO
+    implicit lazy val arbRowDescription: Arbitrary[RowDescription] = Arbitrary {
+      Gen.const(
+        RowDescription(
+          List(
+            FieldDescription("field", None, None, Oid(42), 0, 0, Format.Text)
+          )
+        )
+      )
+    }
+    implicit lazy val arbDataRow: Arbitrary[DataRow] = Arbitrary {
+      for {
+        bytes <- implicitly[Arbitrary[Array[Byte]]].arbitrary.suchThat(_.nonEmpty)
+      } yield DataRow(Buf.ByteArray.Owned(bytes) :: Nil)
+    }
+
+    def resultSetSpec(query: String, rowDesc: RowDescription, rows: List[DataRow]): Reader[DataRow] = {
+      var rowReader: Option[Reader[DataRow]] = None
+
+      val prep = List(
+        checkQuery(query),
+        receive(rowDesc),
+        checkResult("responds") {
+          case Transition(_, Respond(value)) =>
+            value.asScala must beASuccessfulTry
+            value.get must beLike {
+              case Response.ResultSet(desc, reader) =>
+                rowReader = Some(reader)
+                desc must beEqualTo(rowDesc)
+            }
+        }
+      )
+
+      val sendRows = rows.map(receive)
+
+      val post = List(
+        receive(BackendMessage.CommandComplete("command tag")),
+        receive(readyForQuery),
+        checkCompletes
+      )
+
+      machineSpec(mkMachine(query))(
+        prep ++ sendRows ++ post: _*
+      )
+
+      rowReader must beSome
+      rowReader.get
+    }
+
+    "support empty result sets" in forAll { rowDesc: RowDescription =>
+      Reader.toAsyncStream(resultSetSpec("select 1;", rowDesc, Nil)).toSeq.map { rows =>
+        rows must beEmpty
+      }
+    }
+    "return rows in order" in forAll { (rowDesc: RowDescription, rows: List[DataRow]) =>
+      Reader.toAsyncStream(resultSetSpec("select 1;", rowDesc, rows)).toSeq.map { rows =>
+        rows must beEqualTo(rows)
+      }
     }
   }
 }
