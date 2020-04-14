c2c207096ea2ba4d939fe3a0a0129053feef73b9
plaflamme <philippe@hopper.com>
2020-04-13 20:43:14 -0400

    Name anonymous machine.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/machine/StateMachine.scala
@@ -28,13 +28,13 @@ object StateMachine {
   case class Complete[R <: Response](ready: ReadyForQuery, response: Option[Try[R]]) extends TransitionResult[Nothing, R]
 
   /** A machine that sends a single frontend message and expects a ReadyForQuery response */
-  def singleMachine[M <: FrontendMessage: MessageEncoder, R <: Response](msg: M)(f: BackendMessage.ReadyForQuery => R): StateMachine[R] = new StateMachine[R] {
+  def singleMachine[M <: FrontendMessage: MessageEncoder, R <: Response](name: String, msg: M)(f: BackendMessage.ReadyForQuery => R): StateMachine[R] = new StateMachine[R] {
     override type State = Unit
     override def start: TransitionResult[State, R] = Transition((), Send(msg))
     override def receive(state: State, msg: BackendMessage): TransitionResult[State, R] = msg match {
       case r: BackendMessage.ReadyForQuery => Complete(r, Some(Return(f(r))))
       case e: BackendMessage.ErrorResponse => Transition(Unit, Respond(Throw(PgSqlServerError(e))))
-      case msg => throw PgSqlStateMachineError("anonymous", (), msg)
+      case msg => throw PgSqlStateMachineError(name, (), msg)
     }
   }
 }
