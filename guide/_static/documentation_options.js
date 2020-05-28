b7b8e8e1afa35872dd5957c3f8da14a1cd3fe9b0
Ian Bennett <ibennett@twitter.com>
2020-05-27 17:02:47 -0700

    site push by ibennett

--- guide/_static/documentation_options.js
+++ guide/_static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.4.1',
+    VERSION: '20.5.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
