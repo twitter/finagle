01466101beb3d12be565bbff58c334c6b9c3fe75
Scala Steward <me@scala-steward.org>
2020-06-19 12:40:24 +0200

    Update scalatest to 3.2.0

--- src/test/scala/com/twitter/finagle/postgres/Spec.scala
+++ src/test/scala/com/twitter/finagle/postgres/Spec.scala
@@ -1,13 +1,14 @@
 package com.twitter.finagle.postgres
 
-import org.scalatest.{BeforeAndAfter, WordSpec}
-import org.scalatest.MustMatchers
+import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 /*
  * Base class for finagle postgres tests.
  */
 class Spec
-  extends WordSpec
-  with MustMatchers
+  extends AnyWordSpec
+  with Matchers
   with BeforeAndAfter {
 }
