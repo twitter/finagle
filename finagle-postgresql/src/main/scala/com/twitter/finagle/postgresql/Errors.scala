5c0e378526b157e5259ce2cd8deb01ad1d0595c7
plaflamme <philippe@hopper.com>
2020-10-17 15:02:29 -0400

    Cleanup exceptions, ensure they all have a message defined.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/Errors.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/Errors.scala
@@ -1,27 +1,22 @@
 package com.twitter.finagle.postgresql
 
-abstract class PgSqlException extends RuntimeException
-case class PgSqlServerError(error: BackendMessage.ErrorResponse) extends PgSqlException {
-  override def getMessage: String = field(BackendMessage.Field.Message).getOrElse("Server error")
-
+abstract class PgSqlException(message: String) extends RuntimeException(message)
+case class PgSqlServerError(error: BackendMessage.ErrorResponse)
+  extends PgSqlException(error.values.getOrElse(BackendMessage.Field.Message, "Server error")) {
   def field(f: BackendMessage.Field): Option[String] = error.values.get(f)
 }
-case object PgSqlTlsUnsupportedError extends PgSqlException
+case object PgSqlTlsUnsupportedError extends PgSqlException("TLS is not supported by the server.")
 
-abstract class PgSqlClientError extends PgSqlException
-case class PgSqlUnsupportedError(msg: String) extends PgSqlClientError {
-  override def getMessage: String = msg
-}
-case object PgSqlPasswordRequired extends PgSqlClientError
-case class PgSqlUnsupportedAuthenticationMechanism(method: BackendMessage.AuthenticationMessage) extends PgSqlClientError
+abstract class PgSqlClientError(message: String) extends PgSqlException(message)
+case class PgSqlUnsupportedError(msg: String) extends PgSqlClientError(msg)
+case object PgSqlPasswordRequired extends PgSqlClientError("Password was not provided but is required.")
+case class PgSqlUnsupportedAuthenticationMechanism(method: BackendMessage.AuthenticationMessage)
+  extends PgSqlClientError(s"Unsupported authentication mechanism.")
 
-sealed trait PgSqlStateMachineError extends PgSqlClientError
-case class PgSqlInvalidMachineStateError(msg: String) extends PgSqlStateMachineError {
-  override def getMessage: String = msg
-}
-case class PgSqlNoSuchTransition(machine: String, state: String, msg: String) extends PgSqlStateMachineError {
-  override def getMessage: String = s"State machine $machine in state $state has no transition defined for message $msg"
-}
+sealed abstract class PgSqlStateMachineError(message: String) extends PgSqlClientError(message)
+case class PgSqlInvalidMachineStateError(msg: String) extends PgSqlStateMachineError(msg)
+case class PgSqlNoSuchTransition(machine: String, state: String, msg: String)
+  extends PgSqlStateMachineError(s"State machine $machine in state $state has no transition defined for message $msg")
 object PgSqlNoSuchTransition {
   def apply[S, B <: BackendMessage](machine: String, state: S, msg: B): PgSqlNoSuchTransition =
     PgSqlNoSuchTransition(machine, state.toString, msg.toString) // TODO: Show or similar instead of toString
