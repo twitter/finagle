00b6b42bbb6dc1326aa8d9f9e478a58de3896d30
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-10-26 21:11:12 -0400

    Adds tests for clear text and md5 password authentication.

--- finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/HandshakeSpec.scala
+++ finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/HandshakeSpec.scala
@@ -1,15 +1,44 @@
 package com.twitter.finagle.postgresql
 
-import com.twitter.finagle.postgresql.Response.Ready
+import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
 
-class HandshakeSpec extends PgSqlSpec with EmbeddedPgSqlSpec {
+class HandshakeSpec extends PgSqlSpec with EmbeddedPgSqlSpec with ResourceFileSpec {
+
+  override def configure(builder: EmbeddedPostgres.Builder): EmbeddedPostgres.Builder =
+    builder.setServerConfig("hba_file", toTmpFile("/handshake_pg_hba.conf").getAbsolutePath)
+
+  override def prep(e: EmbeddedPostgres): EmbeddedPostgres = {
+    using(e.getPostgresDatabase.getConnection(TestDbUser, "")) { conn =>
+      using(conn.createStatement()) { stmt =>
+        stmt.execute("CREATE USER clear_user WITH PASSWORD 'not secret'")
+        stmt.execute("CREATE USER md5_user WITH PASSWORD 'super secret'")
+      }
+    }
+    e
+  }
 
   "Handshake" should {
-    "support password-less authentication" in {
-      client(Request.Sync)
+
+    def authSpec(username: String, password: Option[String]) =
+      newClient(_.withCredentials(username, password))
+        .toService(Request.ConnectionParameters)
         .map { response =>
-          response must beEqualTo(Ready)
+          response must beLike {
+            case params: Response.ConnectionParameters =>
+              params.parameterMap(BackendMessage.Parameter.SessionAuthorization) must_== username
+          }
         }
+
+    "support password-less authentication" in {
+      authSpec("postgres", None)
+    }
+
+    "support clear text authentication" in {
+      authSpec("clear_user", Some("not secret"))
+    }
+
+    "support md5 authentication" in {
+      authSpec("md5_user", Some("super secret"))
     }
   }
 
