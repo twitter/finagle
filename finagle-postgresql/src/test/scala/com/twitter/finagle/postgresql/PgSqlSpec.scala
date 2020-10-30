c8e84f9ba10800ecd6b06902651543350cbcf74d
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-10-29 21:09:55 -0400

    fix: specs2 no longer delegates to Diffable.
    This changed recently in https://github.com/etorreborre/specs2/commit/59d0c3d0a8f22693b05fa50bfeb11a92c4b6c4c5
    
    Because implementing a proper `equals` for `Json` would require pulling in a json library, we bring back the ability
    to define a custom equals check in specs2 only and then use it where it matters.
    
    The reason why this only matters for jsonb is because postgres will transform the input json (removing duplicate keys and normalizing numbers and such).
    This results in a different json than the input, but still equivalent in the Json sense.

--- finagle-postgresql/src/test/scala/com/twitter/finagle/postgresql/PgSqlSpec.scala
+++ finagle-postgresql/src/test/scala/com/twitter/finagle/postgresql/PgSqlSpec.scala
@@ -1,5 +1,9 @@
 package com.twitter.finagle.postgresql
 
+import org.specs2.matcher.Expectable
+import org.specs2.matcher.MatchResult
+import org.specs2.matcher.Matcher
+import org.specs2.matcher.describe.Diffable
 import org.specs2.mutable.Specification
 import org.specs2.specification.core.Fragment
 import org.specs2.specification.core.Fragments
@@ -13,4 +17,25 @@ trait PgSqlSpec extends Specification with FutureResult {
   }
 
   def fragments(f: Seq[Fragment]) = Fragments(f: _*)
+
+  /**
+   * A Matcher that uses [[Diffable]]'s definition of being equal.
+   *
+   * This allows defining a custom equals definition.
+   *
+   * {{{
+   *   implicit lazy val myDiff: Diffable[Complicated] = ???
+   *
+   *   val complicated: Complicated = ???
+   *
+   *   complicated must beIdentical(expected)
+   * }}}
+   */
+  def beIdentical[V](v: V)(implicit d: Diffable[V]): Matcher[V] = new Matcher[V] {
+    override def apply[S <: V](t: Expectable[S]): MatchResult[S] = {
+      val diff = d.diff(v, t.value)
+      result(diff.identical, s"${t.description} == '$v'", diff.render, t)
+    }
+  }
+
 }
