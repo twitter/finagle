bafc8a4ddd02b2556423fb5783d46bf7fa2f89dc
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-10-29 23:49:30 -0400

    tidy: truncated comment.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/Request.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/Request.scala
@@ -14,7 +14,24 @@ object Request {
    * of parameter status values to the client. These are accumulated in the dispatcher which isn't
    * accessible by the client.
    *
-   * Thus, the client that
+   * This request can be used to obtain those parameters.
+   * Note that in order for these to be meaningful for subsequent requests, those
+   * must be made on the same connection. For example:
+   *
+   * {{{
+   *   val sf: ServiceFactory[Request, Response]
+   *   sf().flatMap { service =>
+   *     service(Request.ConnectionParameters)
+   *       .flatMap { params =>
+   *         val something = withParams(params)
+   *         // subsequent request on the same connection.
+   *         service(Request.Query(...))
+   *       }
+   *   }
+   * }}}
+   *
+   *
+   * @see [[Response.ConnectionParameters]]
    */
   case object ConnectionParameters extends Request
   case object Sync extends Request
