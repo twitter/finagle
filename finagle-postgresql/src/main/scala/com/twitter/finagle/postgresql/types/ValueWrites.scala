09bfac6771e7e76901dc2da3f370369ecc7e40d3
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-11-12 22:30:04 -0500

    Adds spec for ValueWrites.

--- finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/types/ValueWrites.scala
+++ finagle-postgresql/src/main/scala/com/twitter/finagle/postgresql/types/ValueWrites.scala
@@ -2,6 +2,7 @@ package com.twitter.finagle.postgresql.types
 
 import java.nio.charset.Charset
 
+import com.twitter.finagle.postgresql.Types.Inet
 import com.twitter.finagle.postgresql.Types.WireValue
 import com.twitter.finagle.postgresql.transport.PgBuf
 import com.twitter.io.Buf
@@ -57,6 +58,38 @@ object ValueWrites {
     override def accepts(tpe: PgType): Boolean = accept(tpe)
   }
 
+  implicit def optionWrites[T](implicit twrites: ValueWrites[T]): ValueWrites[Option[T]] = new ValueWrites[Option[T]] {
+    override def writes(tpe: PgType, value: Option[T], charset: Charset): WireValue =
+      value match {
+        case Some(v) => twrites.writes(tpe, v, charset)
+        case None => WireValue.Null
+      }
+    override def accepts(tpe: PgType): Boolean = twrites.accepts(tpe)
+  }
+
+  implicit def traversableWrites[F[X] <: Iterable[X], T](implicit twrites: ValueWrites[T]): ValueWrites[F[T]] = {
+    val _ = twrites
+    unimplemented
+  }
+
+  def unimplemented[T] = new ValueWrites[T] {
+    override def writes(tpe: PgType, value: T, charset: Charset): WireValue = ???
+
+    override def accepts(tpe: PgType): Boolean = ???
+  }
+
+  implicit lazy val writesBigDecimal: ValueWrites[BigDecimal] = unimplemented
   implicit lazy val writesBoolean: ValueWrites[Boolean] = simple(PgType.Bool)((w, t) => w.byte(if (t) 1 else 0))
   implicit lazy val writesBuf: ValueWrites[Buf] = simple(PgType.Bytea)(_.buf(_))
+  implicit lazy val writesByte: ValueWrites[Byte] = unimplemented
+  implicit lazy val writesDouble: ValueWrites[Double] = simple(PgType.Float8)(_.double(_))
+  implicit lazy val writesFloat: ValueWrites[Float] = simple(PgType.Float4)(_.float(_))
+  implicit lazy val writesInet: ValueWrites[Inet] = simple(PgType.Inet)(_.inet(_))
+  implicit lazy val writesInstant: ValueWrites[java.time.Instant] = unimplemented
+  implicit lazy val writesInt: ValueWrites[Int] = simple(PgType.Int4)(_.int(_))
+  implicit lazy val writesJson: ValueWrites[Json] = unimplemented
+  implicit lazy val writesLong: ValueWrites[Long] = simple(PgType.Int8)(_.long(_))
+  implicit lazy val writesShort: ValueWrites[Short] = simple(PgType.Int2)(_.short(_))
+  implicit lazy val writesString: ValueWrites[String] = unimplemented
+  implicit lazy val writesUuid: ValueWrites[java.util.UUID] = unimplemented
 }
