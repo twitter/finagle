1d953fbe95bdb3cc96c4dfafd7627881909a27dc
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-11-14 09:50:39 -0500

    Prepared statement caching: adds failing test.

--- finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/RichClientSpec.scala
+++ finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/RichClientSpec.scala
@@ -2,6 +2,7 @@ package com.twitter.finagle.postgresql
 
 import com.twitter.io.Buf
 import com.twitter.io.Reader
+import com.twitter.util.Await
 
 class RichClientSpec extends PgSqlSpec with EmbeddedPgSqlSpec {
 
@@ -69,6 +70,22 @@ class RichClientSpec extends PgSqlSpec with EmbeddedPgSqlSpec {
           rs.rows.head.get[Buf](1) must_== Buf.ByteArray(0, 1, 2, 3, 4)
         }
     }
+
+    "prepare reuse" in {
+      val stmt = newRichClient.prepare("select $1::bool, $2::bytea")
+
+      def read(param1: Boolean, param2: Buf) =
+        stmt
+          .read(Parameter(param1) :: Parameter(param2) :: Nil)
+          .map { rs =>
+            rs.rows must haveSize(1)
+            rs.rows.head.get[Boolean](0) must_== param1
+            rs.rows.head.get[Buf](1) must_== param2
+          }
+
+      Await.result(read(false, Buf.ByteArray(-1, 0,1,2)))
+      Await.result(read(true, Buf.ByteArray(4,3,2,1)))
+    }.pendingUntilFixed
   }
 
 }
