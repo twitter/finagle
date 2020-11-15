eb5d9b03ec1c891a9a9e4106290cad13f1bb25e6
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-11-15 00:12:51 -0500

    Use docker for the handshake spec.

--- finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/HandshakeSpec.scala
+++ finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/HandshakeSpec.scala
@@ -1,33 +1,42 @@
 package com.twitter.finagle.postgresql
 
-import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
+import com.spotify.docker.client.messages.HostConfig
+import com.whisk.docker.testkit.ContainerSpec
 
-class HandshakeSpec extends PgSqlSpec with EmbeddedPgSqlSpec with ResourceFileSpec {
+class HandshakeSpec extends PgSqlIntegrationSpec with ResourceFileSpec {
 
-  override def configure(builder: EmbeddedPostgres.Builder): EmbeddedPostgres.Builder =
-    builder.setServerConfig("hba_file", toTmpFile("/handshake_pg_hba.conf").getAbsolutePath)
+  override def configure(spec: ContainerSpec): ContainerSpec = {
+    spec
+      .withVolumeBindings(
+        HostConfig.Bind.builder()
+          .from(toTmpFile("/handshake_pg_hba.conf").getAbsolutePath)
+          .to("/handshake/pg_hba.conf")
+          .build()
+      )
+      .withCommand("-c", "hba_file=/handshake/pg_hba.conf")
+  }
 
-  override def prep(e: EmbeddedPostgres): EmbeddedPostgres = {
-    using(e.getPostgresDatabase.getConnection(TestDbUser, "")) { conn =>
-      using(conn.createStatement()) { stmt =>
-        stmt.execute("CREATE USER clear_user WITH PASSWORD 'not secret'")
-        stmt.execute("CREATE USER md5_user WITH PASSWORD 'super secret'")
-      }
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val _ = withStatement() { stmt =>
+      stmt.execute("CREATE USER clear_user WITH PASSWORD 'not secret'")
+      stmt.execute("CREATE USER md5_user WITH PASSWORD 'super secret'")
     }
-    e
   }
 
   "Handshake" should {
 
     def authSpec(username: String, password: Option[String]) =
-      newClient(_.withCredentials(username, password))
-        .toService(Request.ConnectionParameters)
-        .map { response =>
-          response must beLike {
-            case params: Response.ConnectionParameters =>
-              params.parameterMap(BackendMessage.Parameter.SessionAuthorization) must_== username
+      withClient(cfg = _.withCredentials(username, password)) { client =>
+        client
+          .toService(Request.ConnectionParameters)
+          .map { response =>
+            response must beLike {
+              case params: Response.ConnectionParameters =>
+                params.parameterMap(BackendMessage.Parameter.SessionAuthorization) must_== username
+            }
           }
-        }
+      }
 
     "support password-less authentication" in {
       authSpec("postgres", None)
