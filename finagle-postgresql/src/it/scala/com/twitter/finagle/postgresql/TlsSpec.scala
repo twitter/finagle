b5e5eeea6b86da7a8a482d1874d00ed6c0d91a67
Philippe Laflamme <philippe.laflamme@gmail.com>
2020-11-15 00:25:53 -0500

    Use docker for TLS specs.

--- finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/TlsSpec.scala
+++ finagle-postgresql/src/it/scala/com/twitter/finagle/postgresql/TlsSpec.scala
@@ -1,31 +1,55 @@
 package com.twitter.finagle.postgresql
 
+import com.spotify.docker.client.messages.HostConfig
 import com.twitter.finagle.PostgreSql
 import com.twitter.finagle.ssl.TrustCredentials
 import com.twitter.finagle.ssl.client.SslClientConfiguration
-import io.zonky.test.db.postgres.embedded.EmbeddedPostgres
+import com.whisk.docker.testkit.ContainerSpec
 
-class TlsSpec extends PgSqlSpec with EmbeddedPgSqlEach with ResourceFileSpec {
+class TlsSpec extends PgSqlIntegrationSpec with ResourceFileSpec {
 
-  def configureTls(builder: EmbeddedPostgres.Builder): EmbeddedPostgres.Builder =
-    builder
-      .setServerConfig("ssl", "true")
-      .setServerConfig("ssl_cert_file", toTmpFile("/server.crt").getAbsolutePath)
-      .setServerConfig("ssl_key_file", toTmpFile("/server.key").getAbsolutePath)
+  override def configure(spec: ContainerSpec): ContainerSpec = {
+    spec
+      .withVolumeBindings(
+        HostConfig.Bind.builder()
+          .from(toTmpFile("/server.crt").getAbsolutePath)
+          .to("/certs/server.crt")
+          .readOnly(true)
+          .build(),
+        HostConfig.Bind.builder()
+          .from(toTmpFile("/server.key").getAbsolutePath)
+          .to("/certs/server.key")
+          .readOnly(true)
+          .build()
+      )
+      .withCommand(
+        "-c", "ssl=true",
+        "-c", "ssl_cert_file=/certs/server.crt",
+        "-c", "ssl_key_file=/certs/server.key",
+      )
+  }
 
   def withTls(client: PostgreSql.Client): PostgreSql.Client =
     client.withTransport.tls(SslClientConfiguration(trustCredentials = TrustCredentials.Insecure))
 
   "TLS" should {
 
-    "support tls" in embeddedPsql(configureTls, withTls) { (_, client) =>
+    "support tls" in withClient(cfg = withTls) { client =>
       client.toService(Request.Sync)
         .map { response =>
           response must beEqualTo(Response.Ready)
         }
     }
+  }
+}
 
-    "handle unsupported tls" in embeddedPsql(identity, withTls) { (_, client) =>
+class MissingTlsSpec extends PgSqlIntegrationSpec with ResourceFileSpec {
+
+  def withTls(client: PostgreSql.Client): PostgreSql.Client =
+    client.withTransport.tls(SslClientConfiguration(trustCredentials = TrustCredentials.Insecure))
+
+  "TLS" should {
+    "handle unsupported tls" in withClient(cfg = withTls) { client =>
       client.toService(Request.Sync)
         .liftToTry
         .map { response =>
