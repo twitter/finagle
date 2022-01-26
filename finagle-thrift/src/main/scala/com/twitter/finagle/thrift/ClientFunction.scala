976f154fd8caba0b6f91916eab382c4b9c583d3b
Jing Yan <jyan@twitter.com>
2022-01-26 17:33:25 +0000

    scrooge: generate a method to validate request and return the violations in Scala
    Problem/Solution
    For each method defined in a `service` in the Thrift IDL, generate a new
    API `violationReturning<method_name>` in `MethodPerEndpoint` to
    validate incoming request (of Struct, Union, Exception types) by invoking
    the `ValidateInstanceValue` method on the request, and return any
    violations (as method parameters `<request_variable>Violations`)
    back to the users in the method API, the new method will by default
    wrapping the implementation of the existing method.
    
    Scrooge will provide the implementation for the new method for client,
    by wrapping the implementation of existing method, there is no behavior
    change on the client side.
    
    When there is validation annotation on the IDL, finagle will call the new
    `violationReturning<method_name>`, if user doesn't implement the new
    method, it will be directing to the existing method; if user implement the
    new method, the existing behavior will be overriden by the new
    impementation.
    
    **Added a flag `hasValidationAnnotation` in the parser, all new APIs
    will only be generated when the flag is true. There is no runtime
    behavior change on existing services.**
    
    Result
    When `violationReturning<method_name>` is unimplemented, both
    call to `<method_name>` and `violationReturning<method_name>`
    will use the implementation of `<method_name>`, and throw an
    exception when any request fails validation.
    When `violationReturning<method_name>` is implemented, both
    call to `<method_name>` and `violationReturning<method_name>`
    will use the implementation of `violationReturning<method_name>`,
    where no exception is thrown per request validation violations.
    
    JIRA Issues: CSL-11150
    
    Differential Revision: https://phabricator.twitter.biz/D799294

---
+++ finagle-thrift/src/main/scala/com/twitter/finagle/thrift/ClientFunction.scala
@@ -0,0 +1,126 @@
+package com.twitter.finagle.thrift
+
+import com.twitter.finagle.Service
+import com.twitter.finagle.SourcedException
+import com.twitter.finagle.context.Contexts
+import com.twitter.finagle.service.ReqRep
+import com.twitter.finagle.service.ResponseClass
+import com.twitter.finagle.service.ResponseClassifier
+import com.twitter.finagle.stats.Counter
+import com.twitter.finagle.stats.StatsReceiver
+import com.twitter.finagle.thrift.ClientDeserializeCtx.Key
+import com.twitter.finagle.thrift.Headers.Request
+import com.twitter.scrooge.TReusableBuffer
+import com.twitter.scrooge.ThriftStruct
+import com.twitter.util.Future
+import com.twitter.util.Throwables
+import com.twitter.util.Try
+import org.apache.thrift.protocol.TMessage
+import org.apache.thrift.protocol.TMessageType
+import org.apache.thrift.protocol.TProtocolFactory
+
+object ClientFunction {
+
+  /**
+   * Serialize a given request {@code inputArgs} and send the serialized
+   * request over the wire to a finagle service {@code service}, and
+   * classify the deserialized response Upon receiving it from the server.
+   *
+   * @param clientFuncNameForWire the over the wire function name
+   * @param replyDeserializer the deserializer used to create a
+   *                          [[ClientDeserializeCtx]]
+   * @param inputArgs the request to serialize
+   * @param serviceName used to exception metrics scoping
+   * @param service the service to execute the serialized request, and
+   *                return a deserialized response
+   * @param responseClassifier classify the deserialized response
+   * @param tlReusableBuffer a buffer to call [[encodeRequest]]
+   * @param protocolFactory a [[TProtocolFactory]] to call
+   *                        [[encodeRequest]]
+   * @param failuresScope a counter scope to report exceptions
+   * @param requestCounter a counter to report number of request
+   * @param successCounter a counter to report number of successful
+   *                       request
+   * @param failuresCounter a counter to report number of failed
+   *                        request
+   * @tparam T The type of deserialized response
+   *
+   * @note This method is intended to be invoked from Scrooge generated
+   *       finagle client, typical scrooge users should not need to call
+   *       it directly.
+   */
+  def serde[T](
+    clientFuncNameForWire: String,
+    replyDeserializer: Array[Byte] => Try[T],
+    inputArgs: ThriftStruct,
+    serviceName: String,
+    service: Service[ThriftClientRequest, Array[Byte]],
+    responseClassifier: PartialFunction[ReqRep, ResponseClass],
+    tlReusableBuffer: TReusableBuffer,
+    protocolFactory: TProtocolFactory,
+    failuresScope: StatsReceiver,
+    requestCounter: Counter,
+    successCounter: Counter,
+    failuresCounter: Counter
+  ): Future[T] = {
+    requestCounter.incr()
+
+    val serdeCtx = new ClientDeserializeCtx[T](inputArgs, replyDeserializer)
+    Contexts.local.let(
+      Key,
+      serdeCtx,
+      Request.Key,
+      Request.newValues
+    ) {
+      serdeCtx.rpcName(clientFuncNameForWire)
+      val start = System.nanoTime
+      val serialized =
+        encodeRequest(clientFuncNameForWire, inputArgs, tlReusableBuffer, protocolFactory)
+      serdeCtx.serializationTime(System.nanoTime - start)
+      service(serialized)
+        .flatMap { response =>
+          Future.const(serdeCtx.deserialize(response))
+        }.respond { response =>
+          val classified =
+            responseClassifier.applyOrElse(ReqRep(inputArgs, response), ResponseClassifier.Default)
+          classified match {
+            case _: ResponseClass.Successful =>
+              successCounter.incr()
+            case _: ResponseClass.Failed =>
+              failuresCounter.incr()
+              if (response.isThrow) {
+                SourcedException.setServiceName(response.throwable, serviceName)
+                failuresScope.counter(Throwables.mkString(response.throwable): _*).incr()
+              }
+            case _ =>
+          } // Last ResponseClass is Ignorable, which we do not need to record
+        }
+    }
+  }
+
+  private[this] def encodeRequest(
+    name: String,
+    args: ThriftStruct,
+    tlReusableBuffer: TReusableBuffer,
+    protocolFactory: TProtocolFactory
+  ): ThriftClientRequest = {
+    val memoryBuffer = tlReusableBuffer.get()
+    try {
+      val oprot = protocolFactory.getProtocol(memoryBuffer)
+
+      oprot.writeMessageBegin(new TMessage(name, TMessageType.CALL, 0))
+      args.write(oprot)
+      oprot.writeMessageEnd()
+      oprot.getTransport.flush()
+      val bytes = java.util.Arrays.copyOfRange(
+        memoryBuffer.getArray(),
+        0,
+        memoryBuffer.length()
+      )
+      new ThriftClientRequest(bytes, false)
+    } finally {
+      tlReusableBuffer.reset()
+    }
+  }
+
+}
