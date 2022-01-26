008f290798553df2f08928c135cdaa50c13c435a
Bryce Anderson <banderson@twitter.com>
2022-01-26 20:38:24 +0000

    finagle-core: Add a reverse engineering based test for Drv
    Problem
    
    All our tests for Drv rely on generating a distribution via invocation
    of a random number generator and trying to make sure that matches with
    what the weights of the probability distribution. This pattern is
    vulnerable to the inherent fluctuations to randomness and makes it so
    our tests are 'seed' dependent. This can result in lowered confidence in
    our tests.
    
    Solution
    
    Provide a test that will reverse engineer the alias tables. The test is
    no longer dependent on random number generation. As a bit of irony, we
    use property based testing to feed this test randomly generated probability
    distributions.
    
    Differential Revision: https://phabricator.twitter.biz/D820500

--- finagle-core/src/test/scala/com/twitter/finagle/util/DrvTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/util/DrvTest.scala
@@ -1,8 +1,10 @@
 package com.twitter.finagle.util
 
+import org.scalacheck.Gen
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class DrvTest extends AnyFunSuite {
+class DrvTest extends AnyFunSuite with ScalaCheckDrivenPropertyChecks {
   val N = 100000
 
   test("Drv.newVose(weights)") {
@@ -43,5 +45,70 @@ class DrvTest extends AnyFunSuite {
 
   test("Drv.apply(nil)") {
     intercept[IllegalArgumentException] { Drv(Nil) }
+  }
+
+  private[this] def normalize(numbers: Seq[Double]): IndexedSeq[Double] = {
+    val sum = numbers.sum
+    if (sum == 0d) {
+      // Must all be zeros
+      val len = numbers.length
+      Vector.fill(len) {
+        1d / len
+      }
+    } else {
+      numbers.iterator.map(_ / sum).toIndexedSeq
+    }
+  }
+
+  private[this] def assertAliasTableIsCorrect(probabilities: IndexedSeq[Double]): Unit = {
+    val Drv.Aliased(aliased, prob) = Drv.fromWeights(probabilities)
+
+    // We're going to reverse engineer the probability distribution from
+    // the alias tables. This is pretty straight forward: for every index `i`
+    // we simply ascribe the probability of that index to the index itself
+    // weighted by it's probability and scribe the rest of the probability
+    // to the element in it's alias table.
+    val p = new Array[Double](probabilities.length)
+    p.indices.foreach { i =>
+      p(i) += prob(i)
+      p(aliased(i)) += (1d - prob(i))
+    }
+
+    // At the end we need to normalize since all our probabilities were scaled
+    // to 1.0 in order to play nicely with the random number generator which
+    // happens to generate floating point numbers on the range [0.0, 1.0).
+    p.indices.foreach { i =>
+      p(i) /= p.length
+    }
+
+    // now make sure they match
+    p.indices.foreach { i =>
+      val epsilon = 0.00001
+      assert(math.abs(probabilities(i) - p(i)) < epsilon)
+    }
+  }
+
+  test("Reverse engineer simple distributions") {
+    val distributions: Seq[IndexedSeq[Double]] = Seq(
+      Seq(0d),
+      Seq(1d),
+      Seq(0d, 1d),
+      Seq(0d, 0d, 1d),
+      Seq(1d, 1d, 1d),
+      Seq(1d, 1d, 2d),
+    ).map(normalize(_))
+
+    distributions.foreach { dist =>
+      assertAliasTableIsCorrect(dist)
+    }
+  }
+
+  test("Property test that Drv alias tables can be reverse engineered") {
+    val normalizedNonEmptyList: Gen[IndexedSeq[Double]] =
+      Gen.nonEmptyListOf(Gen.chooseNum(0d, 1e5)).map(normalize(_))
+
+    forAll(normalizedNonEmptyList) { probabilities =>
+      assertAliasTableIsCorrect(probabilities)
+    }
   }
 }
