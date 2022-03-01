d3e073853f6f9efc1ac74db6bf84400e1144e961
Bryce Anderson <banderson@twitter.com>
2022-03-01 16:29:04 +0000

    finagle-stats-core: Fix broken MetricsHostStatsReceiverTest
    Problem
    
    We're querying for metrics using `Seq(name)` against a map keyed by a
    simple `String`. This isn't caught by the compiler because it's a java
    map and the `.get(..)` method only wants an `Object`. The tests happen
    to work because, as it turns out, `null == null`.
    
    Solution
    
    Both fix the keys and make sure the keys we query for return non-null
    values.
    
    Differential Revision: https://phabricator.twitter.biz/D842563

--- finagle-stats-core/src/test/scala/com/twitter/finagle/stats/MetricsHostStatsReceiverTest.scala
+++ finagle-stats-core/src/test/scala/com/twitter/finagle/stats/MetricsHostStatsReceiverTest.scala
@@ -3,19 +3,25 @@ package com.twitter.finagle.stats
 import org.scalatest.funsuite.AnyFunSuite
 
 class MetricsHostStatsReceiverTest extends AnyFunSuite {
+
+  private[this] def notNull(n: Number): Number = {
+    if (n == null) throw null
+    else n
+  }
+
   val hostStatsReceiver = new MetricsHostStatsReceiver()
 
   def readHostStatsReceiverGauge(name: String): Number =
-    hostStatsReceiver.registry.gauges.get(Seq(name))
+    notNull(hostStatsReceiver.registry.gauges.get(name))
 
   def readHostStatsReceiverCounter(name: String): Number =
-    hostStatsReceiver.registry.counters.get(Seq(name))
+    notNull(hostStatsReceiver.registry.counters.get(name))
 
   def readUnderlyingStatsReceiverGauge(name: String): Number =
-    hostStatsReceiver.self.registry.gauges.get(Seq(name))
+    notNull(hostStatsReceiver.self.registry.gauges.get(name))
 
   def readUnderlyingStatsReceiverCounter(name: String): Number =
-    hostStatsReceiver.self.registry.counters.get(Seq(name))
+    notNull(hostStatsReceiver.self.registry.counters.get(name))
 
   test("MetricsHostStatsReceiver is a proxy of underlying MetricsStatsReceiver") {
     hostStatsReceiver.addGauge("my_cumulative_gauge")(1)
