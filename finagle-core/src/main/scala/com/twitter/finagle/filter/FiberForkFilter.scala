54eb20852426f12d27819f5a6f98983bf9d94de4
Alicia Vargas-Morawetz <avargasmorawetz@twitter.com>
2022-10-25 19:53:29 +0000

    [finagle-core] Use work queue based fiber behind a toggle
    Problem:
    We would like to start using the work queue based fiber in the finagle stack.
    
    Solution:
    Gated by a toggle, pick the ExecutorWorkQueueFiber or the SchedulerWorkQueueFiber in the FiberForkFilter. Toggled on servers with offloading enabled will use the ExecutorWorkQueueFiber, which backs the fiber with the offload pool. All other toggled on servers will use the scheduler backed work queue fiber.
    
    JIRA Issues: CSL-12357
    
    Differential Revision: https://phabricator.twitter.biz/D992075

--- finagle-core/src/main/scala/com/twitter/finagle/filter/FiberForkFilter.scala
+++ finagle-core/src/main/scala/com/twitter/finagle/filter/FiberForkFilter.scala
@@ -1,36 +1,93 @@
 package com.twitter.finagle.filter
 
+import com.twitter.app.GlobalFlag
+import com.twitter.concurrent.Scheduler
+import com.twitter.finagle.CoreToggles
 import com.twitter.finagle.Service
 import com.twitter.finagle.ServiceFactory
 import com.twitter.finagle.SimpleFilter
 import com.twitter.finagle.Stack
 import com.twitter.finagle.Stackable
+import com.twitter.finagle.server.ServerInfo
+import com.twitter.finagle.stats.FinagleStatsReceiver
+import com.twitter.logging.Logger
+import com.twitter.util.ExecutorWorkQueueFiber
+import com.twitter.util.ExecutorServiceFuturePool
 import com.twitter.util.Fiber
 import com.twitter.util.Future
+import com.twitter.util.FuturePool
+import com.twitter.util.SchedulerWorkQueueFiber
+import com.twitter.util.WorkQueueFiber.FiberMetrics
+
+object restrictWorkQueueFiberZone
+    extends GlobalFlag[Boolean](
+      true,
+      "When enabled, restricts work queue fiber usage to ATLA. Enabled by default."
+    )
+
+private class FiberForkFilter[Req, Rep](fiberFactory: () => Fiber) extends SimpleFilter[Req, Rep] {
+
+  def apply(
+    request: Req,
+    service: Service[Req, Rep]
+  ): Future[Rep] = {
+    Fiber.let(fiberFactory()) {
+      service(request)
+    }
+  }
+}
 
 private[finagle] object FiberForkFilter {
+  private[this] val log = Logger.get
 
   val Description: String = "Fork requests into a new fiber"
   val Role: Stack.Role = Stack.Role("FiberForkFilter")
 
+  private val toggle = CoreToggles("com.twitter.finagle.filter.UseWorkQueueFiber")
+
+  private[filter] def generateFiberFactory(pool: Option[FuturePool]): () => Fiber = {
+    val workQueueFiberEnabled: Boolean = {
+      val serverInfo = ServerInfo()
+      val zoneEnabled = serverInfo.zone.contains("atla") || !restrictWorkQueueFiberZone()
+      zoneEnabled && toggle(serverInfo.id.hashCode)
+    }
+    if (workQueueFiberEnabled) {
+      pool match {
+        case Some(exec: ExecutorServiceFuturePool) =>
+          log.info("Server has Offload Pool backed work-queueing fibers enabled")
+          () => new ExecutorWorkQueueFiber(exec.executor, FiberForkFilter.Metrics)
+        case _ =>
+          log.info("Server has Scheduler backed work-queueing fibers enabled")
+          () => new SchedulerWorkQueueFiber(Scheduler(), FiberForkFilter.Metrics)
+      }
+    } else {
+      log.info("Server has work-queueing fibers disabled")
+      () => Fiber.newCachedSchedulerFiber()
+    }
+  }
+
   def module[Req, Rep]: Stackable[ServiceFactory[Req, Rep]] =
-    new Stack.Module0[ServiceFactory[Req, Rep]] {
+    new Stack.Module1[OffloadFilter.Param, ServiceFactory[Req, Rep]] {
       def role: Stack.Role = Role
       def description: String = Description
 
-      private[this] val filter = new SimpleFilter[Req, Rep] {
-        def apply(
-          request: Req,
-          service: Service[Req, Rep]
-        ): Future[Rep] = {
-          Fiber.let(Fiber.newCachedSchedulerFiber()) {
-            service(request)
-          }
-        }
-      }
-
-      def make(next: ServiceFactory[Req, Rep]): ServiceFactory[Req, Rep] = {
-        filter.andThen(next)
+      def make(p: OffloadFilter.Param, next: ServiceFactory[Req, Rep]): ServiceFactory[Req, Rep] = {
+        new FiberForkFilter(generateFiberFactory(p.pool)).andThen(next)
       }
     }
+
+  def Metrics = new FiberMetrics {
+    private[this] val statsReceiver = FinagleStatsReceiver.scope("fiber")
+    private[this] val createdFibers = statsReceiver.counter("created")
+    private[this] val schedulerSubmissions = statsReceiver.counter("submissions")
+    private[this] val taskSubmissions = statsReceiver.counter("tasks")
+    private[this] val threadLocalSubmissions = statsReceiver.counter("threadlocaltask")
+    private[this] val flushes = statsReceiver.counter("flushes")
+
+    override def fiberCreated(): Unit = createdFibers.incr()
+    override def threadLocalSubmitIncrement(): Unit = threadLocalSubmissions.incr()
+    override def taskSubmissionIncrement(): Unit = taskSubmissions.incr()
+    override def schedulerSubmissionIncrement(): Unit = schedulerSubmissions.incr()
+    override def flushIncrement(): Unit = flushes.incr()
+  }
 }
--- finagle-core/src/test/scala/com/twitter/finagle/filter/FiberForkFilterTest.scala
+++ finagle-core/src/test/scala/com/twitter/finagle/filter/FiberForkFilterTest.scala
@@ -5,11 +5,18 @@ import com.twitter.finagle.Service
 import com.twitter.finagle.ServiceFactory
 import com.twitter.finagle.Stack
 import com.twitter.util.Await
+import com.twitter.util.ExecutorWorkQueueFiber
+import com.twitter.util.Fiber
 import com.twitter.util.Local
 import com.twitter.util.Future
+import com.twitter.util.FuturePool
+import com.twitter.util.SchedulerWorkQueueFiber
+import java.util.concurrent.Executors
 import org.scalatest.funsuite.AnyFunSuite
 
 class FiberForkFilterTest extends AnyFunSuite {
+
+  private val useWorkQueueToggleKey = "com.twitter.finagle.filter.UseWorkQueueFiber"
 
   test("FiberForkFilter should modify the Local context") {
     val ctxAtStart: Local.Context = Local.save()
@@ -19,7 +26,6 @@ class FiberForkFilterTest extends AnyFunSuite {
       Future.Unit
     }
 
-    FiberForkFilter.module
     val stack = FiberForkFilter
       .module[Unit, Unit]
       .toStack(Stack.leaf(Stack.Role("test"), ServiceFactory.const(svc)))
@@ -28,5 +34,50 @@ class FiberForkFilterTest extends AnyFunSuite {
     // The FiberForkFilter sets a new Fiber on the context so the Local context within the
     // service should not be equal to the initial Local context
     assert(ctxAtStart != ctxInServiceOption.get)
+  }
+
+  test("Work queueing fibers disabled by zone") {
+    // restrict zone
+    com.twitter.finagle.filter.restrictWorkQueueFiberZone.let(true) {
+      // enable toggle, but it should not matter
+      com.twitter.finagle.toggle.flag.overrides.let(useWorkQueueToggleKey, 1) {
+        val fiberFactory = FiberForkFilter.generateFiberFactory(Option.empty)
+        assert(fiberFactory.apply().getClass == Fiber.newCachedSchedulerFiber().getClass)
+      }
+    }
+  }
+
+  test("Work queue fibers disabled by toggle") {
+    // allow all zones
+    com.twitter.finagle.filter.restrictWorkQueueFiberZone.let(false) {
+      // disable toggle
+      com.twitter.finagle.toggle.flag.overrides.let(useWorkQueueToggleKey, 0) {
+        val fiberFactory = FiberForkFilter.generateFiberFactory(Option.empty)
+        assert(fiberFactory.apply().getClass == Fiber.newCachedSchedulerFiber().getClass)
+      }
+    }
+  }
+
+  test("Executor work queue fibers enabled by toggle") {
+    // allow all zones
+    com.twitter.finagle.filter.restrictWorkQueueFiberZone.let(false) {
+      // enable toggle
+      com.twitter.finagle.toggle.flag.overrides.let(useWorkQueueToggleKey, 1) {
+        val fiberFactory =
+          FiberForkFilter.generateFiberFactory(Some(FuturePool(Executors.newFixedThreadPool(1))))
+        assert(fiberFactory.apply().isInstanceOf[ExecutorWorkQueueFiber])
+      }
+    }
+  }
+
+  test("Scheduler work queue fibers enabled by toggle") {
+    // allow all zones
+    com.twitter.finagle.filter.restrictWorkQueueFiberZone.let(false) {
+      // enable toggle
+      com.twitter.finagle.toggle.flag.overrides.let(useWorkQueueToggleKey, 1) {
+        val fiberFactory = FiberForkFilter.generateFiberFactory(Option.empty)
+        assert(fiberFactory.apply().isInstanceOf[SchedulerWorkQueueFiber])
+      }
+    }
   }
 }
